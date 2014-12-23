86250ca82945025767652293bd65c14edcee6a19
Quenty <rbxquenty@gmail.com>
2014-12-22 23:56:59 -0600

    Reauthored EasyConfiguration

--- Modules/qSystems/EasyConfiguration.lua
+++ Modules/qSystems/EasyConfiguration.lua
@@ -1,138 +1,199 @@
-local ReplicatedStorage = game:GetService("ReplicatedStorage")
-
-local NevermoreEngine   = require(ReplicatedStorage:WaitForChild("NevermoreEngine"))
-local LoadCustomLibrary = NevermoreEngine.LoadLibrary
-
-local qSystems          = LoadCustomLibrary("qSystems")
-local Type              = LoadCustomLibrary("Type")
-
-local Make = qSystems.Make
-local Modify = qSystems.Modify
-
--- EasyConfiguration.lua
 -- @author Quenty
--- Last Modified February 3rd, 2014
+--- A better EasyConfiguration system.
 
-local lib = {}
+local MakeEasyConfiguration do
+	local Index = {
+		AddValue = function(self, ClassType, Properties)
+			local OldValue = self.Container:FindFirstChild(Properties.Name) --- Check to make sure the old one doesn't exist.
 
-local AcceptableTypes = { -- Anything in here has a .Value property. 
-	["StringValue"]         = true;
-	["IntValue"]            = true;
-	["NumberValue"]         = true;
-	["BrickColorValue"]     = true;
-	["BoolValue"]           = true;
-	["Color3Value"]         = true;
-	["Vector3Value"]        = true;
-	["IntConstrainedValue"] = true;
-}
-
-local function TypeIsAcceptable(TypeName)
-	return AcceptableTypes[TypeName]
-end
-lib.TypeIsAcceptable = TypeIsAcceptable;
-lib.typeIsAcceptable = TypeIsAcceptable
-
-local function AddSubDataLayer(DataName, Parent)
-	-- For organization of data. Adds another configuration with the name "DataName", if one can't be found, and then returns it. 
-
-	local DataContainer = Parent:FindFirstChild(DataName) or Make("Configuration", {
-		Name = DataName;
-		Parent = Parent;
-		Archivable = true;
-	})
-
-	return DataContainer
-end
-lib.AddSubDataLayer = AddSubDataLayer
-lib.addSubDataLayer = AddSubDataLayer
-
-local EasyConfigCache = {}
-setmetatable(EasyConfigCache, {__mode = "k"})
-
-local function MakeEasyConfiguration(Configuration)
-	if EasyConfigCache[Configuration] then
-		return EasyConfigCache[Configuration]
-	else
-		local NewConfiguration = {}
-
-		local function Get(ObjectName)
-			return Configuration:FindFirstChild(ObjectName)
-		end
-
-		setmetatable(NewConfiguration, {
-			__index = function(_, value)
-
-				if not type(value) == "string" then
-					error("Only Indexing with strings is supported with easyConfiguration", 2)
+			if not OldValue then
+				local newInstance = Instance.new(ClassType)
+				for Index, Value in pairs(Properties) do
+					newInstance[Index] = Value
 				end
+				newInstance.Parent = self.Container
+			elseif not OldValue:IsA(ClassType) then
+				error("Value '" .. tostring(Properties.Name) .. "' already exists in the configuration, but is the wrong class type")
+			end
+		end;
+		GetValue = function(self, ValueName)
+			return self.Container:FindFirstChild(ValueName) or error("Value '" .. tostring(ValueName) .. "' does not exist in EasyConfiguration")
+		end;
+	}
+	Index.Get = Index.GetValue
+	Index.Add = Index.AddValue
 
-				local loweredValue = value:lower()
+	local Metatable = {
+		__index = function(self, k)
+			if Index[k] then
+				return Index[k]
+			else
+				local Value = self:GetValue(k)
+				return Value and Value.Value or error("Value '" .. k .."' does not exist in the EasyConfiguration")
+			end
+		end;
+		__newindex = function(self, k, NewValue)
+			local Value = self:GetValue(k)
+			Value.Value = NewValue
+		end;
+	}
 
-				if loweredValue == "add" or loweredValue == "addvalue" then
-					--[[ 
-					So they can do stuff like... Config.AddValue('IntValue', {
-						Name = "PizzaEaten"
-						Value = 0;
-					})
 
-					--]]
-					return function(valueType, values)
-						local Object
-
-						if values and values.Name and type(values.Name) == "string" then
-							Object = Get(values.Name)
-							if Object and Object.ClassName ~= valueType then
-								print("[EasyConfiguration] - Invalid class '"..Object.ClassName.."' in configuration, being replaced by new data '"..valueType.."'");
-								Object:Destroy()
-								Object = nil;
-							end
-						else
-							error("[EasyConfiguration] - No values received in the add method of easy configuration. Please give a table of default properties including the name. ", 2)
-						end
-
-						if not Object then
-							local newInstance = Instance.new(valueType)
-							Modify(newInstance, values)
-							newInstance.Parent = Configuration
-						end
-					end
-				elseif loweredValue == "get" or loweredValue == "getvalue" then
-					return function(name)
-						return Get(name)
-					end
-				elseif loweredValue == "getcontainer" then
-					return function(name)
-						return Get(name)
-					end
-				else
-					local Object = Configuration:FindFirstChild(value)
-					if Object and AcceptableTypes[Object.ClassName] then
-						return Object.Value
-					else
-						error("[EasyConfiguration] - " .. (Object and "Object '"..value.."' was a "..Type.getType(Object).." value, and not acceptable, so no return was given" or "Could not find Object '"..value.."' in the configuration"), 2)
-					end
-				end
-			end;
-			__newindex = function(_, value, newValue)
-				if type(value) == "string" then
-					local Object = Get(value)
-					if Object and AcceptableTypes[Object.ClassName] then
-						Object.Value = newValue;
-					else
-						error("[EasyConfiguration] - Value '" .. value .. "' was not accepted, or wasn't in the configuration", 2);
-					end
-				else
-					error("[EasyConfiguration] - Index with a string")
-				end
-			end;
-		})
-
-		EasyConfigCache[Configuration] = NewConfiguration
-		return NewConfiguration;
+	function MakeEasyConfiguration(Container)
+		return setmetatable({Container = Container}, Metatable)
 	end
 end
 
-lib.Make = MakeEasyConfiguration;
-lib.MakeEasyConfiguration = MakeEasyConfiguration;
+local function FindConfigurationOrCreateNewOne(ConfigurationName, Parent)
+	if Parent:FindFirstChild(ConfigurationName) then
+		return Parent[ConfigurationName]
+	else
+		local NewConfiguration = Instance.new("Configuration", Parent)
+		NewConfiguration.Name = ConfigurationName
 
-return lib
+		return NewConfiguration
+	end
+end
+
+return {MakeEasyConfiguration = MakeEasyConfiguration; AddSubDataLayer = FindConfigurationOrCreateNewOne}
+
+
+
+-- local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+-- local NevermoreEngine   = require(ReplicatedStorage:WaitForChild("NevermoreEngine"))
+-- local LoadCustomLibrary = NevermoreEngine.LoadLibrary
+
+-- local qSystems          = LoadCustomLibrary("qSystems")
+-- local Type              = LoadCustomLibrary("Type")
+
+-- local Make = qSystems.Make
+-- local Modify = qSystems.Modify
+
+-- -- EasyConfiguration.lua
+-- -- @author Quenty
+-- -- Last Modified February 3rd, 2014
+
+-- local lib = {}
+
+-- local AcceptableTypes = { -- Anything in here has a .Value property. 
+-- 	["StringValue"]         = true;
+-- 	["IntValue"]            = true;
+-- 	["NumberValue"]         = true;
+-- 	["BrickColorValue"]     = true;
+-- 	["BoolValue"]           = true;
+-- 	["Color3Value"]         = true;
+-- 	["Vector3Value"]        = true;
+-- 	["IntConstrainedValue"] = true;
+-- }
+
+-- local function TypeIsAcceptable(TypeName)
+-- 	return AcceptableTypes[TypeName]
+-- end
+-- lib.TypeIsAcceptable = TypeIsAcceptable;
+-- lib.typeIsAcceptable = TypeIsAcceptable
+
+-- local function AddSubDataLayer(DataName, Parent)
+-- 	-- For organization of data. Adds another configuration with the name "DataName", if one can't be found, and then returns it. 
+
+-- 	local DataContainer = Parent:FindFirstChild(DataName) or Make("Configuration", {
+-- 		Name = DataName;
+-- 		Parent = Parent;
+-- 		Archivable = true;
+-- 	})
+
+-- 	return DataContainer
+-- end
+-- lib.AddSubDataLayer = AddSubDataLayer
+-- lib.addSubDataLayer = AddSubDataLayer
+
+-- local EasyConfigCache = {}
+-- setmetatable(EasyConfigCache, {__mode = "k"})
+
+-- local function MakeEasyConfiguration(Configuration)
+-- 	if EasyConfigCache[Configuration] then
+-- 		return EasyConfigCache[Configuration]
+-- 	else
+-- 		local NewConfiguration = {}
+
+-- 		local function Get(ObjectName)
+-- 			return Configuration:FindFirstChild(ObjectName)
+-- 		end
+
+-- 		setmetatable(NewConfiguration, {
+-- 			__index = function(_, value)
+
+-- 				if not type(value) == "string" then
+-- 					error("Only Indexing with strings is supported with easyConfiguration", 2)
+-- 				end
+
+-- 				local loweredValue = value:lower()
+
+-- 				if loweredValue == "add" or loweredValue == "addvalue" then
+-- 					--[[ 
+-- 					So they can do stuff like... Config.AddValue('IntValue', {
+-- 						Name = "PizzaEaten"
+-- 						Value = 0;
+-- 					})
+
+-- 					--]]
+-- 					return function(valueType, values)
+-- 						local Object
+
+-- 						if values and values.Name and type(values.Name) == "string" then
+-- 							Object = Get(values.Name)
+-- 							if Object and Object.ClassName ~= valueType then
+-- 								print("[EasyConfiguration] - Invalid class '"..Object.ClassName.."' in configuration, being replaced by new data '"..valueType.."'");
+-- 								Object:Destroy()
+-- 								Object = nil;
+-- 							end
+-- 						else
+-- 							error("[EasyConfiguration] - No values received in the add method of easy configuration. Please give a table of default properties including the name. ", 2)
+-- 						end
+
+-- 						if not Object then
+-- 							local newInstance = Instance.new(valueType)
+-- 							Modify(newInstance, values)
+-- 							newInstance.Parent = Configuration
+-- 						end
+-- 					end
+-- 				elseif loweredValue == "get" or loweredValue == "getvalue" then
+-- 					return function(name)
+-- 						return Get(name)
+-- 					end
+-- 				elseif loweredValue == "getcontainer" then
+-- 					return function(name)
+-- 						return Get(name)
+-- 					end
+-- 				else
+-- 					local Object = Configuration:FindFirstChild(value)
+-- 					if Object and AcceptableTypes[Object.ClassName] then
+-- 						return Object.Value
+-- 					else
+-- 						error("[EasyConfiguration] - " .. (Object and "Object '"..value.."' was a "..Type.getType(Object).." value, and not acceptable, so no return was given" or "Could not find Object '"..value.."' in the configuration"), 2)
+-- 					end
+-- 				end
+-- 			end;
+-- 			__newindex = function(_, value, newValue)
+-- 				if type(value) == "string" then
+-- 					local Object = Get(value)
+-- 					if Object and AcceptableTypes[Object.ClassName] then
+-- 						Object.Value = newValue;
+-- 					else
+-- 						error("[EasyConfiguration] - Value '" .. value .. "' was not accepted, or wasn't in the configuration", 2);
+-- 					end
+-- 				else
+-- 					error("[EasyConfiguration] - Index with a string")
+-- 				end
+-- 			end;
+-- 		})
+
+-- 		EasyConfigCache[Configuration] = NewConfiguration
+-- 		return NewConfiguration;
+-- 	end
+-- end
+
+-- lib.Make = MakeEasyConfiguration;
+-- lib.MakeEasyConfiguration = MakeEasyConfiguration;
+
+-- return lib
