3e2831003b8a222e84d8b065c29ae6d5cf8391c3
Quenty <rbxquenty@gmail.com>
2015-05-05 18:25:00 -0500

    New time sync manager to sync between server and client. Replaces the UTCExact() method of qTime.

---
+++ Modules/Events/TimeSyncManager.lua
@@ -0,0 +1,167 @@
+local MasterClock = {}
+MasterClock.__index = MasterClock
+MasterClock.ClassName = "MasterClock"
+
+-- MasterClock sends time to client.
+
+function MasterClock.new(SyncEvent, DelayedRequestFunction)
+	local self = {}
+	setmetatable(self, MasterClock)
+	
+	self.SyncEvent = SyncEvent
+	self.DelayedRequestFunction = DelayedRequestFunction
+	
+	-- Define function calls and events.
+	function self.DelayedRequestFunction.OnServerInvoke(Player, TimeThree)
+		-- @return SlaveMasterDifference
+		
+		return self:HandleDelayRequest(TimeThree)
+	end
+	
+	self.SyncEvent.OnServerEvent:connect(function(Player)
+		 self.SyncEvent:FireClient(Player, self:GetTime())
+	end)
+	
+	-- Create thread.... forever! Yeah! That's right!
+	spawn(function()
+		while true do
+			wait(5)
+			self:Sync()
+		end
+	end)
+	
+	return self
+end
+
+function MasterClock:GetTime()
+	return workspace.DistributedGameTime
+end
+
+function MasterClock:Sync()
+	--- Starts the sync process with all slave clocks.
+	
+	local TimeOne = self:GetTime()
+	--print("[MasterClock] - Syncing all clients, TimeOne = ", TimeOne)
+	
+    self.SyncEvent:FireAllClients(TimeOne)
+end
+
+function MasterClock:HandleDelayRequest(TimeThree)
+    --- Client sends back message to get the SM_Difference.
+    -- @return SlaveMasterDifference
+
+    local TimeFour = self:GetTime()
+    return TimeFour - TimeThree -- -Offset + SM Delay
+end
+
+
+
+
+local SlaveClock = {}
+SlaveClock.__index = SlaveClock
+SlaveClock.ClassName = "SlaveClock"
+
+function SlaveClock.new(SyncEvent, DelayedRequestFunction)
+	local self = {}
+	setmetatable(self, SlaveClock)
+	
+	self.SyncEvent = SyncEvent
+	self.DelayedRequestFunction = DelayedRequestFunction
+	self.Offset = -1 -- Uncalculated.
+	
+	-- Connect
+	self.SyncEvent.OnClientEvent:connect(function(TimeOne)
+		self:HandleSyncEvent(TimeOne)
+	end)
+	
+	-- Request sync.
+	self.SyncEvent:FireServer()
+	
+	return self
+end
+
+function SlaveClock:GetLocalTime()
+    return workspace.DistributedGameTime
+end
+
+function SlaveClock:HandleSyncEvent(TimeOne)
+    -- http://www.nist.gov/el/isd/ieee/upload/tutorial-basic.pdf
+    -- We can't actually get hardware stuff, so we'll send T1 immediately. 
+    local TimeTwo = self:GetLocalTime()
+    local MasterSlaveDifference = TimeTwo - TimeOne -- We have Offst + MS Delay
+
+    -- wait(1)
+    local TimeThree = self:GetLocalTime()
+    local SlaveMasterDifference = self:SendDelayRequest(TimeThree)
+
+    --[[ From explination link.
+        The result is that we have the following two equations:
+        MS_difference = offset + MS delay
+        SM_difference = ?offset + SM delay
+
+        With two measured quantities:
+        MS_difference = 90 minutes
+        SM_difference = ?20 minutes
+
+        And three unknowns:
+        offset , MS delay, and SM delay
+
+        Rearrange the equations according to the tutorial.
+        -- Assuming this: MS delay = SM delay = one_way_delay
+
+        one_way_delay = (MSDelay + SMDelay) / 2
+    ]]
+
+    local Offset = (MasterSlaveDifference - SlaveMasterDifference)/2
+    local OneWayDelay = (MasterSlaveDifference + SlaveMasterDifference)/2
+
+	--print("[SlaveClock] - Synced time at ", self.Offset, "change of ", Offset - self.Offset, ". self:GetTime() = ", self:GetTime(), "OneWayDelay time @ ", OneWayDelay)
+
+    self.Offset = Offset -- Estimated difference between server/client
+    self.OneWayDelay = OneWayDelay -- Estimated time for network events to send. (MSDelay/SMDelay)
+end
+
+function SlaveClock:SendDelayRequest(TimeThree)
+	return self.DelayedRequestFunction:InvokeServer(TimeThree)
+end
+
+function SlaveClock:GetTime()
+	if self.Offset == -1 then
+		warn("[SlaveClock] - Offset is -1, may be unsynced clock!")
+	end
+	
+	return self:GetLocalTime() - self.Offset
+end
+
+
+--- Actual Construction --
+-- Determine what class to send back here: Singleton. 
+-- Usually I wouldn't do something as... badly designed... as this, but in this case
+-- I'm pretty sure these sync calls are expensive, so it's best that we do it here.
+
+local Players           = game:GetService("Players")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local NevermoreEngine   = require(ReplicatedStorage:WaitForChild("NevermoreEngine"))
+
+local SyncEvent = NevermoreEngine.GetRemoteEvent("TimeSyncEvent")
+local DelayedRequestFunction = NevermoreEngine.GetRemoteFunction("DelayedRequestEvent")
+local Manager
+
+if game:FindService("NetworkServer") == nil and game:FindService("NetworkClient") == nil then
+	-- Solo test mode
+	Manager = MasterClock.new(SyncEvent, DelayedRequestFunction)
+	
+	print("[TimeSyncManager] - SoloTestMode enabled. MasterClock constructed.")
+elseif Players.LocalPlayer then
+	-- Client
+	Manager = SlaveClock.new(SyncEvent, DelayedRequestFunction)
+	
+	print("[TimeSyncManager] - Client mode enabled. SlaveClock constructed.")
+else
+	-- Server
+	Manager = MasterClock.new(SyncEvent, DelayedRequestFunction)
+	
+	print("[TimeSyncManager] - Server mode enabled. MasterClock constructed.")
+end
+
+return Manager
