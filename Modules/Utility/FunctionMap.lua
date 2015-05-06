bfbc1c85b6ad5d9b4bfb6df3f3b8a271bda1bc50
Quenty <rbxquenty@gmail.com>
2015-05-05 22:08:26 -0500

    Add children to map, reformat API from tween --> map

--- Modules/Utility/FunctionMap.lua
+++ Modules/Utility/FunctionMap.lua
@@ -12,28 +12,39 @@ local function Map(ItemList, Function, ...)
 end
 
 
-local FunctionMap = {}
+local FunctionMap     = {}
 FunctionMap.ClassName = "FunctionMap"
-FunctionMap.__index = FunctionMap
+FunctionMap.__index   = FunctionMap
 
 function FunctionMap.new(Objects, MapFuncton)
 	-- @param Objects An array of objects to tween using the tween function
 	-- @param MapFuncton The function to use to tween the group. All
-	--        parameters passed via Tween are passed into this function,
+	--        parameters passed via Map are passed into this function,
 	--        plus the object it is working on
 
 	local self = {}
 	setmetatable(self, FunctionMap)
 
-	self.Objects       = Objects or error("No Objects")
+	self.Objects    = Objects or error("No Objects")
 	self.MapFuncton = MapFuncton or error("No MapFuncton")
+	self.Children   = {}
 
 	return self
 end
 
-function FunctionMap:Tween(...)
+function FunctionMap:AddChild(Child)
+	-- @param Child A function map that will also be called upon mapping
+
+	self.Children[Child] = true
+end
+
+function FunctionMap:Map(...)
 	--- Tweens all of the Objects using the MapFuncton
 	Map(self.Objects, self.MapFuncton, ...)
+
+	for ChildMap, _ in pairs(self.Children) do
+		ChildMap:Map(...)
+	end
 end
 
 return FunctionMap
