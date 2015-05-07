182326c14e01bfb852f30ce2b2cebf41186fbfde
Quenty <rbxquenty@gmail.com>
2015-05-07 14:52:43 -0500

    New EffectGroup class composites EffectFence

---
+++ Modules/qCFrame/Regions/EffectGroup.lua
@@ -0,0 +1,36 @@
+local EffectGroup = {}
+EffectGroup.__index = EffectGroup
+EffectGroup.ClassName = "EffectGroup"
+
+-- @author Quenty
+
+function EffectGroup.new()
+	local self = {}
+	setmetatable(self, EffectGroup)
+
+	self.Fences = {}
+
+	return self
+end
+
+function EffectGroup:AddEffectFence(Fence)
+	-- @param Fence An EffectFence to add to the EffectGroup
+	
+	self.Fences[#self.Fences+1] = Fence or error("No fence")
+end
+
+function EffectGroup:FindFirstActive(Point)
+	-- @return The fence that was used to cast.
+
+	assert(Point, "Need point to cast")
+
+	for _, Fence in pairs(self.Fences) do
+		if Fence.Region:CastPoint(Point) then
+			return Fence
+		end
+	end
+
+	return nil
+end
+
+return EffectGroup
