8312d2d42b19b6d4d174da9c9c30c0f4e15ac3ea
Quenty <rbxquenty@gmail.com>
2015-05-07 14:54:06 -0500

    Make the KeyIconProvider use a KeyIconBar

--- Modules/qGUI/KeyIcon/KeyIconProvider.lua
+++ Modules/qGUI/KeyIcon/KeyIconProvider.lua
@@ -30,13 +30,23 @@ KeyIconProvider.KeyCodeToText = {
 	-- [Enum.KeyCode.ButtonB] = "B";
 	-- [Enum.KeyCode.ButtonX] = "X";
 	-- [Enum.KeyCode.ButtonY] = "Y";
+	[Enum.KeyCode.LeftControl] = "CTRL";
+	[Enum.KeyCode.RightControl] = "CTRL";
 }
 
-function KeyIconProvider.new()
+function KeyIconProvider.new(DefaultIconBar)
+	-- Constructs for a specific DefaultIconBar, making the icon bar optional.
+
 	local self = {}
 	setmetatable(self, KeyIconProvider)
 
+	self.DefaultIconBar = DefaultIconBar or error("No DefaultIconBar")
+
 	return self
+end
+
+function KeyIconProvider:GetIconBar()
+	return self.DefaultIconBar
 end
 
 function KeyIconProvider:GetKeycodeText(KeyCode)
@@ -44,26 +54,35 @@ function KeyIconProvider:GetKeycodeText(KeyCode)
 	return self.KeyCodeToText[KeyCode] or KeyCode.Name
 end
 
-function KeyIconProvider:GetIcon(KeyIconBar, KeyCode)
+function KeyIconProvider:GetIcon(KeyCode, KeyIconBar)
 	--- Creates a new KeyIcon to fit the KeyIconBar.
-	-- @param KeyIconBar The icon bar to make the icon sized to. Considers the height.
 	-- @param KeyCode Enum, The KeyCode 
+	-- @param [KeyIconBar] The icon bar to make the icon sized to. Considers the height.
+
+	KeyIconBar       = KeyIconBar or self.DefaultIconBar or error("No KeyIconBar")
 
 	local Height     = KeyIconBar:GetHeight()
 	local Text       = self:GetKeycodeText(KeyCode):upper()
 	
 	local NewIcon    = KeyIcon.NewDefault(Height)
 	NewIcon.GUI.Text = Text
+	NewIcon:RescaleWidth()
 
 	return NewIcon
 end
 
-function KeyIconProvider:GetLabeledKey(KeyIconBar, KeyCode, LabelText)
+function KeyIconProvider:GetLabeledKey(KeyCode, LabelText, KeyIconBar)
 	-- Creates a new labeled key sized to the KeyIconBar
+	-- @param [KeyIconBar] The icon bar to make the icon sized to. Considers the height.
 
-	local NewIcon = self:GetIcon(KeyIconBar, KeyCode)
-	
-	return LabeledKeyIcon.FromKeyIcon(NewIcon, LabelText)
+	KeyIconBar = KeyIconBar or self.DefaultIconBar or error("No KeyIconBar")
+
+	local NewIcon = self:GetIcon(KeyCode, KeyIconBar)
+
+	local NewLabeledIcon = LabeledKeyIcon.FromKeyIcon(NewIcon, LabelText)
+	NewLabeledIcon.GUI.Name = KeyCode.Name .. "_" .. NewLabeledIcon.GUI.Name
+
+	return NewLabeledIcon
 end
 
 return KeyIconProvider
