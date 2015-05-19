ad00822d26027a2fe0287f048ceb09efcee96770
Quenty <rbxquenty@gmail.com>
2015-05-19 14:02:48 -0500

    Untested ripple UI element inspired by Material Design

---
+++ Modules/qGUI/PaperRipple/PaperRipple.lua
@@ -0,0 +1,316 @@
+local UserInputService   = game:GetService("UserInputService")
+local ReplicatedStorage  = game:GetService("ReplicatedStorage")
+local RunService         = game:GetService("RunService")
+
+local NevermoreEngine    = require(ReplicatedStorage:WaitForChild("NevermoreEngine"))
+local LoadCustomLibrary  = NevermoreEngine.LoadLibrary
+
+local Ripple   = LoadCustomLibrary("Ripple")
+local qSystems = LoadCustomLibrary("qSystems")
+local MakeMaid = LoadCustomLibrary("Maid").MakeMaid
+
+local GetIndexByValue = qSystems.GetIndexByValue
+
+--- Paper ripple from material design, based off of Polymer's algorithms.
+-- https://github.com/PolymerElements/paper-ripple/blob/master/paper-ripple.html
+-- @author Quenty
+
+local PaperRipple = {}
+PaperRipple.__index = PaperRipple
+PaperRipple.ClassName = "PaperRipple"
+PaperRipple.InkColor = Color3.new(1, 1, 1)
+PaperRipple.Recenter = false
+
+function PaperRipple.new(Container)
+	-- @param Container A container that clips descendants to parent
+	assert(Container.ClipsDescendants, "Container must clip descendants")
+
+	local self = setmetatable({}, PaperRipple)
+
+	-- COMPOSITE
+	self.InputMaid = MakeMaid()
+	self.Container = Container
+
+	-- PRIVATE
+	self.Ripples = {}
+	self.Animating = false
+
+	self:BindInput()
+
+	return self
+end
+
+function PaperRipple.FromParent(Parent)
+	--- Creates a new container parented to the parent, with a sufficient
+	--  construction.
+	-- @param Parent A ROBLOX GUI parent, will make sure that the
+	--               PaperRipple actually renders under it. Note there is a small 
+	--               restriction when it comes to rotation.
+	-- @return The new paper ripple.
+
+	assert(Parent, "Must send Parent GUI")
+
+	local Container                  = Instance.new("Frame")
+	Container.ClipsDescendants       = true;
+	Container.Archivable             = false;
+	Container.BorderSizePixel        = 0;
+	Container.BackgroundTransparency = 1;
+	Container.BackgroundColor3       = PaperRipple.InkColor
+	Container.Name                   = "PaperRipple";
+	Container.Size                   = UDim2.new(1, 0, 1, 0);
+	Container.ZIndex                 = math.min(Parent.ZIndex + 1, 10)
+	Container.Parent                 = Parent
+
+	return PaperRipple.new(Container)
+end
+
+function PaperRipple:SetInkColor(InkColor)
+	--- Sets the ink color for the ripple and recolors all the things!
+	-- @param InkColor Color3, the ink color to set the ripple
+	
+	self.InkColor = InkColor
+
+	for _, Item in pairs(self.Ripples) do
+		Item:SetInkColor(self.InkColor)
+	end
+	self.Container.BackgroundColor3 = self.InkColor
+end
+
+function PaperRipple:SetRecenter(DoRecenter)
+	--- Should the ripples recenter itself as stuff happens? 
+	-- @param DoRecenter Boolean, if true, will recenter.
+
+	assert(type(DoRecenter) == "boolean", "DoRecenter must be a boolean. (kids these days).")
+
+	self.Recenter = DoRecenter
+end
+
+function PaperRipple:FurthestCornerDistanceFrom(Position)
+	-- @param Position Vector2 world position on the GUI.
+	-- @return Number, the further distance from the corner.
+	
+	local Container = self.Container
+	
+	local ContainerPosition = Container.AbsolutePosition
+	local ContainerSize = Container.AbsoluteSize 
+
+	-- Magnitude distance of each position
+	local TopLeft = (ContainerPosition - Position).magnitude
+	local TopRight = (ContainerPosition + Vector2.new(0, ContainerSize.X) - Position).magnitude
+	local BottomLeft = (ContainerPosition + Vector2.new(ContainerSize.Y, 0) - Position).magnitude
+	local BottomRight = (ContainerPosition + ContainerSize - Position).magnitude
+
+	-- Selection
+	return math.max(TopLeft, TopRight, BottomLeft, BottomRight)
+end
+
+function PaperRipple:ReleaseRipples()
+	--- Releases each ripple for mouse down, so they expand all the way and fade.
+
+	for _, Ripple in pairs(self.Ripples) do
+		Ripple:Up()
+	end
+end
+
+function PaperRipple:AddRipple(NewRipple)
+	--- Adds a new ripple to the processing list. Used internally.
+	--  Also calls :Down() to begin animation.
+
+	self.Ripples[#self.Ripples+1] = NewRipple or error("No ripple sent")
+
+	NewRipple:Down()
+	self:BeginAnimating()
+end
+
+function PaperRipple:RemoveRipple(Ripple)
+	--- Removes the ripple from the processing list. Used internally.
+	--  This removal also GCs the Ripple's GUI.s
+	--  @param Ripple An active ripple in the list to remove. 
+
+	assert(Ripple, "Must send ripple")
+
+	local Index = GetIndexByValue(self.Ripples, Ripple) or error("Ripple does not exist")
+	Ripple:Destroy()
+
+	table.remove(self.Ripples, Index)
+end
+
+
+
+-- ANIMATION BINDING
+function PaperRipple:Draw()
+	--- Updates the animations on each of the ripples, GCing as needed
+	--  Called internally by the animation binding.
+
+	local Index = 1
+	while Index <= #self.Ripples do
+		local Ripples = self.Ripples[Index]
+
+		if Ripple:IsAnimationComplete() then
+			-- Remove completed ripples.
+
+			self:RemoveRipple(Ripple)
+		else
+			Ripple:Draw()
+			self.Container.Transparency = Ripple:GetOuterTransparency()
+			Index = Index + 1 -- Only increment the ripple when we didn't remove a ripple.
+		end
+	end
+end
+
+function PaperRipple:GetBindName()
+	--- Binding to render step requires a unique UID.
+	-- @return String The name being used to bind the ripple to RenderStep
+
+	return "PaperRipple" .. tostring(self)
+end
+
+function PaperRipple:StopAnimating()
+	--- Private, stops the animation of the paper ripple.
+	-- @pre There are no ripples in the array self.Ripples.
+
+	assert(#self.Ripples == 0, "There are still ripples to process.")
+
+	if self.Animating then
+		RunService:UnbindFromRenderStep(self:GetBindName())
+		self.Animating = false
+	end
+
+	self.Container.Transparency = 1
+end
+
+function PaperRipple:BeginAnimating()
+	--- If not animating, begins the animation process of animating.
+
+	if not self.Animating then
+		self.Animating = true
+
+		RunService:BindToRenderStep(self:GetBindName(), 2000, function()
+			self:Draw()
+
+			if #self.Ripples <= 0 or not self.Animating then
+				self:StopAnimating()
+			end
+		end)
+	end
+end
+
+
+
+
+-- INPUT BINDING
+function PaperRipple:ConstructNewRipple(Position)
+	--- Creates a new ripple for use in the Down function. Possible for this
+	--  to be overridden.
+	-- @param [Position] Vector2 position value world space.
+	-- @return The new ripple
+
+	-- No position? SEt to center.
+	Position = Position or (self.Container.AbsolutePosition + self.Container.AbsoluteSize/2)
+
+	local Radius = self:FurthestCornerDistanceFrom()
+
+	local NewRipple = Ripple.FromPosition(self.Container, Position) 
+	NewRipple:SetInkColor(self.InkColor)
+	NewRipple:SetTargetRadius(Radius)
+
+	if self.Recenter then
+		NewRipple:TargetCenter()
+	end
+
+	return NewRipple
+end
+
+function PaperRipple:Down(Position)
+	--- Handles a new ripple. Public function. Also bound to input.
+	-- @param [Position] Vector2 position value world space.
+	-- @return The new ripple
+
+	self:ReleaseRipples() -- Release current ripples...
+	local NewRipple = 
+	self:AddRipple(NewRipple)
+
+	return NewRipple
+end
+
+function PaperRipple:Up()
+	--- Handles input being released, which is basically just releasing all the ripples. 
+
+	self:ReleaseRipples()
+	self.InputMaid.InputEnded = nil
+end
+
+function PaperRipple:BindInput()
+	--- Binds the input to the InputMaid to detect/handle Touch,
+	--  and mouse button inputs over the GUI in question. Will override
+	--  old bindings with the same names.
+
+	local ValidInputEnums = {}
+	for Index, EnumName in pairs({"Touch", "MouseButton1", "MouseButton2", "MouseButton3"}) do
+		ValidInputEnums[Enum.UserInputType[EnumName]] = true
+	end
+
+	-- The reason we bind twice here instead of using the InputChanged is because this 
+	-- event fires everytime a mouse moves over the InputMaid.
+
+	-- Why do we track the DownTypes? Well, if we have a MouseButton2 down, and then a MouseButton1 down on
+	-- another GUI, which then goes up, then we don't want to release the hold. However, if that MouseButton1 goes
+	-- down on the same GUI (thus triggering a reflow of ink), we need that input type (or the other one, MouseButton2)
+	-- to flow up and count. Furthermore, if we have a MouseDown on the GUI, and then the mouse slides off, we need to 
+	-- still have mouse up events track the flow, so we need to bind to the UserInputService to get this sort of data.
+
+	local DownTypes = {}
+
+	local function OnUp(InputObject)
+		-- @param InputObject The input object to handle an up event. Will only
+		--        consider a valid "up" if the type is already down.
+
+		if DownTypes[InputObject.UserInputType] then
+			self:Up()
+			DownTypes = {} -- Clear state.
+		end
+	end
+
+	self.InputMaid.InputBegan = self.Container.InputBegan:connect(function(InputObject)
+		if ValidInputEnums[InputObject.UserInputType] then
+			local Position = Vector2.new(InputObject.Position.X, InputObject.Position.Y)
+			self:Down(Position)
+
+			DownTypes[InputObject.UserInputType] = true
+			
+			-- Bind to event.
+			self.InputMaid.InputEnded = UserInputService.InputEnded:connect(OnUp)
+		end
+	end)
+
+	--[[
+	self.InputMaid.InputEnded = self.Gui.InputEnded:connect(function(InputObject)
+		if ValidInputEnums[InputObject.UserInputType] then
+			self:Up()
+		end
+	end)--]]
+end
+
+function PaperRipple:Destroy()
+	--- Destroys the ripple, and it's container GUI for easy
+	--  GC on dynamically created stuff. Also disconnects all
+	--  events in the offchance that somehow we
+	--  didn't do that well either.
+
+	-- Remove all ripples.
+	while #self.Ripples > 0 do
+		self:RemoveRipple(self.Ripples[1] or error("No ripple?"))
+	end
+
+	self:StopAnimating()
+
+	self.Container:Destroy()
+	self.Container = nil
+
+	self.InputMaid:DoCleaning()
+	self.InputMaid = nil
+
+	setmetatable(self, nil)
+end
+
+return PaperRipple
---
+++ Modules/qGUI/PaperRipple/Ripple.lua
@@ -0,0 +1,265 @@
+local Ripple = {}
+Ripple.__index = Ripple
+Ripple.ClassName = "Ripple"
+Ripple.MaxRadius = 300 -- Absolute maximum
+Ripple.TransparencyDecayVelocity = 0.8
+
+-- @author Quenty
+-- A Ripple animation to be used as part of PaperRipple. 
+-- Based off of Polymer's design and Google material design, designed for ROBLOX GUIs
+-- https://github.com/PolymerElements/paper-ripple/blob/master/paper-ripple.html
+
+function Ripple.new(Gui)
+	--- Creates a new ripple, a wave of sort that expands to its target position and then decays
+	--  past that on a secondary event.
+	-- @param Gui An ImageLabel or ImageButton
+
+	assert(Gui and (Gui:IsA("ImageLabel") or Gui:IsA("ImageButton")), "GUI must be an ImageLabel or ImageButton")
+
+	local self = setmetatable({}, Ripple)
+
+	-- COMPOSITION
+	self.Gui = Gui
+	self.Container = self.Gui.Parent or error("No container")
+
+	-- PROPERTIES
+	self.InitialPosition = self.Container.AbsoluteSize / 2
+	self.TargetPosition = self.InitialPosition
+
+	self.InitialTransparency = Gui.ImageTransparency
+	self.TargetRadius = 20
+
+	-- NOTE: In order to begin, one should call ":Down()"
+	self:Draw()
+
+	return self
+end
+
+function Ripple.NewDefault(Parent)
+	--- Creates a new "default" ripple 
+	-- @param Parent A ROBLOX GUI to parent the ripple to.
+
+	local Circle                  = Instance.new("ImageLabel");
+	Circle.Image                  = "http://www.roblox.com/asset/?id=172318712"
+	Circle.Name                   = "Circle";
+	Circle.ImageTransparency      = 0.75;
+	Circle.BackgroundTransparency = 1;
+	Circle.BorderSizePixel        = 0;
+	Circle.Archivable             = false;
+	Circle.ZIndex                 = math.min(Parent.ZIndex + 1, 10)
+	Circle.ImageColor3            = Color3.new(1, 1, 1)
+	Circle.Parent                 = Parent or error("Must send parent")
+
+	local NewRipple = Ripple.new(Circle)
+
+	return NewRipple
+end
+
+function Ripple.FromPosition(Parent, Position)
+	--- Creates a ripple from at a worldspace position 
+	-- @param Parent A ROBLOX GUI to parent the ripple to.
+	-- @param Position Vector2 world space (2D) vector.
+
+	local NewRipple = Ripple.NewDefault()
+
+	local RelativePosition = Position - NewRipple:GetContainer().AbsolutePosition
+	NewRipple:SetInitialPosition(RelativePosition)
+
+	return NewRipple
+end
+
+function Ripple:SetInitialPosition(Position)
+	--- Sets the initial position of the ripple.
+	-- @param Position Vector2, relative local space position in offset.
+
+	self.InitialPosition = Position
+end
+
+function Ripple:SetTargetPosition(Position)
+	--- Sets the target position of the ripple.
+	-- @param Position Vector2, relative local space in offset to target.s
+
+	self.TargetPosition = Position
+end
+
+function Ripple:TargetCenter()
+	--- Makes the ripple target the center of the container.
+
+	self:SetTargetPosition(self.Container.AbsoluteSize / 2)
+end
+
+function Ripple:GetContainer()
+	--- Returns the container of the ripple.
+	-- @return The GUI container of the ripple. 
+
+	return self.Container
+end
+
+function Ripple:SetInkColor(Color)
+	--- Sets the ink color (of the ripple). Assuming the image is white w/ 0 alpha to get best results
+	-- @param Color A Color3 to set the ink to.
+
+	self.Gui.ImageColor3 = Color or error("No InkColor. This is the deepest betrayal. How dare you.")
+end
+
+function Ripple:SetTargetRadius(Radius)
+	-- If TargetRadius is greater than MaxRadius, it may not fill out all the way (up to max radius)
+	-- @param Radius Number, the radius to target. Shouldn't change after animating. 
+
+	assert(type(Radius) == "number", "Radius must be a number")
+	assert(Radius > 5, "Radius failed sanity check")
+
+	self.TargetRadius = Radius
+end
+
+function Ripple:GetMouseUpElapsed()
+	-- @return The time elapsed since the mouse was up. 0 if it hasn't gone up yet.
+
+	return self.MouseUpStart and (tick() - self.MouseUpStart) or 0
+end
+
+function Ripple:GetMouseDownElapsed()
+	-- @return The time (in seconds) elapsed since the mouse went down. 0 if it hasn't gone down.
+
+	return self.MouseDownStart and (tick() - self.MouseUpStart) or 0
+end
+
+function Ripple:GetMouseInteractionElapsed()
+	return self:GetMouseDownElapsed() + self:GetMouseDownElapsed()
+end
+
+---[[]
+function Ripple:GetOuterTransparency()
+	-- Components usually have an outer transparency set (darkening) too.
+	-- This is a linear background transparency capped at the wave front
+	
+	local OuterTransparency = 1 - (self:GetMouseUpElapsed() * 0.3)
+	local WaveTransparency = self:GetTransparency()
+
+	return math.max(
+		1, 
+		math.max(OuterTransparency, WaveTransparency)
+	)
+end--]]
+
+function Ripple:GetTransparency()
+	-- @return The transparency of the ripple.
+
+	if not self.MouseUpStart then
+		return self.InitialTransparency
+	else
+		-- Inversed from the Polymer version because the opacity vs. transparency deal.
+		return math.min(
+			1,
+			self.InitialTransparency + self:GetMouseUpElapsed() * self.TransparencyDecayVelocity
+		)
+	end
+end
+
+function Ripple:GetRadius()
+	--- Does some fancy formula to calculate radius based on time elapsed.s. <3
+	-- @return Radius based on mouse interaction time
+	-- https://github.com/PolymerElements/paper-ripple/blob/master/paper-ripple.html#L263
+
+	local WaveRadius = math.min(
+		self.Container.AbsoluteSize.magnitude,
+		self.MaxRadius
+	) * 1.1 + 5
+
+	-- Formula from PaperRipple on Polymer's project
+	local Duration = 1.1 - 0.2 * (WaveRadius / Ripple.MaxRadius)
+	local PercentElapsed = self:GetMouseInteractionElapsed() / Duration
+	local Size = WaveRadius * (1 - 80^(-PercentElapsed))
+
+	return math.abs(Size)
+end
+
+function Ripple:IsTransparencyDecayed()
+	-- Returns whether the transparency has completely decayed. This does
+	-- not count if the ripple has not started, thus, we also check
+	-- the radius.
+	-- @return Boolean, true if the transparency is completely decayed.
+
+	return self:GetTransparency() >= 0.99 and 
+		self:GetRadius() >= math.min(self.MaxRadius, self.TargetRadius)
+end
+
+function Ripple:GetTranslationFraction()
+	--- Calculates how far along we've translated the animation
+	--  @return Number [0, 1] of how far along we've translated.
+
+	return math.min(
+		1, 
+		self:GetRadius() / self:GetLargestContainerSize() * 2 / math.sqrt(2)
+	)
+end
+
+--[[
+function Ripple:IsRestingAtMaxRadius()
+	-- Transparency start? 0.75. End? 1.
+
+	return self:GetTransparency() < self.InitialTransparency and
+		self:GetRadius() >= math.min(self.MaxRadius, self.TargetRadius)
+end--]]
+
+function Ripple:IsAnimationComplete()
+	-- Not sure on the logic here, trusting it works. Basically, to be done,
+	-- we need to have the mouse up, and the transparency decayed. 
+
+	return self.MouseUpStart and self:IsTransparencyDecayed() --or self:IsRestingAtMaxRadius()
+end
+
+function Ripple:Down()
+	self.MouseDownStart = tick()
+end
+
+function Ripple:GetLargestContainerSize()
+	--- Determines the larger of two container sizes
+	-- @return Number, the larger size.
+	
+	local Container = self.Container
+	
+	return math.max(Container.AbsoluteSize.X, Container.AbsoluteSize.Y)
+end
+
+function Ripple:Draw()
+	--- Updates the ripple draw state. :)
+	
+	local Gui = self.Gui
+	local Container = self.Container
+
+	Gui.Transparency = self:GetTransparency()
+
+	local Radius = self:GetRadius()
+	local ContainerLargerSize = self:GetLargestContainerSize()
+	local Scale  = Radius / (ContainerLargerSize / 2)
+
+	Gui.Size = UDim2.new(Scale, 0, Scale, 0)
+
+	local Offset = self.TargetPosition - self.InitialPosition
+	local Position = self.InitialPosition + Offset * self:GetTranslationFraction()
+	local RadiusOffset = Vector2.new(Radius, Radius)
+	local RenderPosition = Position - RadiusOffset
+
+	Gui.Position = UDim2.new(0, RenderPosition.X, 0, RenderPosition.Y) 
+end
+
+function Ripple:Up()
+	-- Releases the ripple. May be callled multiple times, but will only perform
+	-- once. 
+
+	if not self.MouseUpStart then
+		self.MouseUpStart = tick()
+	end
+end
+
+function Ripple:Destroy()
+	-- GCs the GUI
+
+	setmetatable(self, nil)
+
+	self.Gui:Destroy()
+	self.Gui = nil
+end
+
+return Ripple
