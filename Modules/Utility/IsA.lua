063fbf052e9c440a68605c3a69d1225e02dd353c
Quenty <rbxquenty@gmail.com>
2015-05-19 14:02:02 -0500

    IsA method to allow for class IsA functions. Generic.

---
+++ Modules/Utility/IsA.lua
@@ -0,0 +1,33 @@
+local lib = {}
+
+-- @author Quenty
+-- Generic IsA interface for Lua classes. 
+
+local function IsA(self, ClassName)
+	--- Using the .ClassName property, returns whether or not a component is 
+	--  a class
+
+	assert(type(ClassName) == "string", "ClassName must be a string")
+	
+	local CurrentMetatable = getmetatable(self)
+	while CurrentMetatable do
+		if CurrentMetatable.ClassName == ClassName then
+			return true
+		end
+		CurrentMetatable = getmetatable(CurrentMetatable)
+	end
+	
+	return false
+end
+
+local function AddTo(Class)
+	--- Adds the IsA function to a class and all descendants
+
+	assert(not Class.IsA, "Class already has an IsA method")
+	assert(Class.ClassName, "Class needs a ClassName")
+
+	Class.IsA = IsA
+end
+lib.AddTo = AddTo
+
+return lib
