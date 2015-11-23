05ffcb1e1e5ead86081fdfcbba9d9795d86a86a3
Quenty <rbxquenty@gmail.com>
2015-11-23 17:54:00 -0600

    Inverse fader for interpolations

---
+++ Modules/Camera/Effects/InverseFader.lua
@@ -0,0 +1,35 @@
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+local NevermoreEngine   = require(ReplicatedStorage:WaitForChild("NevermoreEngine"))
+local LoadCustomLibrary = NevermoreEngine.LoadLibrary
+
+local CameraState       = LoadCustomLibrary("CameraState")
+local SummedCamera      = LoadCustomLibrary("SummedCamera")
+
+local InverseFader = {}
+InverseFader.ClassName = "InverseFader"
+
+-- Intent: Be the inverse of a fading camera (makes scaling in cameras easy).
+
+function InverseFader.new(Camera, Fader)
+	local self = setmetatable({}, InverseFader)
+
+	self.Camera = Camera or error()
+	self.Fader = Fader or error()
+
+	return self
+end
+
+function InverseFader:__add(Other)
+	return SummedCamera.new(self, Other)
+end
+
+function InverseFader:__index(Index)
+	if Index == "State" or Index == "CameraState" or Index == "Camera" then
+		return (self.Camera.CameraState or self.Camera)*(1-self.Fader.Value)
+	else
+		return InverseFader[Index]
+	end
+end
+
+return InverseFader
