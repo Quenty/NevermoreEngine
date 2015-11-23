8e2cb46854bcbaca987a44697737ce5827027f45
Quenty <rbxquenty@gmail.com>
2015-11-23 17:53:51 -0600

    New API

--- Modules/Camera/Effects/FadingCamera.lua
+++ Modules/Camera/Effects/FadingCamera.lua
@@ -5,7 +5,7 @@ local LoadCustomLibrary = NevermoreEngine.LoadLibrary
 
 local CameraState       = LoadCustomLibrary("CameraState")
 local SpringPhysics     = LoadCustomLibrary("SpringPhysics")
-local SummedCamera       = LoadCustomLibrary("SummedCamera")
+local SummedCamera      = LoadCustomLibrary("SummedCamera")
 
 
 -- Intent: Add another layer of effects that can be faded in/out
@@ -20,18 +20,16 @@ function FadingCamera.new(Camera)
 
 	self.Camera = Camera or error("No camera")
 	self.Damper = 1
-	self.Speed = 20
+	self.Speed = 15
 
 	return self
-end
-
-function FadingCamera:__add(Other)
-	return SummedCamera.new(self, Other)
 end
 
 function FadingCamera:__newindex(Index, Value)
 	if Index == "Damper" then
 		self.Spring.Damper = Value
+	elseif Index == "Value" then
+		self.Spring.Value = Value
 	elseif Index == "Speed" then
 		self.Spring.Speed = Value
 	elseif Index == "Target" then
@@ -45,13 +43,20 @@ end
 
 function FadingCamera:__index(Index)
 	if Index == "State" or Index == "CameraState" or Index == "Camera" then
+		-- print("Spring value", self.Spring.Value)
 		return (self.Camera.CameraState or self.Camera) * self.Spring.Value
 	elseif Index == "Damper" then
 		return self.Spring.Damper
+	elseif Index == "Value" then
+		return self.Spring.Value
 	elseif Index == "Speed" then
 		return self.Spring.Speed
 	elseif Index == "Target" then
 		return self.Spring.Target
+	elseif Index == "Velocity" then
+		return self.Spring.Velocity
+	elseif Index == "HasReachedTarget" then
+		return math.abs(self.Value - self.Target) < 1e-4 and math.abs(self.Velocity) < 1e-4
 	else
 		return FadingCamera[Index]
 	end
