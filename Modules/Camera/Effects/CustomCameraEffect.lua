1dfb0f765d7c3d338ded358823cd58b5eb26e0bc
Quenty <rbxquenty@gmail.com>
2015-11-23 17:54:46 -0600

    Overridable camera effect for cutscenes

---
+++ Modules/Camera/Effects/CustomCameraEffect.lua
@@ -0,0 +1,34 @@
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+local NevermoreEngine   = require(ReplicatedStorage:WaitForChild("NevermoreEngine"))
+local LoadCustomLibrary = NevermoreEngine.LoadLibrary
+
+local CameraState       = LoadCustomLibrary("CameraState")
+local SummedCamera      = LoadCustomLibrary("SummedCamera")
+
+-- For those barely used effects...
+
+local CustomCameraEffect = {}
+CustomCameraEffect.ClassName = "CustomCameraEffect"
+
+function CustomCameraEffect.new(RetrieveState)
+	local self = setmetatable({}, CustomCameraEffect)
+
+	self.RetrieveState = RetrieveState or error()
+
+	return self
+end
+
+function CustomCameraEffect:__add(Other)
+	return SummedCamera.new(self, Other)
+end
+
+function CustomCameraEffect:__index(Index)
+	if Index == "State" or Index == "CameraState" or Index == "Camera" then
+		return self.RetrieveState()
+	else
+		return CustomCameraEffect[Index]
+	end
+end
+
+return CustomCameraEffect
