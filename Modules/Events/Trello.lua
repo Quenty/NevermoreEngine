903d351aed91206cf611e763ffe4cbf774f601e4
Narrev <NarrevRBX@gmail.com>
2016-02-07 01:04:17 -0600

    Update Trello.lua

--- Modules/Events/Trello.lua
+++ Modules/Events/Trello.lua
@@ -4,30 +4,13 @@ local LoadCustomLibrary	= require(game:GetService("ReplicatedStorage"):WaitForCh
 local ModRemote			= LoadCustomLibrary("ModRemote")
 
 if game:GetService("RunService"):IsServer() then -- Server
-	return function(trelloUsername, boardName, listName, key, token)
-		local listId
+	return function(tab)
+		local trelloUsername, boardName, listName, key, token = tab.trelloUsername, tab.boardName, tab.listName, tab.key, tab.token
 		local trelloClientBridge = ModRemote:CreateEvent("TrelloClientBridge") -- RemoteFunction for logging Client errors too
-	
-		local function POSTError(isClient, message, stackTrace)
-			while not listId do -- Wait until listId has been defined
-				print('waiting for listId '.. tostring(listId))
-				wait(0.03)
-			end
-			print(listId)
-			HttpService:PostAsync("https://api.trello.com/1/cards?key=" .. key .. "&token=" .. token, HttpService:JSONEncode({
-				name = (isClient and "Client" or "Server") .. " Error " .. message;
-				desc = stackTrace or "";
-				labels = isClient and "blue" or "red";
-				idList = listId;
-			}))
-		end
-	
-		trelloClientBridge:Listen(			   function(player, ...) POSTError(true, ...) end) -- Hookup Client
-		game:GetService("ScriptContext").Error:connect(function(...) POSTError(false, ...) end) -- Hookup Server
-	
-		listId = (function(username, boardName, listName)
+		
+		local listId = (function()
 			local boards = HttpService:JSONDecode(HttpService:GetAsync("https://trello.com/1/members/" .. trelloUsername:lower() .. "/boards"))
-
+			
 			for a = 1, #boards do
 				if boards[a].name == boardName then
 					local lists = HttpService:JSONDecode(HttpService:GetAsync("http://trello.com/1/boards/" .. boards[a].id .. "/lists"))
@@ -40,10 +23,21 @@ if game:GetService("RunService"):IsServer() then -- Server
 				end
 			end
 			error("No board found with name " .. boardName)
+		end)()
 
-		end)(trelloUsername, boardName, listName)
+		local function POSTError(isClient, message, stackTrace)
+			HttpService:PostAsync("https://api.trello.com/1/cards?key=" .. key .. "&token=" .. token, HttpService:JSONEncode({
+				name = (isClient and "Client" or "Server") .. " Error " .. message;
+				desc = stackTrace or "";
+				labels = isClient and "blue" or "red";
+				idList = listId;
+			}))
+		end
+	
+		trelloClientBridge:Listen(function(player, ...) POSTError(true, ...) end) -- Hookup Client
+		game:GetService("ScriptContext").Error:connect(function(...) POSTError(false, ...) end) -- Hookup Server
 	end
-else -- Client
+else
 	local trelloClientBridge = ModRemote:GetEvent("TrelloClientBridge")
 	game:GetService("ScriptContext").Error:connect(function(...) trelloClientBridge:SendToServer(...) end)
 end
