03df54f1a3991ce1c784d4402cdce4955b773438
Narrev <NarrevRBX@gmail.com>
2016-02-06 21:25:02 -0600

    Update Signal.lua

--- Modules/Events/Signal.lua
+++ Modules/Events/Signal.lua
@@ -1,41 +1,40 @@
 -- Credit to Stravant
 
-local Signal = {}
+return {
+	new = function()
+		local sig = {}
+		
+		local mSignaler = Instance.new('BindableEvent')
+		
+		local mArgData = nil
+		local mArgDataCount = nil
+		
+		function sig:fire(...)
+			mArgData = {...}
+			mArgDataCount = select('#', ...)
+			mSignaler:Fire()
+		end
+		
+		function sig:connect(f)
+			if not f then error("connect(nil)", 2) end
+			return mSignaler.Event:connect(function()
+				f(unpack(mArgData, 1, mArgDataCount))
+			end)
+		end
+		
+		function sig:wait()
+			mSignaler.Event:wait()
+			assert(mArgData, "Missing arg data, likely due to :TweenSize/Position corrupting threadrefs.")
+			return unpack(mArgData, 1, mArgDataCount)
+		end
 
-function Signal.new()
-	local sig = {}
-	
-	local mSignaler = Instance.new('BindableEvent')
-	
-	local mArgData, mArgDataCount
-	
-	function sig:fire(...)
-		mArgData = {...}
-		mArgDataCount = select('#', ...)
-		mSignaler:Fire()
+		function sig:Destroy()
+			mSignaler:Destroy()
+			mArgData      = nil
+			mArgDataCount = nil
+			mSignaler     = nil
+		end
+		
+		return sig
 	end
-	
-	function sig:connect(f)
-		if not f then error("connect(nil)", 2) end
-		return mSignaler.Event:connect(function()
-			f(unpack(mArgData, 1, mArgDataCount))
-		end)
-	end
-	
-	function sig:wait()
-		mSignaler.Event:wait()
-		assert(mArgData, "Missing arg data, likely due to :TweenSize/Position corrupting threadrefs.")
-		return unpack(mArgData, 1, mArgDataCount)
-	end
-
-	function sig:Destroy()
-		mSignaler:Destroy()
-		mArgData      = nil
-		mArgDataCount = nil
-		mSignaler     = nil
-	end
-	
-	return sig
-end
-
-return Signal
+}
