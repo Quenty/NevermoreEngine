3bac452ad465058229234535c84b1a1cd5c31be1
Narrev <NarrevRBX@gmail.com>
2016-04-10 03:46:32 -0500

    Update Character.lua

--- Modules/Character/Character.lua
+++ Modules/Character/Character.lua
@@ -1,30 +1,20 @@
+-- Character.lua
+-- This library handles making sure it is safe to use RawCharacter functions. 
+-- @author Quenty
+
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local LoadCustomLibrary = require(ReplicatedStorage:WaitForChild("NevermoreEngine"))
 
-local NevermoreEngine   = require(ReplicatedStorage:WaitForChild("NevermoreEngine"))
-local LoadCustomLibrary = NevermoreEngine.LoadLibrary
-
-local RawCharacter      = LoadCustomLibrary("RawCharacter")
 local qSystems          = LoadCustomLibrary("qSystems")
 local Type              = LoadCustomLibrary("Type")
 
-local CheckCharacter = qSystems.CheckCharacter
+local CheckCharacter    = qSystems.CheckCharacter
 
 local safeLib = {}
 
---- This library handles making sure it is safe to use RawCharacter functions. 
--- @author Quenty
--- Last modified January 14th, 2013
-
---[[-- Change Log --
-
-January 19th, 2014
-- Updated to include output parser
-
---]]
-
-for functionName, libraryItem in pairs(RawCharacter) do
+for functionName, libraryItem in pairs(LoadCustomLibrary("RawCharacter")) do
 	if type(functionName) ~= "string" then
-		error("[Character] - functionName '"..tostring(functionName).."' a '"..Type.GetType(functionName).."' value should be a string")
+		error("[Character] - functionName '"..tostring(functionName).."' a '"..Type(functionName).."' value should be a string")
 	end
 	if type(libraryItem) == "function" and functionName:lower() ~= "import" then
 		safeLib[functionName] = function(character, ...) 
