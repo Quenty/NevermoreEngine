45d7cb8f0c9c1deb3ab033b897094be7c771f93e
Quenty <rbxquenty@gmail.com>
2016-04-10 14:21:31 -0500

    Animation playing system for ROBLOX animations

---
+++ Modules/ROBLOXAnimations/AnimationPlayer.lua
@@ -0,0 +1,91 @@
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+local NevermoreEngine   = require(ReplicatedStorage:WaitForChild("NevermoreEngine"))
+local LoadCustomLibrary = NevermoreEngine.LoadLibrary
+
+local Signal = LoadCustomLibrary("Signal")
+
+local AnimationPlayer = {}
+AnimationPlayer.__index = AnimationPlayer
+AnimationPlayer.ClassName = "AnimationPlayer"
+
+-- Intent: Makes playing and loading tracks into a humanoid easy.
+-- @author Quenty
+
+function AnimationPlayer.new(Humanoid)
+	local self = setmetatable({}, AnimationPlayer)
+	
+	self.Humanoid = Humanoid or error("No Humanoid")
+	self.Tracks = {}
+	self.FadeTime = 0.4 -- Default
+	
+	self.TrackPlayed = Signal.new()
+
+	return self
+end
+
+function AnimationPlayer:WithAnimation(Animation)
+	self.Tracks[Animation.Name] = self.Humanoid:LoadAnimation(Animation)
+
+	return self
+end
+
+function AnimationPlayer:AddAnimation(Name, AnimationID)
+	local Animation = Instance.new("Animation")
+	Animation.AnimationId = "http://www.roblox.com/Asset?ID=" .. tonumber(AnimationID) or error("No AnimationID")
+	Animation.Name = Name or error("No name")
+	
+	return self:WithAnimation(Animation)
+end
+
+function AnimationPlayer:GetTrack(TrackName)
+	return self.Tracks[TrackName] or error("Track does not exist")
+end
+
+
+function AnimationPlayer:PlayTrack(TrackName, FadeTime, Weight, Speed, StopFadeTime)
+	-- @param FadeTime How much time it will take to transition into the animation.	
+	-- @param Weight Acts as a multiplier for the offsets and rotations of the playing animation
+		-- This parameter is extremely unstable. 
+		-- Any parameter higher than 1.5 will result in very shaky motion, and any parameter higher '
+		-- than 2 will almost always result in NAN errors. Use with caution.
+	-- @param Speed The time scale of the animation.	
+		-- Setting this to 2 will make the animation 2x faster, and setting it to 0.5 will make it 
+		-- run 2x slower.
+	
+	FadeTime = FadeTime or self.FadeTime
+	local Track = self:GetTrack(TrackName)
+	
+	self.TrackPlayed:fire(TrackName, FadeTime, Weight, Speed, StopFadeTime)
+
+	if not Track.IsPlaying then
+		
+		self:StopAllTracks(StopFadeTime or FadeTime)
+		Track:Play(FadeTime, Weight, Speed)
+	end
+	
+	return Track
+end
+
+function AnimationPlayer:StopTrack(TrackName, FadeTime)
+	FadeTime = FadeTime or self.FadeTime
+	
+	local Track = self:GetTrack(TrackName)
+	
+	Track:Stop(FadeTime)
+	
+	return Track
+end
+
+function AnimationPlayer:StopAllTracks(FadeTime)
+	for TrackName, _ in pairs(self.Tracks) do
+		self:StopTrack(TrackName, FadeTime)
+	end
+end
+
+function AnimationPlayer:Destroy()
+	self:StopAllTracks()
+	setmetatable(self, nil)
+end
+
+return AnimationPlayer
