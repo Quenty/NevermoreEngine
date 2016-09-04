c4a5c7843759c156cc188d460c64a1fba86c38c6
Narrev <NarrevRBX@gmail.com>
2016-09-03 20:38:22 -0500

    Update NevermoreEngine.lua

--- App/NevermoreEngine.lua
+++ App/NevermoreEngine.lua
@@ -1,11 +1,11 @@
--- @original Quenty
 -- @author Narrev
+-- @original Quenty
 -- @readme https://github.com/Narrev/Nevermore
 -- Nevermore Loader
 
 -- Configuration
 local DEBUG_MODE = false -- Helps identify which modules fail to load
-local ServerScriptService_Module_Folder_Name = "Nevermore"
+local ServerScriptService_Module_Folder_Name = "Modules"
 local Classes = { -- Allows for abbreviations
 	Event = "RemoteEvent";
 	Function = "RemoteFunction";
@@ -20,7 +20,7 @@ local ServerScriptService = game:GetService("ServerScriptService")
 local self = {__metatable = "[Nevermore] Cannot change metatable because Nevermore relies on __index for procedural generation"}
 local LibraryCache = {}
 local ServerModules = ServerScriptService:FindFirstChild(ServerScriptService_Module_Folder_Name)
-local RetrieveObject, Repository
+local Appended, RetrieveObject, Repository = true
 
 -- Error for incorrect setup
 assert(script:IsA("ModuleScript"), "[Nevermore] Nevermore must be a ModuleScript")
@@ -40,6 +40,7 @@ local function Cache(Object, Name)
 	if Object:IsA("ModuleScript") then
 		assert(not LibraryCache[Name], "[Nevermore] Duplicate Module with name \"" .. Name .. "\"")
 		LibraryCache[Name] = Object
+		return true
 	end
 end
 
@@ -62,10 +63,9 @@ if RunService:IsServer() then
 	end
 
 	if not RunService:IsStudio() then
+		local CacheModule = Cache
 		function Cache(Object, Name)
-			if Object:IsA("ModuleScript") then
-				assert(not LibraryCache[Name], "[Nevermore] Duplicate Module with name \"" .. Name .. "\"")
-				LibraryCache[Name] = Object
+			if CacheModule(Object, Name) then
 				Object.Parent = string.find(string.lower(Name), "server") and ServerModules or Repository
 			elseif not Object:IsA("Script") then
 				Object:Destroy()
@@ -80,65 +80,48 @@ else
 	end
 end
 
-function self:Folder() -- This gets cleaned by Repository declaration
-	return RetrieveObject(self, "NevermoreResources", ReplicatedStorage, "Folder")
-end
-
 function self:__index(index) -- Using several strings for the same method (e.g. Event and GetRemoteEvent) is slightly less efficient
 	assert(type(index) == "string", "[Nevermore] Method must be a string")
-	local originalIndex = index
-	local index = string.gsub(index, "^Get", "")
-	local Class = Classes[index] or index
-	local Table = {}
-	local Folder = self:Folder(Class .. "s")
-
-	local function Function(...)
-		local Name, Parent = extract(...)
-		return Table[Name] or RetrieveObject(Table, Name, Parent or Folder, Class)
+	if not Appended then
+		local NevermoreDescendants = script:GetChildren()
+		for a = 1, #NevermoreDescendants do
+			local Appendage = NevermoreDescendants[a]
+			if Appendage:IsA("ModuleScript") then
+				local func = require(Appendage)
+				self[Appendage.Name] = function(...)
+					return func(extract(...))
+				end
+			end
+		end
+		Appended = true
+		return self[index]
+	else
+		local originalIndex = index
+		local index = string.gsub(index, "^Get", "")
+		local Class = Classes[index] or index
+		local Table = {}
+		local Folder = self.Folder and self:Folder(Class .. "s") or RetrieveObject(self, "Resources", script, "Folder")
+		local function Function(...)
+			local Name, Parent = extract(...)
+			return Table[Name] or RetrieveObject(Table, Name, Parent or Folder, Class)
+		end
+		self[originalIndex] = Function
+		return Function
 	end
-
-	self[originalIndex] = Function
-	return Function
 end
-
 Repository = self:__index("Folder")("Modules") -- Generates Folder manager and grabs Module folder
-CacheAssemble(ServerModules or Repository) -- Assembles table LibraryCache
+Appended = not CacheAssemble(ServerModules or Repository) -- Assembles table LibraryCache
 
-function self:Module(...)
+function self.Module(...)
 	local Name = extract(...)
 	return type(Name) ~= "string" and error("[Nevermore] ModuleName must be a string") or require(LibraryCache[Name] or error("[Nevermore] Module \"" .. Name .. "\" does not exist."))
-end
-self.LoadLibrary = self.Module
-
-function self:Append(index) -- Because metatables cannot yield across the C boundary :/
-	local Appendage = script:FindFirstChild(index)
-
-	if Appendage and Appendage:IsA("ModuleScript") then
-		local func = require(Appendage)
-		local function Function(...)
-			return func(extract(...))
-		end
-
-		self[index] = Function
-		return Function
-	else
-		error("Appendage " .. index .. " doesn't exist")
-	end
-end
-
-function self:__call(str, ...)
-	if ... then
-		return self[str](...)
-	else
-		return self:Module(str)
-	end
 end
 
 if DEBUG_MODE then
 	local GetModule = self.Module
 	local DebugID, RequestDepth = 0, 0
 
-	function self:Module(...)
+	function self.Module(...)
 		local Name = extract(...)
 		DebugID = DebugID + 1
 		local LocalDebugID = DebugID
@@ -151,4 +134,13 @@ if DEBUG_MODE then
 	end
 end
 
+function self:__call(str, ...)
+	if ... then
+		return self[str](...)
+	else
+		return self:Module(str)
+	end
+end
+
+self.LoadLibrary = self.Module
 return setmetatable(self, self)
