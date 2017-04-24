54c6c642a0d90edbce6be9d3ce718c0b2aaca0a3
James Onnen <rbxquenty@gmail.com>
2017-04-24 10:40:52 -0500

    Update Maid to handle nested maids and use new API

--- Modules/Events/Maid.lua
+++ Modules/Events/Maid.lua
@@ -4,15 +4,16 @@ Manages the cleaning of events and other things.
 Modified by Quenty
  
 API:
-	HireMaid()                        Returns a new Maid object.
+	MakeMaid()                        Returns a new Maid object.
  
 	Maid[key] = (function)            Adds a task to perform when cleaning up.
 	Maid[key] = (event connection)    Manages an event connection. Anything that isn't a function is assumed to be this.
 	Maid[key] = (Maid)                Maids can act as an event connection, allowing a Maid to have other maids to clean up.
-	Maid[key] = nil                   Removes a named task. If the task is an event, it is disconnected.
+	Maid[key] = nil                   Removes a named task. If the task is an event, it is disconnected. If it is an object, it is destroyed.
  
 	Maid:GiveTask(task)               Same as above, but uses an incremented number as a key.
 	Maid:DoCleaning()                 Disconnects all managed events and performs all clean-up tasks.
+	Maid:Destroy()                    Alias for DoCleaning()
 ]]
 
 local lib = {}
