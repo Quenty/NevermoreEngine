2be0084b615b6bcfa20c0269c46ce653d4770e73
James Onnen <rbxquenty@gmail.com>
2017-04-24 10:48:06 -0500

    Update camera controls to handle touch

--- Modules/Camera/Effects/CameraControls.lua
+++ Modules/Camera/Effects/CameraControls.lua
@@ -1,5 +1,6 @@
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local UserInputService  = game:GetService("UserInputService")
+local ContextActionService  = game:GetService("ContextActionService")
 
 local NevermoreEngine   = require(ReplicatedStorage:WaitForChild("NevermoreEngine"))
 local LoadCustomLibrary = NevermoreEngine.LoadLibrary
@@ -15,22 +16,24 @@ local CameraControls = {}
 CameraControls.__index = CameraControls
 CameraControls.ClassName = "CameraControls"
 CameraControls.MOUSE_SENSITIVITY = Vector2.new(math.pi*4, math.pi*1.9)
+CameraControls.DragBeginTypes = {Enum.UserInputType.MouseButton2, Enum.UserInputType.Touch}
 
 function CameraControls.new()
 	local self = setmetatable({}, CameraControls)
 
 	self.Enabled = false
-
+	self.Key = tostring(self) .. "CameraControls"
+	
 	return self
 end
 
-function CameraControls:SetZoomCamera(ZoomCamera)
-	self.ZoomCamera = ZoomCamera or error()
+function CameraControls:SetZoomedCamera(ZoomedCamera)
+	self.ZoomedCamera = ZoomedCamera or error()
 	return self
 end
 
-function CameraControls:SetRotationCamera(RotationCamera)
-	self.RotationCamera = RotationCamera or error()
+function CameraControls:SetRotatedCamera(RotatedCamera)
+	self.RotatedCamera = RotatedCamera or error()
 	return self
 end
 
@@ -66,16 +69,18 @@ end
 function CameraControls:HandleMouseWheel(InputObject)
 	--- This code was the same algorithm used by ROBLOX. It makes it so you can zoom easier at further distances.
 
-	local Delta = ClampNumber(-InputObject.Position.Z, -1, 1)*1.4
-	local Zoom = rk4Integrator(self.ZoomCamera.TargetZoom, Delta, 1)
-
-	if self.RotationCamera then
-		if self.RotationCamera.ClassName == "PushCamera" then
-			self.RotationCamera:StopRotateBack()
+	if self.ZoomedCamera then
+		local Delta = ClampNumber(-InputObject.Position.Z, -1, 1)*1.4
+		local Zoom = rk4Integrator(self.ZoomedCamera.TargetZoom, Delta, 1)
+		
+		self.ZoomedCamera.TargetZoom = Zoom
+	end
+	
+	if self.RotatedCamera then
+		if self.RotatedCamera.ClassName == "PushCamera" then
+			self.RotatedCamera:StopRotateBack()
 		end
 	end
-
-	self.ZoomCamera.TargetZoom = Zoom
 end
 
 function CameraControls:MouseTranslationToAngle(translationVector)
@@ -86,46 +91,143 @@ function CameraControls:MouseTranslationToAngle(translationVector)
 	return Vector2.new(xTheta, yTheta)
 end
 
-function CameraControls:HandleMouseMovement(InputObject)
-	if self.LastMousePosition then
+function CameraControls:GetVelocityTracker(Strength, StartVelocity)
+	Strength = Strength or 1
+	
+	local LastUpdate = tick()
+	local Velocity = StartVelocity
+	
+	return {
+		Update = function(self, Delta)
+			local Elapsed = tick() - LastUpdate
+			LastUpdate = tick()
+			Velocity = Velocity / (2^(Elapsed/Strength)) + (Delta / (0.0001 + Elapsed)) * Strength
+		end;
+		GetVelocity = function(self)
+			self:Update(StartVelocity*0)
+			return Velocity
+		end;
+	}
+end
 
-		if self.RotationCamera then
+function CameraControls:HandleMouseMovement(InputObject, IsMouse)
+	if self.LastMousePosition then
+		
+		if self.RotatedCamera then
 			-- This calculation may seem weird, but either .Position updates (if it's locked), or .Delta updates (if it's not).
-			local Delta = -InputObject.Delta + self.LastMousePosition - InputObject.Position
-			self.RotationCamera:RotateXY(self:MouseTranslationToAngle(Delta) * self.MOUSE_SENSITIVITY)
+			local Delta
+			if IsMouse then
+				Delta = -InputObject.Delta + self.LastMousePosition - InputObject.Position
+			else
+				Delta = -InputObject.Delta
+			end
+			
+			local DeltaAngle = self:MouseTranslationToAngle(Delta) * self.MOUSE_SENSITIVITY
+			self.RotatedCamera:RotateXY(DeltaAngle)
+			
+			if self.RotVelocityTracker then
+				self.RotVelocityTracker:Update(DeltaAngle)
+			end
 		end
 		
 		self.LastMousePosition = InputObject.Position
 	end
 end
 
+function CameraControls:BeginDrag(BeginInputObject)
+	if not self.RotatedCamera then
+		self.Maid.DragMaid = nil
+		return
+	end
+	
+	local Maid = MakeMaid()
+	
+	self.LastMousePosition = BeginInputObject.Position
+	local IsMouse = BeginInputObject.UserInputType.Name:find("Mouse")
+	
+	if IsMouse then
+		UserInputService.MouseBehavior = Enum.MouseBehavior.LockCurrentPosition
+	end
+	
+	Maid.InputEnded = UserInputService.InputEnded:connect(function(InputObject, GameProcessed)
+		if InputObject == BeginInputObject then
+			self:EndDrag()
+		end
+	end)
+	
+	Maid.InputChanged = UserInputService.InputChanged:connect(function(InputObject)
+		if IsMouse and InputObject.UserInputType == Enum.UserInputType.MouseMovement 
+			or InputObject == BeginInputObject then
+			
+			self:HandleMouseMovement(InputObject)
+		end
+	end)
+
+	
+	
+	if self.RotatedCamera.ClassName == "SmoothRotatedCamera" then
+		self.RotVelocityTracker = self:GetVelocityTracker(0.05, Vector2.new())
+	end
+	
+	
+	
+	self.Maid.DragMaid = Maid
+end
+
+
+function CameraControls:EndDrag()
+	if self.RotVelocityTracker then
+		local Position = self.RotatedCamera.AngleXZ
+		local Velocity = self.RotVelocityTracker:GetVelocity().X
+		local NewVelocityTarget = Position + Velocity
+		local Target = self.RotatedCamera.TargetAngleXZ
+		
+		if math.abs(NewVelocityTarget - Position) > math.abs(Target - Position) then
+			self.RotatedCamera.TargetAngleXZ = NewVelocityTarget
+		end
+		
+		self.RotVelocityTracker = nil
+		
+		self.RotatedCamera:SnapIntoBounds()
+	end
+	self.LastMousePosition = nil
+	self.Velocity = nil
+	UserInputService.MouseBehavior = Enum.MouseBehavior.Default
+	
+	self.Maid.DragMaid = nil
+end
+
+function CameraControls:IsEnabled()
+	return self.Enabled
+end
+
+function CameraControls:GetKey()
+	return self.Key
+end
+
 function CameraControls:Enable()
 	if not self.Enabled then
+		assert(not self.Maid)
 		self.Enabled = true
 
 		self.Maid = MakeMaid()
-
-		self.Maid.InputChanged = UserInputService.InputChanged:connect(function(InputObject, Processed)
-			if InputObject.UserInputType.Name == "MouseWheel" then
+		
+		ContextActionService:BindAction(self.Key, function(ActionName, UserInputState, InputObject)
+			if InputObject.UserInputType == Enum.UserInputType.MouseWheel then
 				self:HandleMouseWheel(InputObject)
-			elseif InputObject.UserInputType.Name == "MouseMovement" then
-				self:HandleMouseMovement(InputObject)
 			end
-		end)
-
-		self.Maid.InputBegan = UserInputService.InputBegan:connect(function(InputObject, Processed)
-			if InputObject.UserInputType.Name == "MouseButton2" then
-				self.LastMousePosition = InputObject.Position
-				UserInputService.MouseBehavior = "LockCurrentPosition"
+		end, false, Enum.UserInputType.MouseWheel)
+		
+		ContextActionService:BindAction(self.Key .. "Drag", function(ActionName, UserInputState, InputObject)
+			if UserInputState == Enum.UserInputState.Begin then
+				self:BeginDrag(InputObject)
 			end
-		end)
-
-		self.Maid.InputEnded = UserInputService.InputEnded:connect(function(InputObject, Processed)
-			if InputObject.UserInputType.Name == "MouseButton2" then
-				self.LastMousePosition = nil
-				UserInputService.MouseBehavior = "Default"
-			end
-		end)
+		end, false, unpack(self.DragBeginTypes))
+		
+		self.Maid.Cleanup = function()
+			ContextActionService:UnbindAction(self.Key)
+			ContextActionService:UnbindAction(self.Key .. "Drag")
+		end
 	end
 end
 
@@ -137,7 +239,6 @@ function CameraControls:Disable()
 		self.Maid = nil
 
 		self.LastMousePosition = nil
-		UserInputService.MouseBehavior = "Default"
 	end
 end
 
