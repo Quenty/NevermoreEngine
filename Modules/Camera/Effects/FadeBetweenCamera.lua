4b4075c3b2d76a5221e5f8f7f5cdd2d9af7f8283
James Onnen <rbxquenty@gmail.com>
2017-08-12 20:42:40 -0500

    Add FadeBetweenCamera

---
+++ Modules/Camera/Effects/FadeBetweenCamera.lua
@@ -0,0 +1,95 @@
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+local NevermoreEngine   = require(ReplicatedStorage:WaitForChild("NevermoreEngine"))
+local LoadCustomLibrary = NevermoreEngine.LoadLibrary
+
+local CameraState       = LoadCustomLibrary("CameraState")
+local SpringPhysics     = LoadCustomLibrary("SpringPhysics")
+local SummedCamera      = LoadCustomLibrary("SummedCamera")
+
+
+-- Intent: Add another layer of effects that can be faded in/out
+
+local FadeBetweenCamera = {}
+FadeBetweenCamera.ClassName = "FadeBetweenCamera"
+
+function FadeBetweenCamera.new(CameraA, CameraB)
+	local self = setmetatable({}, FadeBetweenCamera)
+
+	self.Spring = SpringPhysics.NumberSpring.New()
+
+	self.CameraA = CameraA or error("No CameraA")
+	self.CameraB = CameraB or error("No CameraB")
+	self.Damper = 1
+	self.Speed = 15
+
+	return self
+end
+
+function FadeBetweenCamera:__newindex(Index, Value)
+	if Index == "Damper" then
+		self.Spring.Damper = Value
+	elseif Index == "Value" then
+		self.Spring.Value = Value
+	elseif Index == "Speed" then
+		self.Spring.Speed = Value
+	elseif Index == "Target" then
+		self.Spring.Target = Value
+	elseif Index == "Velocity" then
+		self.Spring.Velocity = Value
+	elseif Index == "Spring" or Index == "CameraA" or Index == "CameraB" then
+		rawset(self, Index, Value)
+	else
+		error(Index .. " is not a valid member of fading camera")
+	end
+end
+
+
+function FadeBetweenCamera:__index(Index)
+	if Index == "State" or Index == "CameraState" or Index == "Camera" then
+		--print("Spring value", self.Spring.Value)
+		--return (self.Camera.CameraState or self.Camera) * self.Spring.Value
+		--Qmul(q0, Qpow(Qmul(q1, Qinv(q0)), t))
+		local Value = self.Spring.Value
+		
+		--[[
+		if self.Spring.Velocity < 0 then
+			StateA, StateB = StateB, StateA
+			Value = 1-Value
+		end--]]
+		
+		if math.abs(Value - 1) <= 1e-4 then
+			return self.CameraStateB
+		elseif math.abs(Value) <= 1e-4 then
+			return self.CameraStateA
+		else
+			local StateA = self.CameraStateA
+			local StateB = self.CameraStateB
+	
+			return StateA + (StateB - StateA)*Value
+		end
+		--local Quaternion = LoadCustomLibrary("Quaternion")
+		--print(NewState)
+		--return NewState
+	elseif Index == "CameraStateA" then
+		return self.CameraA.CameraState or self.CameraA
+	elseif Index == "CameraStateB" then
+		return self.CameraB.CameraState or self.CameraB
+	elseif Index == "Damper" then
+		return self.Spring.Damper
+	elseif Index == "Value" then
+		return self.Spring.Value
+	elseif Index == "Speed" then
+		return self.Spring.Speed
+	elseif Index == "Target" then
+		return self.Spring.Target
+	elseif Index == "Velocity" then
+		return self.Spring.Velocity
+	elseif Index == "HasReachedTarget" then
+		return math.abs(self.Value - self.Target) < 1e-4 and math.abs(self.Velocity) < 1e-4
+	else
+		return FadeBetweenCamera[Index]
+	end
+end
+
+return FadeBetweenCamera
