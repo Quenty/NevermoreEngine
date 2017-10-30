0d599b25a82554d210f1a071b16c606cdc6dab96
James Onnen <rbxquenty@gmail.com>
2017-10-30 15:31:27 -0500

    Add documentation to Maid

--- Modules/Events/Maid.lua
+++ Modules/Events/Maid.lua
@@ -14,22 +14,26 @@ API:
  
 	Maid:GiveTask(task)               Same as above, but uses an incremented number as a key.
 	Maid:DoCleaning()                 Disconnects all managed events and performs all clean-up tasks.
+	Maid:IsCleaning()				  Returns true is in cleaning process.
 	Maid:Destroy()                    Alias for DoCleaning()
 ]]
- 
+
+--- Manages the cleaning of events and other things.
 local Maid = {}
 
+--- Returns a new Maid object
 function Maid.new()
 	local self = {}
 	
 	self.Tasks = {}
-	self.Instances = {}
 	self.IsCurrentlyCleaning = false
 	
 	return setmetatable(self, Maid)
 end
 Maid.MakeMaid = Maid.new
 
+--- Returns Maid[key] if not part of Maid metatable
+-- @return Maid[key] value
 function Maid:__index(Index)
 	if Maid[Index] then
 		return Maid[Index]
@@ -38,6 +42,12 @@ function Maid:__index(Index)
 	end
 end
 
+--- Add a task to clean up
+-- Maid[key] = (function)            Adds a task to perform when cleaning up.
+-- Maid[key] = (event connection)    Manages an event connection. Anything that isn"t a function is assumed to be this.
+-- Maid[key] = (Maid)                Maids can act as an event connection, allowing a Maid to have other maids to clean up.
+-- Maid[key] = (Object)              Maids can cleanup objects with a `Destroy` method
+-- Maid[key] = nil                   Removes a named task. If the task is an event, it is disconnected. If it is an object, it is destroyed.
 function Maid:__newindex(Index, Value)
 	if self.IsCurrentlyCleaning then
 		error(("Already cleaning, cannot add index '%s'"):format(tostring(Index)), 2)
@@ -61,6 +71,9 @@ function Maid:__newindex(Index, Value)
 	Tasks[Index] = Value
 end
 
+--- Same as indexing, but uses an incremented number as a key
+-- @param Task An item to clean
+-- @return int TaskId
 function Maid:GiveTask(Task)
 	if self.IsCurrentlyCleaning then
 		error(("Currently cleaning, cannot give task"), 2)
@@ -71,10 +84,13 @@ function Maid:GiveTask(Task)
 	return TaskId
 end
 
+--- Returns true is in cleaning process
+-- @return True if cleaning, false otherwise
 function Maid:IsCleaning()
 	return self.IsCurrentlyCleaning
 end
 
+--- Disconnects all managed events and performs all clean-up tasks
 function Maid:DoCleaning()
 	if self.IsCurrentlyCleaning then
 		error("Already cleaning, cannot call DoCleaning()", 2)
