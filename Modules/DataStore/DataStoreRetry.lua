03efd5daf1d6911a131aa4a336cbed7e1b610cc6
James Onnen <rbxquenty@gmail.com>
2017-11-20 17:55:50 -0600

    Add DataStoreRetry utility

---
+++ Modules/DataStore/DataStoreRetry.lua
@@ -0,0 +1,29 @@
+local RunService = game:GetService("RunService")
+
+-- Intent: General usage datastore retry function
+
+local DATASTORE_RETRIES = 3
+local function DataStoreRetry(DataStoreFunction)
+	local Tries = 0	
+	local Success = true
+	local Data = nil
+	repeat
+		Tries = Tries + 1
+		local Error
+		Success, Error = ypcall(function() 
+			Data = DataStoreFunction() 
+		end)
+		if not Success then
+			warn(("[DataStoreRetry] - Datastore failure '%s'. Retrying!"):format(tostring(Error)))
+			if not RunService:IsStudio() then
+				wait(1)
+			end
+		end
+	until Tries == DATASTORE_RETRIES or Success
+	if not Success then
+		warn("[DataStoreRetry] - Datastore completely failed. No more recovery attempts.")
+	end
+	return Success, Data
+end
+
+return DataStoreRetry
