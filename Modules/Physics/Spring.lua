ad1115d9df39a6f980e6eb361dda3af0ea6597fa
James Onnen <rbxquenty@gmail.com>
2017-11-20 15:43:29 -0600

    SpringPhysics -> Spring

--- Modules/Physics/Spring.lua
+++ Modules/Physics/Spring.lua
@@ -1,7 +1,47 @@
+--[[
+class Spring
+
+Description:
+	A physical model of a spring, useful in many applications. Properties only evaluate
+	upon index making this model good for lazy applications
+
+API:
+	Spring = Spring.new(number position)
+		Creates a new spring in 1D
+	Spring = Spring.new(Vector3 position)
+		Creates a new spring in 3D
+
+	Spring.Position
+		Returns the current position
+	Spring.Velocity
+		Returns the current velocity
+	Spring.Target
+		Returns the target
+	Spring.Damper
+		Returns the damper
+	Spring.Speed
+		Returns the speed
+
+	Spring.Target = number/Vector3
+		Sets the target
+	Spring.Position = number/Vector3
+		Sets the position
+	Spring.Velocity = number/Vector3
+		Sets the velocity
+	Spring.Damper = number [0, 1]
+		Sets the spring damper, defaults to 1
+	Spring.Speed = number [0, infinity)
+		Sets the spring speed, defaults to 1
+
+	Spring:TimeSkip(number DeltaTime)
+		Instantly skips the spring forwards by that amount of time
+	Spring:Impulse(number/Vector3 Velocity)
+		Impulses the spring, increasing velocity by the amount given
+]]
+
+
 local Spring = {}
 
-
---- Spring model for animations
 -- @param Initial A number or Vector3 (anything with * number and addition/subtraction defined)
 function Spring.new(Initial)
 	local self = setmetatable({}, Spring)
