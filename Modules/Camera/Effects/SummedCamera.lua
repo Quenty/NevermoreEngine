c6a499e1eb5efada64fd07140c130097837fa031
James Onnen <rbxquenty@gmail.com>
2017-11-20 12:37:00 -0600

    Refactored CameraState indexing

--- Modules/Camera/Effects/SummedCamera.lua
+++ Modules/Camera/Effects/SummedCamera.lua
@@ -3,15 +3,14 @@ local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local NevermoreEngine   = require(ReplicatedStorage:WaitForChild("NevermoreEngine"))
 local LoadCustomLibrary = NevermoreEngine.LoadLibrary
 
-local CameraState       = LoadCustomLibrary("CameraState")
+local CameraState = LoadCustomLibrary("CameraState")
+
+-- Intent: Add two cameras together
 
 local SummedCamera = {}
 SummedCamera.ClassName = "SummedCamera"
-
 SummedCamera.Mode = "World" -- If World, then it just adds positions. 
                             -- If relative, then it moves position relative to CameraA's CoordinateFrame.
-
--- Intent: Add two cameras together
 
 function SummedCamera.new(CameraA, CameraB)
 	-- @param CameraA A CameraState or another CameraEffect to be used
@@ -49,18 +48,23 @@ end
 function SummedCamera:__index(Index)
 	if Index == "State" or Index == "CameraState" or Index == "Camera" then
 		if self.Mode == "World" then
-			return self.CameraA + self.CameraB
+			return self.CameraAState + self.CameraBState
 		else
-			local Result = self.CameraA + self.CameraB
-			Result.qPosition = self.CameraA.CoordinateFrame * self.CameraB.qPosition
+			local StateA = self.CameraAState
+			local StateB = self.CameraBState
+			
+			local Result = StateA + StateB
+			Result.qPosition = StateA.CoordinateFrame * StateB.qPosition
 			return Result
 		end
-	elseif Index == "CameraA" then
+	elseif Index == "CameraAState" then
 		return self._CameraA.CameraState or self._CameraA
-	elseif Index == "CameraB" then
+	elseif Index == "CameraBState" then
 		return self._CameraB.CameraState or self._CameraB
-	else
+	elseif SummedCamera[Index] then
 		return SummedCamera[Index]
+	else
+		error(("[SummedCamera] - '%s' is not a valid member"):format(tostring(Index)))
 	end
 end
 
