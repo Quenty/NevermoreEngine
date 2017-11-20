b057f41644a13df85ba2ae7729e987c54e4ef188
James Onnen <rbxquenty@gmail.com>
2017-11-20 13:28:55 -0600

    Documentation changes

--- Modules/Math/Probability.lua
+++ Modules/Math/Probability.lua
@@ -1,11 +1,10 @@
 -- Probability.lua
--- @author Quenty, w/ contribs for this...
+-- @author Quenty
 
 local lib = {}
 
 function lib.BoxMuller()
 	--- Normal curve. [-1, 1]
-	
     return math.sqrt(-2 * math.log(math.random())) * math.cos(2 * math.pi * math.random()) / 2
 end
 
