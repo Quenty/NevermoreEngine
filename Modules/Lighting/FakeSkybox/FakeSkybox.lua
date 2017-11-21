93a9f73fae1a9a350c6c422195459fe89bfd5755
James Onnen <rbxquenty@gmail.com>
2017-11-20 18:49:57 -0600

    Add FakeSkybox

---
+++ Modules/Lighting/FakeSkybox/FakeSkybox.lua
@@ -0,0 +1,137 @@
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local RunService = game:GetService("RunService")
+
+local NevermoreEngine = require(ReplicatedStorage:WaitForChild("NevermoreEngine"))
+local LoadCustomLibrary = NevermoreEngine.LoadLibrary
+
+local MakeMaid = LoadCustomLibrary("Maid").MakeMaid
+local FakeSkyboxSide = LoadCustomLibrary("FakeSkyboxSide")
+local AccelTween = LoadCustomLibrary("AccelTween")
+local Signal = LoadCustomLibrary("Signal")
+
+-- Intent: Allow transitions between skyboxes
+
+local SkyboxPropertyImageMap = {}
+for Surface, Direction in pairs({
+	Top = "Up";
+	Bottom = "Dn";
+	
+	 -- Bind backwards
+	Right = "Lf";
+	Left = "Rt";
+	
+	Front = "Ft";
+	Back = "Bk";
+}) do
+	SkyboxPropertyImageMap[Enum.NormalId[Surface]] = "Skybox" .. Direction
+end
+
+
+local FakeSkybox = {}
+FakeSkybox.__index = FakeSkybox
+FakeSkybox.ClassName = "FakeSkybox"
+FakeSkybox.PartSize = 1024
+
+function FakeSkybox.new(Skybox)
+	local self = setmetatable({}, FakeSkybox)
+	
+	self.Maid = MakeMaid()
+	
+	self.ParentFolder = Instance.new("Folder")
+	self.ParentFolder.Name = "Skybox"
+	self.Maid:GiveTask(self.ParentFolder)
+	
+	self.Sides = {}
+	for _, NormalId in pairs(Enum.NormalId:GetEnumItems()) do
+		self.Sides[NormalId] = FakeSkyboxSide.new(self.PartSize, NormalId, self.ParentFolder)
+		--self.Maid[NormalId] = self.Sides[NormalId]
+	end
+	
+	self.Visible = false
+	self.VisibleChanged = Signal.new()
+	
+	self.PercentVisible = AccelTween.new(0.25)
+	self.PercentVisible.t = 0
+	self.PercentVisible.p = 0
+	
+	if Skybox then
+		self:SetSkybox(Skybox)
+	end
+	
+	self.ParentFolder.Parent = workspace.CurrentCamera
+	
+	return self
+end
+
+
+function FakeSkybox:SetPartSize(PartSize)
+	self.PartSize = PartSize or error("No PartSize")
+	
+	for _, Side in pairs(self.Sides) do
+		Side:SetPartSize(self.PartSize)
+	end
+	
+	return self
+end
+
+function FakeSkybox:Show(DoNotAnimate)
+	if self.Visible then
+		return
+	end
+	
+	self.Visible = true
+	self.VisibleChanged:fire(self.Visible, DoNotAnimate)
+	
+	self.PercentVisible.t = 1
+	
+	if DoNotAnimate then
+		self.PercentVisible.p = 1
+	end
+end
+
+function FakeSkybox:Hide(DoNotAnimate)
+	if not self.Visible then
+		return
+	end
+	
+	self.Visible = false
+	self.VisibleChanged:fire(self.Visible, DoNotAnimate)
+	
+	self.PercentVisible.t = 0
+	
+	if DoNotAnimate then
+		self.PercentVisible.p = 0
+	end
+end
+
+function FakeSkybox:SetSkybox(Skybox)
+	self.Skybox = Skybox or error("No Skybox")
+	
+	for Normal, Side in pairs(self.Sides) do
+		local PropertyName = SkyboxPropertyImageMap[Normal]
+		Side:SetImage(Skybox[PropertyName])
+	end
+	
+	return self
+end
+
+function FakeSkybox:IsVisible()
+	return self.Visible
+end
+
+
+function FakeSkybox:UpdateRender(BaseCFrame)
+	local Transparency = 1-self.PercentVisible.p
+	
+	for _, Side in pairs(self.Sides) do
+		Side:UpdateRender(BaseCFrame)
+		Side:SetTransparency(Transparency)
+	end
+end
+
+function FakeSkybox:Destroy()
+	self.Maid:DoCleaning()
+	self.Maid = nil
+end
+
+return FakeSkybox
---
+++ Modules/Lighting/FakeSkybox/FakeSkyboxSide.lua
@@ -0,0 +1,91 @@
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local RunService = game:GetService("RunService")
+
+local NevermoreEngine = require(ReplicatedStorage:WaitForChild("NevermoreEngine"))
+local LoadCustomLibrary = NevermoreEngine.LoadLibrary
+
+local FakeSkyboxSide = {}
+FakeSkyboxSide.__index = FakeSkyboxSide
+FakeSkyboxSide.ClassName = "FakeSkyboxSide"
+FakeSkyboxSide.CanvasSize = 1024
+FakeSkyboxSide.PartWidth = 1
+
+function FakeSkyboxSide.new(PartSize, Normal, Parent)
+	local self = setmetatable({}, FakeSkyboxSide)
+	
+	self.PartSize = PartSize or error("No PartSize")
+	self.Transparency = 1
+	self.Normal = Normal or error("No Normal")
+	
+	self.Part = Instance.new("Part")
+	self.Part.Name = "SkyboxSide" .. self.Normal.Name
+	self.Part.Anchored = true
+	self.Part.Transparency = 1
+	self.Part.CanCollide = false
+	
+	self.SurfaceGui = Instance.new("SurfaceGui")
+	self.SurfaceGui.Adornee = self.Part
+	self.SurfaceGui.CanvasSize = Vector2.new(self.CanvasSize, self.CanvasSize)
+	self.SurfaceGui.LightInfluence = 0
+	self.SurfaceGui.Face = Enum.NormalId.Front
+	self.SurfaceGui.Parent = self.Part
+	
+	local ImageLabel = Instance.new("ImageLabel")
+	ImageLabel.AnchorPoint = Vector2.new(0.5, 0.5)
+	ImageLabel.Position = UDim2.new(0.5, 0, 0.5, 0)
+	ImageLabel.Size = UDim2.new(1, 0, 1, 0)
+	ImageLabel.ImageTransparency = self.Transparency
+	ImageLabel.BackgroundColor3 = Color3.new(0, 0, 0)
+	ImageLabel.BackgroundTransparency = 1
+	ImageLabel.BorderSizePixel = 0
+	
+	ImageLabel.Parent = self.SurfaceGui
+	self.ImageLabel = ImageLabel
+		
+	self:UpdateSizing()
+	
+	self.Part.Parent = Parent	
+	
+	return self
+end
+
+function FakeSkyboxSide:SetPartSize(PartSize)
+	self.PartSize = PartSize or error("No PartSize")
+	self:UpdateSizing()
+	
+	return self
+end
+
+function FakeSkyboxSide:UpdateSizing()
+	self.Part.Size = Vector3.new(self.PartSize, self.PartSize, self.PartWidth)
+	
+	local Direction = Vector3.FromNormalId(self.Normal)
+	local Offset = Direction * self.PartWidth/2
+	
+	self.Relative = CFrame.new(Direction*(self.PartSize/2) + Offset) 
+		* CFrame.new(Vector3.new(0, 0, 0), -Direction)
+	
+	if self.Normal == Enum.NormalId.Bottom then
+		-- Hack
+		self.Relative = self.Relative * CFrame.Angles(0, 0, math.pi)
+	end
+end
+
+function FakeSkyboxSide:SetImage(Image)
+	self.ImageLabel.Image = Image
+	
+	return self
+end
+
+function FakeSkyboxSide:SetTransparency(Transparency)
+	self.Transparency = Transparency or error("No Transparency")
+	self.ImageLabel.ImageTransparency = Transparency
+	
+	return self
+end
+
+function FakeSkyboxSide:UpdateRender(RelativeCFrame)
+	self.Part.CFrame = RelativeCFrame * self.Relative
+end
+
+return FakeSkyboxSide
