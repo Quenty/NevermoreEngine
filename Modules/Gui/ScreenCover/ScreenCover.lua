c1d0dd9aecf95afece9f6e2bc77244b84c123c6e
James Onnen <rbxquenty@gmail.com>
2017-12-11 19:37:36 -0600

    Reprogram ScreenCover

---
+++ Modules/Gui/ScreenCover/ScreenCover.lua
@@ -0,0 +1,195 @@
+--- Covers the screen in a satisfying way
+-- @classmod ScreenCover
+
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+local NevermoreEngine = require(ReplicatedStorage:WaitForChild("NevermoreEngine"))
+local LoadCustomLibrary = NevermoreEngine.LoadLibrary
+
+local ScreenCoverHelperBuilder = LoadCustomLibrary("ScreenCoverHelperBuilder")
+local Signal = LoadCustomLibrary("Signal")
+local Maid = LoadCustomLibrary("Maid")
+local Promise = LoadCustomLibrary("Promise")
+
+local ScreenCover = {}
+ScreenCover.__index = ScreenCover
+ScreenCover.ClassName = "ScreenCover"
+ScreenCover.SQUARE_PADDING = 2 -- Extra pixels to prevent splits
+
+function ScreenCover.new(gui)
+	local self = setmetatable({}, ScreenCover)
+	
+	self.Gui = gui or error("No gui")
+	self.Done = Signal.new()
+	self.Maid = Maid.new()
+	
+	self._builder = ScreenCoverHelperBuilder.new(gui)
+
+	self.Maid:GiveTask(self.Gui:GetPropertyChangedSignal("AbsoluteSize"):connect(function()
+		self:_updateSize()
+	end))
+	self:_updateSize()
+	
+	return self
+end
+
+function ScreenCover:Show(playbackTime)
+	playbackTime = playbackTime or 0.6
+
+	local diagonals = self:_getDiagonalSquares(12)
+	self:_showSquares(diagonals, playbackTime)
+	
+	return Promise.new(self.Done)
+end
+
+function ScreenCover:Hide(playbackTime)
+	playbackTime = playbackTime or 0.6
+	
+	local diagonals = self:_getDiagonalSquares(12)
+	self:_hideSquares(diagonals, playbackTime)
+	
+	return Promise.new(self.Done)
+end
+
+--- Force size constraint to fill whole screen
+function ScreenCover:_updateSize()
+	local absoluteSize = self.Gui.AbsoluteSize
+	if absoluteSize.X < absoluteSize.Y then
+		self.Gui.SizeConstraint = Enum.SizeConstraint.RelativeXX
+	else
+		self.Gui.SizeConstraint = Enum.SizeConstraint.RelativeYY
+	end
+end
+
+function ScreenCover:_getSquareData(squareCount)
+	squareCount = squareCount or 10
+	
+	local squareSize = 1/squareCount
+	local size = UDim2.new(squareSize, self.SQUARE_PADDING, squareSize, self.SQUARE_PADDING)
+	
+	local matrix = {}
+	for x=1, squareCount do
+		matrix[x] = {}
+		for y=1, squareCount do
+			matrix[x][y] = {
+				Position = UDim2.new(x/squareCount - squareSize/2, 0, y/squareCount - squareSize/2, 0);
+				Size = size;
+				AnchorPoint = Vector2.new(0.5, 0.5)
+			}
+		end
+	end
+	
+	return matrix
+end
+
+function ScreenCover:_getDiagonalSquares(squareCount)
+	local matrix = self:_getSquareData(squareCount)
+	
+	local squares = {} -- Array of arrays
+	
+	for x=1, #matrix do
+		for y=1, #matrix do
+			local square = matrix[x][y]
+			local index = (x-1)+(y-1)+1
+			squares[index] = squares[index] or {}
+			table.insert(squares[index], square)
+		end
+	end
+	
+	return squares
+end
+
+function ScreenCover:_showSquares(squareDataList, playbackTime)
+	local maid = Maid.new()
+	self.Maid.animMaid = maid
+
+	local tweenTime = playbackTime/(#squareDataList)*8
+	local squarePlaybackTime = (playbackTime - tweenTime)
+	
+	self.Gui.BackgroundTransparency = 1
+	local alive = true
+	maid:GiveTask(function()
+		self.Gui.BackgroundTransparency = 0
+		alive = false
+		self.Done:Fire()
+	end)
+	
+	for i=1, #squareDataList do
+		local dataList = squareDataList[i]
+		local delayTime = squarePlaybackTime*(i/#squareDataList)
+		delay(delayTime, function()
+			if not alive then
+				return
+			end
+			
+			for _, squareData in pairs(dataList) do
+				local frame = self._builder:CreateSquare(squareData)
+				frame.Size = UDim2.new(0, 0, 0, 0)
+				frame.Parent = self.Gui
+				
+				frame:TweenSize(squareData.Size, Enum.EasingDirection.Out, Enum.EasingStyle.Quad, tweenTime, true)
+				maid:GiveTask(frame)
+			end
+		end)
+	end
+	
+	delay(playbackTime, function()
+		maid:DoCleaning()
+	end)
+end
+
+function ScreenCover:_hideSquares(squareDataList, playbackTime)
+	local maid = Maid.new()
+	self.Maid.animMaid = maid
+	
+	local tweenTime = playbackTime/(#squareDataList)*4
+	local squarePlaybackTime = (playbackTime - tweenTime)
+	
+	-- Build squares
+	local frameList = {}
+	for i=1, #squareDataList do
+		local frames = {}
+		table.insert(frameList, frames)
+		
+		local dataList = squareDataList[i]
+		for _, squareData in pairs(dataList) do
+			local frame = self._builder:CreateSquare(squareData)
+			frame.Size = squareData.Size
+			frame.Parent = self.Gui
+			
+			table.insert(frames, frame)
+			maid:GiveTask(frame)
+		end
+	end
+	
+	self.Gui.BackgroundTransparency = 1
+	local alive = true
+	maid:GiveTask(function()
+		alive = false
+		self.Done:Fire()
+	end)
+	
+	for i=1, #frameList do
+		local delayTime = squarePlaybackTime*(i/#frameList)
+		delay(delayTime, function()
+			if not alive then
+				return
+			end
+			
+			for _, frame in pairs(frameList[i]) do
+				frame:TweenSize(UDim2.new(0, 0, 0, 0), Enum.EasingDirection.Out, Enum.EasingStyle.Quad, tweenTime, true)
+			end
+		end)
+	end
+	
+	delay(playbackTime, function()
+		maid:DoCleaning()
+	end)
+end
+
+function ScreenCover:Destroy()
+	self.Maid:DoCleaning()
+	self.Maid = nil
+end
+
+return ScreenCover
---
+++ Modules/Gui/ScreenCover/ScreenCoverBuilder.lua
@@ -0,0 +1,58 @@
+---
+-- @classmod ScreenCoverBuilder
+
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+local NevermoreEngine = require(ReplicatedStorage:WaitForChild("NevermoreEngine"))
+local LoadCustomLibrary = NevermoreEngine.LoadLibrary
+
+local ScreenCover = LoadCustomLibrary("ScreenCover")
+
+
+local ScreenCoverBuilder = {}
+ScreenCoverBuilder.__index = ScreenCoverBuilder
+ScreenCoverBuilder.ClassName = "ScreenCoverBuilder"
+
+function ScreenCoverBuilder.new(PlayerGui)
+	local self = setmetatable({}, ScreenCoverBuilder)
+	
+	self._playerGui = PlayerGui or error("No PlayerGui")
+	
+	return self
+end
+
+--- Creates new ScreenCover
+function ScreenCoverBuilder:Create(options)
+	local screenGui = Instance.new("ScreenGui")
+	screenGui.Name = "ScreenCover"
+	
+	if options.DisplayOrder then
+		screenGui.DisplayOrder = options.DisplayOrder
+	end
+	
+	local frame = Instance.new("Frame")
+	frame.Name = "ScreenCover"
+	frame.BorderSizePixel = 0
+	frame.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+	frame.Active = true
+	frame.Size = UDim2.new(1, 0, 1, 36)
+	frame.Position = UDim2.new(0, 0, 0, -36)
+
+	for _, property in pairs({"BackgroundColor3"}) do
+		if options[property] then
+			frame[property] = options[property]
+		end
+	end
+	
+	frame.Parent = screenGui
+	
+	local cover = ScreenCover.new(frame)
+	cover.Maid:GiveTask(screenGui)
+	
+	screenGui.Parent = self._playerGui
+	
+	return cover
+end
+
+
+return ScreenCoverBuilder
