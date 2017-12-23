55c6d2fef7fca7d95b8b6d22838408d4a27407df
James Onnen <rbxquenty@gmail.com>
2017-12-23 15:31:00 -0600

    BaseAction no longer uses BasicPane

--- Modules/ActionManager/BaseAction.lua
+++ Modules/ActionManager/BaseAction.lua
@@ -1,25 +1,28 @@
+--- BaseAction state for Actionmanager
+-- @classmod BaseAction
+
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local ContextActionService = game:GetService("ContextActionService")
 
 local NevermoreEngine = require(ReplicatedStorage:WaitForChild("NevermoreEngine"))
 local LoadCustomLibrary = NevermoreEngine.LoadLibrary
 
-local BasicPane = LoadCustomLibrary("BasicPane")
 local Signal = LoadCustomLibrary("Signal")
 local MakeMaid = LoadCustomLibrary("Maid").MakeMaid
+local EnabledMixin = LoadCustomLibrary("EnabledMixin")
 
--- Intent: 
--- @author Quenty
-
-local BaseAction = setmetatable({}, BasicPane)
+local BaseAction = {}
 BaseAction.__index = BaseAction
 BaseAction.ClassName = "BaseAction"
 
+EnabledMixin:Add(BaseAction)
+
 function BaseAction.new(Name)
-	local self = setmetatable(BasicPane.new(nil, {
-		BasicPane.EnabledModule
-	}), BaseAction)
-	
+	local self = setmetatable({}, BaseAction)
+
+	self.Maid = MakeMaid()
+	self:InitEnableChanged()
+
 	self.ActivateData = nil -- Data to be fired with the Activated event
 	self.Activated = Signal.new() -- :fire(ActionMaid, ActivateData)
 	self.Deactivated = Signal.new() -- :fire()
@@ -42,7 +45,7 @@ function BaseAction.new(Name)
 	
 	self.ContextActionKey = ("%s_ContextAction"):format(tostring(self.Name))
 	
-	-- Prevent being activated when disabled	
+	-- Prevent being activated when disabled
 	self.Maid:GiveTask(self.EnabledChanged:Connect(function(IsEnabled)
 		if not IsEnabled then
 			self:Deactivate()
@@ -133,5 +136,10 @@ function BaseAction:Activate(...)
 	end
 end
 
+function BaseAction:Destroy()
+	self.Maid:DoCleaning()
+
+	setmetatable(self, nil)
+end
 
 return BaseAction
