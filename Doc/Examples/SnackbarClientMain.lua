a353d16d3e955ba34f8c16c1bb87c255d928d19d
James Onnen <rbxquenty@gmail.com>
2017-12-23 16:14:21 -0600

    Update example

--- Doc/Examples/SnackbarClientMain.lua
+++ Doc/Examples/SnackbarClientMain.lua
@@ -1,11 +1,16 @@
-local ReplicatedStorage = game:GetService("ReplicatedStorage")
-local LoadCustomLibrary = require(ReplicatedStorage:WaitForChild("NevermoreEngine"))
+local require = require(game:GetService("ReplicatedStorage"):WaitForChild("NevermoreEngine"))
 
-
--- Do actual things
 local Players = game:GetService("Players")
+local UserInputService = game:GetService("UserInputServic")
 
-local SnackbarManager = LoadCustomLibrary("SnackbarManager").new()
+-- Load snackbar manager
+local SnackbarManager = require("SnackbarManager").new()
 	:WithPlayerGui(Players.LocalPlayer:WaitForChild("PlayerGui"))
 
+-- Create snackbar
 SnackbarManager:MakeSnackbar("Nevermore loaded!")
+
+-- Show snackbar after every input!
+UserInputService.InputBegan:Connect(function()
+	SnackbarManager:MakeSnackbar("New input happend!")
+end)
