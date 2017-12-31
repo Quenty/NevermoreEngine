ff92c03d0bc22d68d479531f9e61dbd6c77a61c1
James Onnen <rbxquenty@gmail.com>
2017-12-31 16:36:19 -0600

    Value object also has Maids and :Destroy() method if needed

--- Modules/Utility/ValueObject.lua
+++ Modules/Utility/ValueObject.lua
@@ -5,56 +5,66 @@
 local require = require(game:GetService("ReplicatedStorage"):WaitForChild("NevermoreEngine"))
 
 local Signal = require("Signal")
+local Maid = require("Maid")
 
 local ValueObject = {}
 ValueObject.ClassName = "ValueObject"
+
+--- The value of the ValueObject
+-- @tfield Variant Value
+
+--- Event fires when the value's object value change
+-- @signal Changed
+-- @tparam Variant newValue The new value
+-- @tparam Variant oldValue The old value
+
 
 --- Constructs a new value object
 -- @constructor
 -- @treturn ValueObject
 function ValueObject.new()
-	local self = setmetatable({}, ValueObject)
+	local self = {}
 	
-	--- The value of the ValueObject
-	-- @tfield Variant Value
 
-	--- Event fires when the value's object value change
-	-- @signal Changed
-	-- @tparam Variant NewValue The new value
-	-- @tparam Variant OldValue The old value
-	self.Changed = Signal.new() -- :Fire(NewValue, OldValue)
-	
-	return self
+	self.Changed = Signal.new() -- :Fire(newValue, oldValue, maid)
+
+	self._maid = Maid.new()
+	self._maid:GiveTask(self.Changed)
+
+	return setmetatable(self, ValueObject)
 end
 
-
-
-function ValueObject:__index(Index)
-	if Index == "Value" then
-		return self._Value
-	elseif Index == "_Value" then
-		return nil -- Edge case.
-	elseif ValueObject[Index] then
-		return ValueObject[Index]
+function ValueObject:__index(index)
+	if index == "Value" then
+		return self._value
+	elseif ValueObject[index] then
+		return ValueObject[index]
+	elseif index == "_value" then
+		return nil -- Edge case
 	else
-		error("'" .. tostring(Index) .. "' is not a member of ValueObject")
+		error(("'%s' is not a member of ValueObject"):format(tostring(index)))
 	end
 end
 
-function ValueObject:__newindex(Index, Value)
-	if Index == "Value" then
-		if self.Value ~= Value then
-			local Old = self.Value
-			self._Value = Value
-			self.Changed:Fire(Value, Old)
+function ValueObject:__newindex(index, value)
+	if index == "Value" then
+		local previous = rawget(self, "_value", value)
+		if previous ~= value then
+			rawset(self, "_value", value)
+
+			local maid = Maid.new()
+			self._maid._valueMaid = maid
+			self.Changed:Fire(value, previous, maid)
 		end
-	elseif Index == "_Value" then
-		rawset(self, Index, Value)
-	elseif Index == "Changed" then
-		rawset(self, Index, Value)
 	else
-		error("'" .. tostring(Index) .. "' is not a member of ValueObject")
+		error(("'%s' is not a member of ValueObject"):format(tostring(index)))
 	end
+end
+
+--- Forces the value to be nil on cleanup, cleans up the Maid
+function ValueObject:Destroy()
+	self.Value = nil
+	self._maid:DoCleaning()
 end
 
 return ValueObject
