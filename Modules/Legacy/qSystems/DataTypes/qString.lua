18833bbd4379b157cd779b7637b6086f9fbdfca4
James Onnen <rbxquenty@gmail.com>
2018-01-02 17:38:02 -0600

    Refactor qString API

--- Modules/Legacy/qSystems/DataTypes/qString.lua
+++ Modules/Legacy/qSystems/DataTypes/qString.lua
@@ -1,219 +1,76 @@
--- @author Quenty
--- Revised January 2nd, 2013
--- This script provides utility functions for strings
-
--- Change Log --
---- Removed VerifyArg methods
+--- This module provides utility functions for strings
 
 local lib = {}
 
-local function CompareStrings(firstString, secondString)
-	-- Compares two strings, ignoring capitals.
-	-- @return Boolean if the string found match
-
-	return string.lower(firstString) == string.lower(secondString);
-end
-lib.CompareStrings = CompareStrings;
-lib.compareStrings = CompareStrings;
-lib.compare_strings = CompareStrings;
-lib.Compare = CompareStrings
-lib.compare = CompareStrings
--- lib.Equals = CompareStrings
--- lib.equals = CompareStrings
-
-local function CompareCutFirst(firstString, secondString)
-	-- Compare's two strings, but only the beginning of the first string, to see if it matches the second one
-	-- @param firstString The string to "cut" off of
-	-- @param secondString The string to compare direct to
-	-- @return Boolean if a match or not
-
-	return string.lower(firstString):sub(1, #secondString) == string.lower(secondString);
-end
-lib.CompareCutFirst = CompareCutFirst;
-lib.compareCutFirst = CompareCutFirst;
-lib.compare_cut_first = CompareCutFirst;
-
-local function CutoutPrefix(firstString, prefix)
-
-	return firstString:sub(#prefix + 1);
-end
-
-local PatternCache = {}
-setmetatable(PatternCache, {__mode = "k"})
-
-local function BreakString(StringA, Seperator)
-	-- Tokenizer system.. In heinsight, 2 years later, Should really be a factory function. Urgh. 
-	-- @param Seperator The deliminator 
-
-	local Pattern = Seperator 
-	local ActualSeperator = Seperator 
-
-	if type(Seperator) ~= "string" then 
-		if PatternCache[Seperator] then
-			Pattern = PatternCache[Seperator]
-			ActualSeperator = Seperator[1]
-		else
-			Pattern = "[" 
-			for _, PotentialSeperator in pairs(Seperator) do 
-				if #PotentialSeperator == 1 then 
-					Pattern = Pattern.."%"..PotentialSeperator 
-					ActualSeperator = PotentialSeperator; 
-				else 
-					error("A Seperator must be 1 character, it was "..#PotentialSeperator.." characters long."); 
-				end 
-			end 
-			Pattern = Pattern.."]"; 
-			PatternCache[Seperator] = Pattern
-		end
-	elseif #Seperator ~= 1 then
-		error("Seperator must be 1  character, it was "..#Seperator.." characters long."); 
-	end 
-
-	local Parts = {} 
-	for NewString in string.gmatch(StringA..ActualSeperator, ".-"..Pattern) do 
-		if not (#NewString <= 1) then
-			Parts[#Parts+1] = NewString:sub(1, #NewString-1); 
-		-- else
-		-- 	print("Empty string: '"..NewString.."'")
-		end
-	end 
-
-	return Parts; 
-end
-lib.breakString = BreakString;
-lib.BreakString = BreakString;
-lib.break_string = BreakString;
-
-local function TrimString(str, pattern)
+function lib.Trim(str, pattern)
 	pattern = pattern or "%s";
 	-- %S is whitespaces
-	-- When we find the first non space character defined by ^%s 
-	-- we yank out anything in between that and the end of the string 
-	-- Everything else is replaced with %1 which is essentially nothing  
-
-	-- Credit Sorcus, Modified by Quenty
+	-- When we find the first non space character defined by ^%s
+	-- we yank out anything in between that and the end of the string
+	-- Everything else is replaced with %1 which is essentially nothing
 	return (str:gsub("^"..pattern.."*(.-)"..pattern.."*$", "%1"))
-end 
-lib.TrimString = TrimString
-lib.trimString = TrimString
-lib.trim_string = TrimString
+end
 
-local function TrimStringFront(str, pattern)
+function lib.ToCamelCase(str)
+	str = str:lower()
+	str = str:gsub("_(%a)", string.upper)
+	str = str:gsub("^%a", string.upper)
+	return str
+end
+
+-- Only trims the front of the string...
+function lib.TrimFront(str, pattern)
 	pattern = pattern or "%s";
-
-	-- Only trims the front of the string...
 	return (str:gsub("^"..pattern.."*(.-)"..pattern.."*", "%1"))
-end 
-lib.TrimStringFront = TrimStringFront
-lib.trimStringFront = TrimStringFront
-lib.trim_stream_front = TrimStringFront
+end
 
-local function GetRestOfSemiTokenizedString(StringA, Seperator, Counts)
-	-- Get's the rest of a string past a tokenizer count...
+function lib.CheckNumOfCharacterInString(str, char)
+	local count = 0
+	for _ in string.gmatch(str, char) do
+		count = count + 1
+	end
+	return count
+end
+--- Returns whether or not text is whitespace
+function lib.IsWhitespace(str)
+	return string.match(str, "[%s]+") == str
+end
 
-	-- Much hax.
+--- Converts text to have a ... after it if it's too long.
+function lib.ElipseLimit(str, characterLimit)
 
-	local Count = 0
-	local PartToCutOff = 0
-	local Pattern = Seperator 
-	local ActualSeperator = Seperator 
+	if #str > characterLimit then
+		str = str:sub(1, characterLimit-3).."..."
+	end
+	return str
+end
 
-	if type(Seperator) ~= "string" then 
-		if PatternCache[Seperator] then
-			Pattern = PatternCache[Seperator]
-			ActualSeperator = Seperator[1]
-		else
-			Pattern = "[" 
-			for _, PotentialSeperator in pairs(Seperator) do 
-				if #PotentialSeperator == 1 then 
-					Pattern = Pattern.."%"..PotentialSeperator 
-					ActualSeperator = PotentialSeperator; 
-				else 
-					error("A Seperator must be 1 character, it was "..#PotentialSeperator.." characters long."); 
-				end 
-			end 
-			Pattern = Pattern.."]"; 
-			PatternCache[Seperator] = Pattern
-		end
-	elseif #Seperator ~= 1 then
-		error("Seperator must be 1  character, it was "..#Seperator.." characters long."); 
+function lib.AddCommas(number)
+	if type(number) == "number" then
+		number = tostring(number)
 	end
 
-	for NewString in string.gmatch(StringA..ActualSeperator, ".-"..Pattern) do 
-		if not (#NewString <= 1) then
-			PartToCutOff = PartToCutOff + #NewString
-			Count = Count + 1
-			if Count >= Counts then
-				return TrimStringFront(StringA:sub(PartToCutOff), Pattern)
-			end
-		else
-			PartToCutOff = PartToCutOff + 1;
-		end
-	end
-end
-lib.GetRestOfSemiTokenizedString = GetRestOfSemiTokenizedString
-lib.getRestOfSemiTokenizedString = GetRestOfSemiTokenizedString
-lib.get_rest_of_semi_tokenized_string = GetRestOfSemiTokenizedString
+	local index = -1
 
-local function IsWhitespace(Text) 
-	--- Returns whether or not text is whitespace
-	
-	return string.match(Text, "[%s]+") == Text
-end
-lib.isWhitespace = IsWhitespace
-lib.IsWhitespace = IsWhitespace
-lib.is_whitespace = IsWhitespace
-
-local function DumbElipseLimit(Text, CharacterLimit)
-	--- Converts text to have a ... after it if it's too long.
-
-	if #Text > CharacterLimit then 
-		Text = Text:sub(1, CharacterLimit-3).."..." 
-	end 
-	return Text 
-end
-lib.DumbElipseLimit = DumbElipseLimit
-lib.dumbElipseLimit = DumbElipseLimit
-
-local function CheckNumOfCharacterInString(TheString, Character)
-	local Number = 0
-	for ID in string.gmatch(TheString, Character) do 
-		Number = Number + 1;
-	end
-	--print("Checked Num for \""..TheStr	ng.."\" with the Character \""..Character.."\" and got "..Number)
-	return Number;
-end
-lib.CheckNumOfCharacterInString = CheckNumOfCharacterInString
-lib.checkNumOfCharacterInString = CheckNumOfCharacterInString
-
-local function CommaValue(Amount)
-	local FormattedString = Amount
-	local Index
-
-	while true do  
-		FormattedString, Index = string.gsub(FormattedString, "^(-?%d+)(%d%d%d)", '%1,%2')
-		if Index==0 then
-			return FormattedString
-		end
+	while index ~= 0 do
+		number, index = string.gsub(number, "^(-?%d+)(%d%d%d)", '%1,%2')
 	end
 
-	return FormattedString
+	return number
 end
-lib.CommaValue = CommaValue
 
-
-local function GetRomanNumeral(Number)
-	--- Return's the Roman Numeral version of the number
-	-- @param Number The number to convert
-
+--- Return's the Roman Numeral version of the number
+-- @param Number The number to convert
+function lib.GetRomanNumeral(Number)
 	if type(Number) == "number" then
-		local Numbers = {1000; 900; 500; 400; 100; 90; 50; 40; 10; 9; 5; 4; 1;};
+		local Numbers = {1000; 900; 500; 400; 100; 90; 50; 40; 10; 9; 5; 4; 1;}
 		local Numerals = {"M"; "CM"; "D"; "CD"; "C"; "XC"; "L"; "XL"; "X"; "IX"; "V"; "IV"; "I";}
-		local Result = "";
+		local Result = ""
+
 		if Number < 0 or Number >= 4000 then
-			return nil;
+			return nil
 		elseif Number == 0 then
-			return "N";
+			return "N"
 		else
 			for Index=1, 13 do
 				while Number >= Numbers[Index] do
@@ -222,10 +79,9 @@ local function GetRomanNumeral(Number)
 				end
 			end
 		end
-		return Result;
+		return Result
 	elseif type(Number == "string") then
-		Number = string.upper(Number);
-		local Result = 0;
+		Number = string.upper(Number)
 
 		local RomanDigit = {
 			["I"] = 1;
@@ -243,84 +99,79 @@ local function GetRomanNumeral(Number)
 			["C"] = 100;
 		}
 
-		if Number == "N" then 
-			return 0;
-		elseif CheckNumOfCharacterInString(Number, "V") >= 2 or CheckNumOfCharacterInString(Number, "L") >= 2 or CheckNumOfCharacterInString(Number, "D") >= 2 then --(#{string.find(Number, "V*.V")} >= 2) or (#{string.find(Number, "L*.L")} >= 2) or (#{string.find(Number, "D*.D")} >= 2) then
-			print("Rule 4");
-			return nil; 
+		if Number == "N" then
+			return 0
+		elseif lib.CheckNumOfCharacterInString(Number, "V") >= 2
+			or lib.CheckNumOfCharacterInString(Number, "L") >= 2
+			or lib.CheckNumOfCharacterInString(Number, "D") >= 2 then
+			return nil -- Rule 4
 		end
 
 		local Last = "Z"
-		local Count = 1;
+		local Count = 1
 
 		for i=1, #Number do
 			local Numeral = string.sub(Number, i, i)
 			if not RomanDigit[Numeral] then
-				print("Invalid Numeral");
-				return nil;
+				return nil -- Invalid numeral
 			end
 			if Numeral == Last then
-				Count = Count + 1;
+				Count = Count + 1
 				if Count >= 4 then
-					print("Rule 4 (Second check)");
-					return nil;
+					return nil -- Rule 4
 				end
 			else
-				Count = 1;
+				Count = 1
 			end
 			Last = Numeral
 		end
 
-		local Pointer = 1;
+		local Pointer = 1
 		local Values = {}
-		local MaxDigit = 1000;
+		local MaxDigit = 1000
 
 		while Pointer <= #Number do
 			local Numeral = string.sub(Number, Pointer, Pointer)
 			local Digit = RomanDigit[Numeral]
 
 			if Digit > MaxDigit then
-				print("Rule 3");
-				return nil;
+				return nil -- Rule 3
 			end
 
-			local NextDigit = 0;
 			if Pointer <= #Number - 1 then
 				local NextNumeral = string.sub(Number, Pointer+1, Pointer+1);
-				NextDigit = RomanDigit[NextNumeral]
+				local NextDigit = RomanDigit[NextNumeral]
 
 				if NextDigit > Digit then
-					if (not SpecialRomanDigit[Numeral]) or NextDigit > (Digit * 10) or CheckNumOfCharacterInString(Number, Numeral) > 3 then --(#{string.find(Number, Numeral.."*."..Numeral)} >= 3) then
-						print("Rule 3 (Second check)");
-						return nil;
+					if (not SpecialRomanDigit[Numeral])
+							or NextDigit > (Digit * 10)
+						or lib.CheckNumOfCharacterInString(Number, Numeral) > 3 then
+						return nil -- Rule 3
 					end
-					MaxDigit = Digit - 1;
+					MaxDigit = Digit - 1
 					Digit = NextDigit - Digit
-					Pointer = Pointer + 1;
+					Pointer = Pointer + 1
 				end
 			end
 
 			Values[#Values + 1] = Digit
-			Pointer = Pointer + 1;
+			Pointer = Pointer + 1
 		end
 
-		--print("#Values = "..#Values)
 		for Index = 1, #Values-1 do
 			print(Index.." : "..Values[Index])
 			if Values[Index] < Values[Index + 1] then
-				print("Rule 5");
-				return nil;
+				print("Rule 5")
+				return nil
 			end
 		end
 
-		local Total = 0;
-		for Index, Digit in pairs(Values) do
-			Total = Total + Digit;
+		local Total = 0
+		for _, Digit in pairs(Values) do
+			Total = Total + Digit
 		end
-		return Total;
+		return Total
 	end
 end
-lib.GetRomanNumeral = GetRomanNumeral
-lib.getRomanNumeral = GetRomanNumeral
 
 return lib
