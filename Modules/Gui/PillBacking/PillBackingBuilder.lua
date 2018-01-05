4a954fabec1835db50fa11895c716877ccd7e923
James Onnen <rbxquenty@gmail.com>
2018-01-05 16:29:07 -0600

    Add shadows to PillBackingBuilder, expose better API

--- Modules/Gui/PillBacking/PillBackingBuilder.lua
+++ Modules/Gui/PillBacking/PillBackingBuilder.lua
@@ -1,65 +1,208 @@
 --- Builds a pill backing for Guis
 -- @classmod PillBackingBuilder
 
+local require = require(game:GetService("ReplicatedStorage"):WaitForChild("Nevermore"))
+
+local Table = require("Table")
+
 local PillBackingBuilder = {}
 PillBackingBuilder.__index = PillBackingBuilder
 PillBackingBuilder.ClassName = "PillBackingBuilder"
+PillBackingBuilder.CIRCLE_IMAGE_ID = "rbxassetid://633244888" -- White circle
+PillBackingBuilder.CIRCLE_SIZE = Vector2.new(256, 256)
+PillBackingBuilder.SHADOW_IMAGE_ID = "rbxassetid://707852973"
+PillBackingBuilder.SHADOW_SIZE = Vector2.new(128, 128)
+PillBackingBuilder.PILL_SHADOW_IMAGE_ID = "rbxassetid://1304004290"
+PillBackingBuilder.PILL_SHADOW_SIZE = Vector2.new(256, 128)
+PillBackingBuilder.SHADOW_OFFSET_Y = UDim.new(0.05, 0)
+PillBackingBuilder.SHADOW_TRANSPARENCY = 0.7
 
-function PillBackingBuilder.new()
+function PillBackingBuilder.new(options)
 	local self = setmetatable({}, PillBackingBuilder)
+
+	self._options = options or {}
 
 	return self
 end
 
-function PillBackingBuilder:CreateRoundedParts(gui, options)
-	options = options or {}
-
-	local left = Instance.new("ImageLabel")
-	left.SizeConstraint = Enum.SizeConstraint.RelativeYY
-	left.Size = UDim2.new(0.5, 0, 1, 0)
-	left.AnchorPoint = Vector2.new(1, 0.5)
-	left.Position = UDim2.new(0, 0, 0.5, 0)
-	left.Image = "rbxassetid://633244888"
-	left.ImageColor3 = gui.BackgroundColor3
-	left.ImageRectSize = Vector2.new(128, 256) -- Half circle
-	left.Name = "HalfCircle"
-	left.ZIndex = options.ZIndex or (gui.ZIndex - 1)
-	left.BackgroundTransparency = 1
-
-	local right = left:Clone()
-	right.AnchorPoint = Vector2.new(0, 0.5)
-	right.Position = UDim2.new(1, 0, 0.5, 0)
-	right.ImageRectOffset = Vector2.new(128, 0)
-
-	right.Parent = gui
-	left.Parent = gui
-
-	return {left, right}
-end
-
 function PillBackingBuilder:Create(gui, options)
-	options = options or {}
-
-	local zindex = options.ZIndex or (gui.ZIndex - 1)
+	options = self:_configureOptions(gui, options)
 	local diameter = gui.Size.Y
 
-	local background = Instance.new("Frame")
-	background.Name = "RoundedBackground"
-	background.BackgroundColor3 = gui.BackgroundColor3
-	background.BorderSizePixel = 0
-	background.AnchorPoint = Vector2.new(0.5, 0.5)
-	background.Position = UDim2.new(0.5, 0, 0.5, 0)
-	background.Size = UDim2.new(1 - diameter.Scale, -diameter.Offset, 1, 0)
-	background.ZIndex = zindex
+	local pillBacking = Instance.new("Frame")
+	pillBacking.AnchorPoint = Vector2.new(0.5, 0.5)
+	pillBacking.BackgroundColor3 = options.BackgroundColor3
+	pillBacking.BorderSizePixel = 0
+	pillBacking.Name = "PillBacking"
+	pillBacking.Position = UDim2.new(0.5, 0, 0.5, 0)
+	pillBacking.Size = UDim2.new(1 - diameter.Scale, -diameter.Offset, 1, 0)
+	pillBacking.ZIndex = options.ZIndex
 
-	self:CreateRoundedParts(background, {
-		ZIndex = zindex;
-	})
+	local left = self:_createLeft(options)
+	local right = self:_createRight(options)
+	right.Parent = pillBacking
+	left.Parent = pillBacking
 
 	gui.BackgroundTransparency = 1
-	background.Parent = gui
+	pillBacking.Parent = gui
 
-	return background
+	return pillBacking
+end
+
+function PillBackingBuilder:CreateShadow(gui, options)
+	options = self:_configureOptions(gui, options)
+	local diameter = gui.Size.Y
+
+	local shadow = self:_createPillShadow(options)
+	shadow.SizeConstraint = Enum.SizeConstraint.RelativeXY
+	shadow.AnchorPoint = Vector2.new(0.5, 0.5)
+	shadow.Name = "PillShadow"
+	shadow.ImageRectSize = self.PILL_SHADOW_SIZE * Vector2.new(0.5, 1)
+	shadow.ImageRectOffset = self.PILL_SHADOW_SIZE * Vector2.new(0.25, 0)
+	shadow.Position = UDim2.new(UDim.new(0.5, 0), UDim.new(0.5, 0) + self.SHADOW_OFFSET_Y)
+	shadow.Size = UDim2.new(1 - diameter.Scale, -diameter.Offset, 2, 0)
+	shadow.ZIndex = options.ShadowZIndex
+
+	local left = self:_createLeftShadow(options)
+	local right = self:_createRightShadow(options)
+	right.Parent = shadow
+	left.Parent = shadow
+
+	shadow.Parent = gui
+
+	return shadow
+end
+
+function PillBackingBuilder:CreateCircle(gui, options)
+	options = self:_configureOptions(gui, options)
+	local circle = self:_createCircle(options)
+
+	gui.BackgroundTransparency = 1
+	circle.Parent = gui
+
+	return circle
+end
+
+function PillBackingBuilder:CreateCircleShadow(gui, options)
+	options = self:_configureOptions(gui, options)
+	local shadow = Instance.new("ImageLabel")
+	shadow.AnchorPoint = Vector2.new(0.5, 0.5)
+	shadow.BackgroundTransparency = 1
+	shadow.Image = self.SHADOW_IMAGE_ID
+	shadow.ImageTransparency = self.SHADOW_TRANSPARENCY
+	shadow.Name = "CircleShadow"
+	shadow.Position = UDim2.new(UDim.new(0.5, 0), UDim.new(0.5, 0) + self.SHADOW_OFFSET_Y)
+	shadow.Size = UDim2.new(2, 0, 2, 0)
+	shadow.SizeConstraint = Enum.SizeConstraint.RelativeYY
+
+	shadow.ZIndex = options.ShadowZIndex
+
+	shadow.Parent = gui
+
+	return shadow
+end
+
+function PillBackingBuilder:CreateLeft(gui, options)
+	options = self:_configureOptions(gui, options)
+	local left = self:_createLeft(options)
+	left.Parent = gui
+	return left
+end
+
+function PillBackingBuilder:CreateRight(gui, options)
+	options = self:_configureOptions(gui, options)
+	local right = self:_createRight(options)
+	right.Parent = gui
+	return right
+end
+
+
+function PillBackingBuilder:_createLeft(options)
+	local left = self:_createCircle(options)
+	left.AnchorPoint = Vector2.new(1, 0.5)
+	left.ImageRectSize = self.CIRCLE_SIZE * Vector2.new(0.5, 1)
+	left.Name = "LeftHalfCircle"
+	left.Position = UDim2.new(0, 0, 0.5, 0)
+	left.Size = UDim2.new(0.5, 0, 1, 0)
+
+	return left
+end
+
+function PillBackingBuilder:_createRight(options)
+	options = self:_configureOptions(options)
+
+	local right = self:_createCircle(options)
+	right.AnchorPoint = Vector2.new(0, 0.5)
+	right.ImageRectOffset = self.CIRCLE_SIZE * Vector2.new(0.5, 0)
+	right.ImageRectSize = self.CIRCLE_SIZE * Vector2.new(0.5, 1)
+	right.Name = "RightHalfCircle"
+	right.Position = UDim2.new(1, 0, 0.5, 0)
+	right.Size = UDim2.new(0.5, 0, 1, 0)
+
+	return right
+end
+
+function PillBackingBuilder:_createLeftShadow(options)
+	local left = self:_createPillShadow(options)
+	left.AnchorPoint = Vector2.new(1, 0.5)
+	left.ImageRectSize = self.PILL_SHADOW_SIZE * Vector2.new(0.25, 1)
+	left.Name = "LeftShadow"
+	left.Position = UDim2.new(0, 0, 0.5, 0)
+	left.Size = UDim2.new(0.5, 0, 1, 0)
+
+	return left
+end
+
+function PillBackingBuilder:_createRightShadow(options)
+	local right = self:_createPillShadow(options)
+	right.AnchorPoint = Vector2.new(0, 0.5)
+	right.ImageRectOffset = self.PILL_SHADOW_SIZE * Vector2.new(0.75, 0)
+	right.ImageRectSize = self.PILL_SHADOW_SIZE * Vector2.new(0.25, 1)
+	right.Name = "RightShadow"
+	right.Position = UDim2.new(1, 0, 0.5, 0)
+	right.Size = UDim2.new(0.5, 0, 1, 0)
+
+	return right
+end
+
+function PillBackingBuilder:_createCircle(options)
+	local circle = Instance.new("ImageLabel")
+	circle.BackgroundTransparency = 1
+	circle.Image = self.CIRCLE_IMAGE_ID
+	circle.Name = "Circle"
+	circle.Size = UDim2.new(1, 0, 1, 0)
+	circle.SizeConstraint = Enum.SizeConstraint.RelativeYY
+
+	-- set options
+	circle.ImageColor3 = options.BackgroundColor3
+	circle.ZIndex = options.ZIndex
+
+	return circle
+end
+
+function PillBackingBuilder:_createPillShadow(options)
+	local shadow = Instance.new("ImageLabel")
+	shadow.BackgroundTransparency = 1
+	shadow.Image = self.PILL_SHADOW_IMAGE_ID
+	shadow.ImageTransparency = self.SHADOW_TRANSPARENCY
+	shadow.Name = "PillShadow"
+	shadow.Size = UDim2.new(1, 0, 1, 0)
+	shadow.SizeConstraint = Enum.SizeConstraint.RelativeYY
+
+	shadow.ZIndex = options.ShadowZIndex
+
+	return shadow
+end
+
+function PillBackingBuilder:_configureOptions(gui, options)
+	assert(gui)
+
+	options = Table.Copy(options or self._options)
+	options.ZIndex = options.ZIndex or gui.ZIndex
+	options.ShadowZIndex = options.ShadowZIndex or options.ZIndex - 1
+	options.BackgroundColor3 = options.BackgroundColor3 or gui.BackgroundColor3
+
+	return options
 end
 
 return PillBackingBuilder
