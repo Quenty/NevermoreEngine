13c667cbab9701d7732bce58114f657bb6e74b45
James Onnen <rbxquenty@gmail.com>
2018-01-08 11:20:11 -0600

    Remove Weld to Joint library

---
+++ Modules/Joint/Joint.lua
@@ -0,0 +1,36 @@
+--- General joint utilities
+-- @module Weld
+
+local lib = {}
+
+--- Weld's 2 parts together
+-- @param Part0 The first part
+-- @param Part1 The second part (Dependent part most of the time).
+-- @param[opt="Weld"] jointType The type of joint.
+-- @param[opt=Part0] parent Parent of the weld, Defaults to Part0 (so GC is better).
+-- @return The weld created.
+function lib.Weld(part0, part1, jointType, parent)
+	local weld = Instance.new(jointType or "Weld")
+	weld.Part0 = part0
+	weld.Part1 = part1
+	weld.C0 = CFrame.new()
+	weld.C1 = part1.CFrame:toObjectSpace(part0.CFrame)
+	weld.Parent = parent or part0
+
+	return weld
+end
+
+---
+-- @param Parts The Parts to weld. Should be anchored to prevent really horrible results.
+-- @param MainPart The part to weld the model to (can be in the model).
+-- @param[opt="Weld"] jointType The type of joint
+function lib.WeldParts(parts, mainPart, jointType)
+	for _, part in pairs(parts) do
+		if part ~= mainPart then
+			lib.Weld(mainPart, part, jointType, mainPart)
+		end
+	end
+end
+
+
+return lib
--- Modules/Legacy/qCFrame/qCFrame.lua
+++ Modules/Legacy/qCFrame/qCFrame.lua
@@ -119,43 +119,6 @@ function lib.PointInsidePart(part, Point)
 	return true
 end
 
---- Weld's 2 parts together
--- @param Part0 The first part
--- @param Part1 The second part (Dependent part most of the time).
--- @param[opt="Weld"] jointType The type of joint.
--- @param[opt=Part0] parent Parent of the weld, Defaults to Part0 (so GC is better).
--- @return The weld created.
-function lib.WeldTogether(part0, part1, jointType, parent)
-	local weld = Instance.new(jointType or "Weld")
-	weld.Part0 = part0
-	weld.Part1 = part1
-	weld.C0 = CFrame.new()
-	weld.C1 = part1.CFrame:toObjectSpace(part0.CFrame)
-	weld.Parent = parent or part0
-
-	return weld
-end
-
----
--- @param Parts The Parts to weld. Should be anchored to prevent really horrible results.
--- @param MainPart The part to weld the model to (can be in the model).
--- @param[opt="Weld"] jointType The type of joint
--- @tparam[opt=false] boolean doNotUnanchor, if true, will not unachor the model after cmopletion.
-function lib.WeldParts(parts, mainPart, jointType, doNotUnanchor)
-	for _, part in pairs(parts) do
-		if part ~= mainPart then
-			lib.WeldTogether(mainPart, part, jointType, mainPart)
-		end
-	end
-
-	if not doNotUnanchor then
-		for _, part in pairs(parts) do
-			part.Anchored = false
-		end
-		mainPart.Anchored = false
-	end
-end
-
 --- Returns unit vector of the surface normal given a point on the surface of part
 function lib.GetSurfaceNormal(Part, Vector)
 	local Percent = Part.CFrame:toObjectSpace(CFrame.new(Vector)).p / ((Part.Size  - Vector3.new(0.02, 0.02, 0.02))/ 2)
