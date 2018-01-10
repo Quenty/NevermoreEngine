9ed4ac8975197f47a2b5cdb04021164f4e8846c9
James Onnen <rbxquenty@gmail.com>
2018-01-10 01:11:34 -0600

    Add additional character util methods, move methods to Humanoid util

---
+++ Modules/Character/HumanoidUtil.lua
@@ -0,0 +1,35 @@
+--- General humanoid utility code.
+-- @module HumanoidUtil
+-- @alias lib
+
+local lib = {}
+
+--- Retrieves a humanomid from a descendant (Players only).
+-- @param descendant Child of a humanoid model, like a limb
+-- @return Humanoid
+function lib.GetHumanoid(descendant)
+	local character = descendant
+	while character do
+		local humanoid = character:FindFirstChildOfClass("Humanoid")
+		if humanoid then
+			return humanoid
+		end
+		character = character.Parent
+	end
+
+	return nil
+end
+
+--- Forcefully unseats the humanoid. Useful when teleporting humanoid
+function lib.ForceUnseatHumanoid(humanoid)
+	if humanoid.SeatPart then
+		local seatWeld = humanoid.SeatPart:FindFirstChild("SeatWeld")
+		if seatWeld then
+			seatWeld:Destroy()
+		end
+	end
+
+	humanoid.Sit = false
+end
+
+return lib
