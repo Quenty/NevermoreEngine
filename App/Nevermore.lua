7a4152b2f929797dacf3ad4c87e9624431db9f99
James Onnen <rbxquenty@gmail.com>
2018-01-10 11:52:04 -0600

    Refactor Nevermore module

--- App/Nevermore.lua
+++ App/Nevermore.lua
@@ -16,198 +16,194 @@ assert(script.Parent == ReplicatedStorage, "Invalid parent. For Nevermore to wor
 	"a ModuleScript named \"Nevermore\" parented to ReplicatedStorage")
 
 --- Handles yielded operations by caching the retrieval process
-local function _handleRetrieving(Retrieving, Function, Argument)
-	assert(type(Retrieving) == "table", "Error: Retrieving must be a table")
-	assert(type(Function) == "function", "Error: Function must be a function")
+local function handleRetrieving(retrieving, func, argument)
+	assert(type(retrieving) == "table", "Error: retrieving must be a table")
+	assert(type(func) == "function", "Error: func must be a function")
 
-	local Signal = Instance.new("BindableEvent")
-	local Result
-	Retrieving[Argument] = function()
-		if Result ~= nil and Result then
-			return Result
+	local bindableEvent = Instance.new("BindableEvent")
+	local result
+	retrieving[argument] = function()
+		if result ~= nil and result then
+			return result
 		end
 
-		Signal.Event:Wait()
-		return Result
+		bindableEvent.Event:Wait()
+		return result
 	end
 
-	Result = Function(Argument)
-	assert(Result ~= nil, "Result cannot be nil")
-	Retrieving[Argument] = nil
-	Signal:Fire()
-	Signal:Destroy()
+	result = func(argument)
+	assert(result ~= nil, "result cannot be nil")
+	retrieving[argument] = nil
+	bindableEvent:Fire()
+	bindableEvent:Destroy()
 
-	return Result
+	return result
 end
 
 --- Caches single argument, single output only
-local function _asyncCache(Function)
-	assert(type(Function) == "function", "Error: Function must be a userdata")
+local function asyncCache(func)
+	assert(type(func) == "function", "Error: func must be a userdata")
 
-	local Cache = {}
-	local Retrieving = {}
+	local cache = {}
+	local retrieving = {}
 
-	return function(Argument)
-		assert(Argument ~= nil, "Error: ARgument ")
-		if Cache[Argument] ~= nil then
-			return Cache[Argument]
-		elseif Retrieving[Argument] then
-			return Retrieving[Argument]()
+	return function(argument)
+		assert(argument ~= nil, "Error: ARgument ")
+		if cache[argument] ~= nil then
+			return cache[argument]
+		elseif retrieving[argument] then
+			return retrieving[argument]()
 		else
-			Cache[Argument] = _handleRetrieving(Retrieving, Function, Argument)
-			return Cache[Argument]
+			cache[argument] = handleRetrieving(retrieving, func, argument)
+			return cache[argument]
 		end
 	end
 end
 
 --- Retrieves an instance from a parent
-local function _retrieve(Parent, ClassName)
-	assert(type(ClassName) == "string", "Error: ClassName must be a string")
-	assert(typeof(Parent) == "Instance", ("Error: Parent must be an Instance, got '%s'"):format(typeof(Parent)))
+local function retrieve(parent, className)
+	assert(type(className) == "string", "Error: className must be a string")
+	assert(typeof(parent) == "Instance", ("Error: parent must be an Instance, got '%s'"):format(typeof(parent)))
 
-	return RunService:IsServer() and function(Name)
-		local Item = Parent:FindFirstChild(Name)
-		if not Item then
-			Item = Instance.new(ClassName)
-			Item.Archivable = false
-			Item.Name = Name
-			Item.Parent = Parent
+	return RunService:IsServer() and function(name)
+		local rbxObject = parent:FindFirstChild(name)
+		if not rbxObject then
+			rbxObject = Instance.new(className)
+			rbxObject.Archivable = false
+			rbxObject.Name = name
+			rbxObject.Parent = parent
 		end
-		return Item
-	end or function(Name)
-		local Resource = Parent:WaitForChild(Name, 5)
+		return rbxObject
+	end or function(name)
+		local resource = parent:WaitForChild(name, 5)
 
-		if Resource then
-			return Resource
+		if resource then
+			return resource
 		end
 
 		warn(("Warning: No '%s' found, be sure to require '%s' on the server. Yielding for '%s'")
-			:format(tostring(Name), tostring(Name), ClassName))
+			:format(tostring(name), tostring(name), className))
 
-		return Parent:WaitForChild(Name)
+		return parent:WaitForChild(name)
 	end
 end
 
-local function _getRepository(GetSubFolder)
+local function getRepository(getSubFolder)
 	if RunService:IsServer() then
-		local RepositoryFolder = ServerScriptService:FindFirstChild("Nevermore")
+		local repositoryFolder = ServerScriptService:FindFirstChild("Nevermore")
 
-		if not RepositoryFolder then
+		if not repositoryFolder then
 			warn("Warning: No repository of Nevermore modules found (Expected in ServerScriptService with name \"Nevermore\")" ..
 				". Library retrieval will fail.")
 
-			RepositoryFolder = Instance.new("Folder")
-			RepositoryFolder.Name = "Nevermore"
+			repositoryFolder = Instance.new("Folder")
+			repositoryFolder.Name = "Nevermore"
 		end
 
-		return RepositoryFolder
+		return repositoryFolder
 	else
-		return GetSubFolder("Modules")
+		return getSubFolder("Modules")
 	end
 end
 
-local function _getLibraryCache(RepositoryFolder)
-	local LibraryCache = {}
+local function getLibraryCache(repositoryFolder)
+	local libCache = {}
 
-	for _, Child in pairs(RepositoryFolder:GetDescendants()) do
-		if Child:IsA("ModuleScript") and not Child:FindFirstAncestorOfClass("ModuleScript") then
-			if LibraryCache[Child.Name] then
-				warn(("Warning: Duplicate name of '%s' already exists! Using first found!"):format(Child.Name))
+	for _, child in pairs(repositoryFolder:GetDescendants()) do
+		if child:IsA("ModuleScript") and not child:FindFirstAncestorOfClass("ModuleScript") then
+			if libCache[child.Name] then
+				warn(("Warning: Duplicate name of '%s' already exists! Using first found!"):format(child.Name))
 			end
 
-			LibraryCache[Child.Name] = Child
+			libCache[child.Name] = child
 		end
 	end
 
-	return LibraryCache
+	return libCache
 end
 
-local function _replicateRepository(ReplicationFolder, LibraryCache)
-	for Name, Library in pairs(LibraryCache) do
-		if not Name:lower():find("server") then
-			Library.Parent = ReplicationFolder
+local function replicateRepository(replicationFolder, libCache)
+	for name, library in pairs(libCache) do
+		if not name:lower():find("server") then
+			library.Parent = replicationFolder
 		end
 	end
 end
 
-local function _debugLoading(Function)
-	local Count = 0
-	local RequestDepth = 0
+local function debugLoading(func)
+	local count = 0
+	local depth = 0
 
-	return function(Module, ...)
-		Count = Count + 1
-		local LibraryID = Count
+	return function(module, ...)
+		count = count + 1
+		local LibraryID = count
 		local StartTime = tick()
 
 		if DEBUG_MODE then
-			print(("\t"):rep(RequestDepth), LibraryID, "Loading: ", Module)
-			RequestDepth = RequestDepth + 1
+			print(("\t"):rep(depth), LibraryID, "Loading: ", module)
+			depth = depth + 1
 		end
 
-		local Result = Function(Module, ...)
+		local result = func(module, ...)
 
 		if DEBUG_MODE then
-			RequestDepth = RequestDepth - 1
-			print(("\t"):rep(RequestDepth), LibraryID, "Done loading: ", Module, "in", tick() - StartTime)
+			depth = depth - 1
+			print(("\t"):rep(depth), LibraryID, "Done loading: ", module, "in", tick() - StartTime)
 		end
 
-		return Result
+		return result
 	end
 end
 
-local function _getLibraryLoader(LibraryCache)
-
-	--- Loads a library from Nevermore's library cache
-	-- @param Module The name of the library or a module
-	-- @return The library's value
-	return function(Module)
-		if typeof(Module) == "Instance" and Module:IsA("ModuleScript") then
-			return require(Module)
-		elseif type(Module) == "string" then
-			local ModuleScript = LibraryCache[Module] or error("Error: Library '" .. Module .. "' does not exist.", 2)
-			return require(ModuleScript)
+local function getLibraryLoader(libCache)
+	return function(module)
+		if typeof(module) == "Instance" and module:IsA("ModuleScript") then
+			return require(module)
+		elseif type(module) == "string" then
+			local moduleScript = libCache[module] or error("Error: Library '" .. module .. "' does not exist.", 2)
+			return require(moduleScript)
 		else
-			error(("Error: Module must be a string or ModuleScript, got '%s'"):format(typeof(Module)))
+			error(("Error: module must be a string or ModuleScript, got '%s' for '%s'"):format(typeof(module), tostring(module)))
 		end
 	end
 end
 
-local ResourceFolder = _retrieve(ReplicatedStorage, "Folder")("NevermoreResources")
-local GetSubFolder = _retrieve(ResourceFolder, "Folder")
-local RepositoryFolder = _getRepository(GetSubFolder)
-local LibraryCache = _getLibraryCache(RepositoryFolder)
+local resourceFolder = retrieve(ReplicatedStorage, "Folder")("NevermoreResources")
+local getSubFolder = retrieve(resourceFolder, "Folder")
+local repositoryFolder = getRepository(getSubFolder)
+local libCache = getLibraryCache(repositoryFolder)
 if RunService:IsServer() and not RunService:IsClient() then -- Don't move in SoloTestMode
-	_replicateRepository(GetSubFolder("Modules"), LibraryCache)
+	replicateRepository(getSubFolder("Modules"), libCache)
 end
 
-local Nevermore = {}
+local lib = {}
 
---- Load a library through Nevermore
+--- Load a library
 -- @function LoadLibrary
 -- @tparam Variant LibraryName Can either be a ModuleScript or string
 -- @treturn Variant Library
-Nevermore.LoadLibrary = _asyncCache(_debugLoading(_getLibraryLoader(LibraryCache)))
-Nevermore.require = Nevermore.LoadLibrary
+lib.LoadLibrary = asyncCache(debugLoading(getLibraryLoader(libCache)))
+lib.require = lib.LoadLibrary
 
 --- Get a remote event
 -- @function GetRemoteEvent
 -- @tparam string RemoteEventName
 -- @treturn RemoteEvent RemoteEvent
-Nevermore.GetRemoteEvent = _asyncCache(_retrieve(GetSubFolder("RemoteEvents"), "RemoteEvent"))
+lib.GetRemoteEvent = asyncCache(retrieve(getSubFolder("RemoteEvents"), "RemoteEvent"))
 
 --- Get a remote function
 -- @function GetRemoteFunction
 -- @tparam string RemoteFunctionName
 -- @treturn RemoteFunction RemoteFunction
-Nevermore.GetRemoteFunction = _asyncCache(_retrieve(GetSubFolder("RemoteFunctions"), "RemoteFunction"))
+lib.GetRemoteFunction = asyncCache(retrieve(getSubFolder("RemoteFunctions"), "RemoteFunction"))
 
-setmetatable(Nevermore, {
+setmetatable(lib, {
 	__call = function(self, ...)
 		return self.LoadLibrary(...)
 	end;
-	__index = function(self, Index)
-		return Nevermore.LoadLibrary(Index)
+	__index = function(self, index)
+		return lib.LoadLibrary(index)
 	end;
 })
 
-return Nevermore
+return lib
