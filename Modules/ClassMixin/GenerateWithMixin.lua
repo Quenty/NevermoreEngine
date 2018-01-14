392916b6cf3d9cf8bf21b49848266fc397ea995d
James Onnen <rbxquenty@gmail.com>
2018-01-13 22:06:54 -0600

    Update GenerateWith mixin to avoid showing warnings

--- Modules/ClassMixin/GenerateWithMixin.lua
+++ Modules/ClassMixin/GenerateWithMixin.lua
@@ -1,6 +1,10 @@
 --- Simple mixin to generate code for a class
 -- @module GenerateWithMixin
 
+local require = require(game:GetService("ReplicatedStorage"):WaitForChild("Nevermore"))
+
+local String = require("String")
+
 local module = {}
 
 --- Adds the GenerateWith API to the class
@@ -8,7 +12,6 @@ local module = {}
 -- @tparam[opt] table staticResources If provided, these resources are added to the class automatically
 function module:Add(class, staticResources)
 	assert(class)
-	assert(not class.GenerateWith)
 
 	class.GenerateWith = self.GenerateWith
 
@@ -19,24 +22,24 @@ end
 
 --- Generates resources
 -- @tparam table resources Resources to add
-function module:GenerateWith(resources)
+function module.GenerateWith(class, resources)
 	assert(type(resources) == "table")
 
 	for _, resourceName in ipairs(resources) do
-		local storeName = ("_%s"):format(resourceName:sub(1, 1):lower() .. resourceName:sub(2, #resourceName))
+		local storeName = String.ToPrivateCase(resourceName)
 
-		self[("With%s"):format(resourceName)] = function(self, resource)
+		class[("With%s"):format(resourceName)] = function(self, resource)
 			self[storeName] = resource or error(("Failed to set '%s', %s"):format(resourceName, tostring(resource)))
 			self[resourceName] = resource -- inject publically too, for now
 			return self
 		end
 
-		self[("Get%s"):format(resourceName)] = function(self)
+		class[("Get%s"):format(resourceName)] = function(self)
 			return self[storeName]
 		end
 	end
 
-	return self
+	return class
 end
 
 return module
