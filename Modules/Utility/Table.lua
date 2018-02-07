45aa5b86553211dc00f189384c39df1bd5d73542
James Onnen <rbxquenty@gmail.com>
2018-02-07 09:56:11 -0600

    ReadOnly errors at location of index

--- Modules/Utility/Table.lua
+++ Modules/Utility/Table.lua
@@ -141,10 +141,10 @@ end
 function lib.ReadOnly(table)
 	return setmetatable(table, {
 		__index = function(self, index)
-			error(("Bad index '%s'"):format(tostring(index)))
+			error(("Bad index '%s'"):format(tostring(index)), 2)
 		end;
 		__newindex = function(self, index, value)
-			error(("Bad index '%s'"):format(tostring(index)))
+			error(("Bad index '%s'"):format(tostring(index)), 2)
 		end;
 	})
 end
