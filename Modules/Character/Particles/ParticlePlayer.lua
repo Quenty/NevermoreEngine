994c238e255b8692983db97d520e2d8803c7052e
James Onnen <rbxquenty@gmail.com>
2018-02-09 19:50:33 -0600

    Refactor ParticlePlayer

--- Modules/Character/Particles/ParticlePlayer.lua
+++ Modules/Character/Particles/ParticlePlayer.lua
@@ -14,81 +14,79 @@ function ParticlePlayer.new()
 	return self
 end
 
-function ParticlePlayer:PlayLevelUpEffect(Humanoid)
-	return self:PlayHumanoidEffect(Humanoid, ReplicatedStorage.Particles.LevelUpEffect)
-end
-
-function ParticlePlayer:PlayDescendantsOnce(Parent)
-	local LongestLife = 0
-
-	for _, Item in pairs(Parent:GetDescendants()) do
-		if Item:IsA("ParticleEmitter") then
-			Item:Emit(Item.Rate)
-			LongestLife = math.max(LongestLife, Item.Lifetime.Max)
-		end
-	end
-
-	return LongestLife
-end
-
-function ParticlePlayer:PlayHumanoidEffect(Humanoid, EffectTemplate)
-	if not Humanoid then
-		warn("[ParticlePlayer] - No Humanoid")
+function ParticlePlayer:PlayLevelUpEffect(humanoid)
+	if not humanoid then
+		warn("[ParticlePlayer] - No humanoid")
 		return false
 	end
 
-	local RootPart = Humanoid.RootPart
-	if not RootPart then
+	-- Load animation
+	local animation = Instance.new("Animation")
+	animation.AnimationId = "rbxassetid://1097650171"
+
+	local track = humanoid:LoadAnimation(animation)
+	track:Play()
+
+	return self:_playHumanoidEffect(humanoid, ReplicatedStorage.Particles.LevelUpEffect)
+end
+
+function ParticlePlayer:_playDescendantsOnce(parent)
+	local longestLife = 0
+
+	for _, item in pairs(parent:GetDescendants()) do
+		if item:IsA("ParticleEmitter") then
+			item:Emit(item.Rate)
+			longestLife = math.max(longestLife, item.Lifetime.Max)
+		end
+	end
+
+	return longestLife
+end
+
+function ParticlePlayer:_playHumanoidEffect(humanoid, effectTemplate)
+	local rootPart = humanoid.RootPart
+	if not rootPart then
 		warn("[ParticlePlayer] - No root part")
 		return false
 	end
 
-	local Effect = EffectTemplate:Clone()
-	local Core = Effect:FindFirstChild("Core")
-	if not Core then
+	local effect = effectTemplate:Clone()
+	local core = effect:FindFirstChild("Core")
+	if not core then
 		warn("[ParticlePlayer] - No core")
 		return false
 	end
 
-	for _, Child in pairs(Core:GetChildren()) do
-		if Child:IsA("Attachment") then
-			Child.Parent = RootPart
+	for _, child in pairs(core:GetChildren()) do
+		if child:IsA("Attachment") then
+			child.Parent = rootPart
 
-			local LongestTime = self:PlayDescendantsOnce(Child)
-			Debris:AddItem(Child, LongestTime)
+			local longestTime = self:_playDescendantsOnce(child)
+			Debris:AddItem(child, longestTime)
 		end
 	end
 
-	-- Load animation
-	spawn(function()
-		local Animation = Instance.new("Animation")
-		Animation.AnimationId = "rbxassetid://1097650171"
-
-		local Track = Humanoid:LoadAnimation(Animation)
-		Track:Play()
-	end)
-
 	-- Add non-core items to
-	for _, Part in pairs(Effect:GetChildren()) do
-		if Part ~= Core then
-			local Relative = Core.CFrame:toObjectSpace(Part.CFrame)
-			Part.CFrame = RootPart.CFrame:toWorldSpace(Relative)
+	for _, part in pairs(effect:GetChildren()) do
+		if part ~= core then
+			local relative = core.CFrame:toObjectSpace(part.CFrame)
+			part.CFrame = rootPart.CFrame:toWorldSpace(relative)
 
-			Part.CanCollide = false
-			Part.Anchored = false
-			--Part.Transparency = 1
+			part.CanCollide = false
+			part.Anchored = false
+			--part.Transparency = 1
 
-			local Weld = Instance.new("Weld")
-			Weld.Parent = Part
-			Weld.Part0 = Part
-			Weld.Part1 = RootPart
-			Weld.C1 = Relative
-			Weld.Name = "ParticleEmitterWeld"
+			local weld = Instance.new("Weld")
+			weld.Parent = part
+			weld.Part0 = part
+			weld.Part1 = rootPart
+			weld.C1 = relative
+			weld.Name = "ParticleEmitterWeld"
 
-			Part.Parent = Humanoid.Parent
-			local LongestTime = self:PlayDescendantsOnce(Part)
+			part.Parent = humanoid.Parent
+			local longestTime = self:_playDescendantsOnce(part)
 
-			Debris:AddItem(Part, LongestTime)
+			Debris:AddItem(part, longestTime)
 		end
 	end
 
