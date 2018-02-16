6a2b434238f5b2d456515f4996ab2489f76fc911
James Onnen <rbxquenty@gmail.com>
2018-02-16 10:25:33 -0600

    Cleanup projectile physics and remove old APIs

--- Modules/Legacy/ProjectilePhysics.lua
+++ Modules/Legacy/ProjectilePhysics.lua
@@ -1,87 +1,93 @@
-local Physics={}
+--- Projectile physics used to syncronize state
+-- @classmod ProjectilePhysics
 
-local tick			=tick
-local v3			=Vector3.new
+local require = require(game:GetService("ReplicatedStorage"):WaitForChild("Nevermore"))
 
-Physics.Projectile = {}
-Physics.NumberProjectile = {}
-Physics.VectorProjectile = {}
+local TimeSyncManager = require("TimeSyncManager")
 
-function Physics.Projectile.New(Initial)
-	local x0 = tick()                     -- tick0
-	local p0 = Initial or 0               -- Position0
-	local v0 = Initial and 0*Initial or 0 -- Velocity0
-	local a0 = v0                         -- Acceleration
+local ProjectilePhysics = {}
+ProjectilePhysics.ClassName = "ProjectilePhysics"
 
-	local function PositionVelocity(tick)
-		local dt = tick-x0
-		return p0 + v0*dt + 0.5*dt*dt*a0,
-		       v0 + a0*dt
+function ProjectilePhysics.new(initial)
+	initial = initial or 0
+
+	local self = setmetatable({}, ProjectilePhysics)
+
+	rawset(self, "_position0", initial)
+	rawset(self, "_velocity0", 0*initial)
+	rawset(self, "_acceleration", 0*initial)
+	rawset(self, "_time0", TimeSyncManager:GetTime())
+
+	return self
+end
+
+function ProjectilePhysics:Impulse(velocity)
+	self.Velocity = self.Velocity + velocity
+end
+
+function ProjectilePhysics:TimeSkip(delta)
+	local time = TimeSyncManager:GetTime()
+	local position, velocity = self:_positionVelocity(time+delta)
+	rawset(self, "_position0", position)
+	rawset(self, "_velocity0", velocity)
+	rawset(self, "_time0", time)
+end
+
+function ProjectilePhysics:SetData(startTime, position0, velocity0, acceleration)
+	rawset(self, "_time0", startTime)
+	rawset(self, "_position0", position0)
+	rawset(self, "_velocity0", velocity0)
+	rawset(self, "_acceleration", acceleration)
+end
+
+function ProjectilePhysics:__index(index)
+	local time = TimeSyncManager:GetTime()
+
+	if ProjectilePhysics[index] then
+		return ProjectilePhysics[index]
+	elseif index == "Position" then
+		local position, _ = self:_positionVelocity(time)
+		return position
+	elseif index == "Velocity" then
+		local _, velocity = self:_positionVelocity(time)
+		return velocity
+	elseif index == "Acceleration" then
+		return rawget(self, _"acceleration")
+	elseif index == "Age" then
+		return TimeSyncManager:GetTime() - rawget(self, "_time0")
+	else
+		error(("'%s' is not a valid member of ProjectilePhysics"):format(tostring(index)), 2)
 	end
-	local Projectile
-	Projectile = setmetatable({
-			Impulse = function(_,v)
-				Projectile.Velocity=Projectile.Velocity+v
-			end;
-			TimeSkip = function(_, dt)
-				local tick = tick()
-				local p, v = PositionVelocity(tick+dt)
-				x0 = tick
-				v0 = v
-				p0 = p
-			end;
-			SetData = function(_, new_x0, new_p0, new_v0, new_a0)
-				x0 = new_x0
-				p0 = new_p0
-				v0 = new_v0
-				a0 = new_a0
-			end;
-		}, {
-		__index = function(self, Index)
-			if Index=="Value" or Index=="Position" or Index=="p" then
-				local p, v = PositionVelocity(tick())
-				return p
-			elseif Index=="Velocity" or Index=="v" then
-				local p, v = PositionVelocity(tick())
-				return v
-			elseif Index=="Acceleration" or Index=="a" or Index == "Gravity" then
-				return a0
-			else
-				error(Index .. " is not a valid member of Projectile")
-			end
-		end;
-		__newindex = function(self, Index, Value)
-			local tick = tick()
-			if Index=="Value" or Index=="Position" or Index=="p" then
-				local p, v = PositionVelocity(tick)
-				p0 = Value
-				v0 = v
-				x0 = tick
-			elseif Index=="Velocity" or Index=="v" then
-				local p, v = PositionVelocity(tick)
-				p0 = p
-				v0 = Value
-				x0 = tick
-			elseif Index=="Acceleration" or Index=="a" or Index == "Gravity" then
-				local p, v = PositionVelocity(tick)
-				a0 = Value
-				p0 = p
-				v0 = v
-				x0 = tick
-			else
-				error(Index .. " is not a valid member of Projectile")
-			end
-		end;
-	})
-	return Projectile
 end
 
-function Physics.NumberProjectile.New(Initial)
-	return Physics.Projectile.New(Initial)
+function ProjectilePhysics:__newindex(index, value)
+	local time = TimeSyncManager:GetTime()
+	if index == "Position" then
+		local _, velocity = self:_positionVelocity(time)
+		rawset(self, "_position0", value)
+		rawset(self, "_velocity0", velocity)
+	elseif index == "Velocity" then
+		local position, _ = self:_positionVelocity(time)
+		rawset(self, "_position0", position)
+		rawset(self, "_velocity0", value)
+	elseif index == "Acceleration" then
+		local position, velocity = self:_positionVelocity(time)
+		rawset(self, "_position0", position)
+		rawset(self, "_velocity0", velocity)
+		rawset(self, "_acceleration", value)
+	else
+		error(("'%s' is not a valid member of ProjectilePhysics"):format(tostring(index)), 2)
+	end
+	rawset(self, "_time0", time)
 end
 
-function Physics.VectorProjectile.New(Initial)
-	return Physics.Projectile.New(Initial or v3())
+function ProjectilePhysics:_positionVelocity(time)
+	local dt = time - rawget(self, "_time0")
+	local a0 = rawget(self, "_acceleration")
+	local v0 = rawget(self, "_velocity0")
+	local p0 = rawget(self, "_position0")
+	return p0 + v0*dt + 0.5*dt*dt*a0,
+	       v0 + a0*dt
 end
 
-return Physics
+return ProjectilePhysics
