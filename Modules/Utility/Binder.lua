9523035bc9ec53dfc30dbf092525fcf213eff126
James Onnen <rbxquenty@gmail.com>
2018-03-11 16:06:39 -0500

    Fix binder with stuff that takes a while to load and fix spaces -> tabs

--- Modules/Utility/Binder.lua
+++ Modules/Utility/Binder.lua
@@ -13,57 +13,70 @@ Binder.__index = Binder
 Binder.ClassName = "Binder"
 
 function Binder.new(tagName, class)
-    local self = setmetatable({}, Binder)
+	local self = setmetatable({}, Binder)
 
-    self._maid = Maid.new()
-    self._tagName = tagName or error("No tagName")
-    self._class = class or error("No class")
+	self._maid = Maid.new()
+	self._tagName = tagName or error("No tagName")
+	self._class = class or error("No class")
 
-    for _, inst in pairs(CollectionService:GetTagged(self._tagName)) do
-        fastSpawn(function()
-            self:_add(inst)
-        end)
-    end
+	self._loading = setmetatable({}, {__mode = "kv"})
 
-    self._maid:GiveTask(CollectionService:GetInstanceAddedSignal(self._tagName):Connect(function(inst)
-        self:_add(inst)
-    end))
-    self._maid:GiveTask(CollectionService:GetInstanceRemovedSignal(self._tagName):Connect(function(inst)
-        self:_remove(inst)
-    end))
+	for _, inst in pairs(CollectionService:GetTagged(self._tagName)) do
+		fastSpawn(function()
+			self:_add(inst)
+		end)
+	end
 
-    return self
+	self._maid:GiveTask(CollectionService:GetInstanceAddedSignal(self._tagName):Connect(function(inst)
+		self:_add(inst)
+	end))
+	self._maid:GiveTask(CollectionService:GetInstanceRemovedSignal(self._tagName):Connect(function(inst)
+		self:_remove(inst)
+	end))
+
+	return self
+end
+
+function Binder:GetAll()
+	local all = {}
+	for _, inst in pairs(CollectionService:GetTagged(self._tagName)) do
+		table.insert(all, self._maid[inst])
+	end
+	return all
 end
 
 function Binder:Bind(inst)
-    CollectionService:AddTag(inst, self._tagName)
-    return self:Get(inst)
+	CollectionService:AddTag(inst, self._tagName)
+	return self:Get(inst)
 end
 
 function Binder:Get(inst)
-    return self._maid[inst]
+	return self._maid[inst]
 end
 
 function Binder:_add(inst)
-	if self._maid[inst] then
+	if self._loading[inst] then
 		return
 	end
 
-    if type(self._class) == "function" then
-        self._maid[inst] = self._class(inst)
-    elseif self._class.Create then
-        self._maid[inst] = self._class:Create(inst)
-    else
-        self._maid[inst] = self._class.new(inst)
-    end
+	self._loading[inst] = true
+
+	if type(self._class) == "function" then
+		self._maid[inst] = self._class(inst)
+	elseif self._class.Create then
+		self._maid[inst] = self._class:Create(inst)
+	else
+		self._maid[inst] = self._class.new(inst)
+	end
 end
 
 function Binder:_remove(inst)
-    self._maid[inst] = nil
+	self._maid[inst] = nil
+	self._loading[inst] = nil
 end
 
 function Binder:Destroy()
-    self._maid:DoCleaning()
+	self._maid:DoCleaning()
 end
 
 return Binder
