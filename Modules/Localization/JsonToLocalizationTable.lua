8338dbff3ee65e047f45db3f178db0c779bef1ff
James Onnen <rbxquenty@gmail.com>
2018-07-28 22:35:18 -0700

    Add to localization library

---
+++ Modules/Localization/JsonToLocalizationTable.lua
@@ -0,0 +1,66 @@
+--- Utility to build a localization table from json, intended to be used with rojo
+-- @module JsonToLocalizationTable
+
+local HttpService = game:GetService("HttpService")
+
+local lib = {}
+
+local DEFAULT_SOURCE = ""
+local DEFAULT_CONTEXT = ""
+
+--- Recursively iterates through the object to construct strings and add it to the localization table
+-- @param localeId The localizationid to add
+-- @param baseKey the key to add
+-- @param object The value to iterate over
+local function recurseAdd(localizationTable, localeId, baseKey, object)
+	if baseKey ~= "" then
+		baseKey = baseKey .. "."
+	end
+
+	for index, value in pairs(object) do
+		local key = baseKey .. index
+		if type(value) == "table" then
+			recurseAdd(localizationTable, localeId, key, value)
+		elseif type(value) == "string" then
+			localizationTable:SetEntryValue(key, DEFAULT_SOURCE, DEFAULT_CONTEXT, localeId, value)
+		else
+			error("Bad type for value in '" .. key .. "'.")
+		end
+	end
+end
+
+--- Extracts the locale from the name
+-- @param name The name to parse
+-- @return The locale
+function lib.localeFromName(name)
+	if name:sub(-5) == ".json" then
+		return name:sub(1, #name-5)
+	else
+		return name
+	end
+end
+
+--- Loads a folder into a localization table
+-- @parm folder A Roblox folder with StringValues containing JSON, named with the localization in mind
+function lib.loadFolder(folder)
+	local localizationTable = Instance.new("LocalizationTable")
+	for _, item in pairs(folder:GetChildren()) do
+		if item:IsA("StringValue") then
+			local localeId = lib.localeFromName(item.Name)
+			lib.addJsonToTable(localizationTable, localeId, item.Value)
+		end
+	end
+	return localizationTable
+end
+
+--- Adds json to a localization table
+-- @param localizationTable The localization table to add to
+-- @param localeId The localeId to use
+-- @param json The json to add with
+function lib.addJsonToTable(localizationTable, localeId, json)
+	local object = HttpService:JSONDecode(json)
+	recurseAdd(localizationTable, localeId, "", object)
+end
+
+return lib
+
---
+++ Modules/Localization/PseudoLocalize.lua
@@ -0,0 +1,112 @@
+--- Pseudo localizes text. Useful for verifying translation without having
+--  actual translations available
+-- @module PseudoLocalize
+
+local lib = {}
+
+--- Translates a line into pseudo text while maintaining params
+-- @tparam {string} line The line to translate
+-- @return The translated line
+function lib.pseudoLocalize(line)
+	local charMap = lib.PseudoCharacterMap
+	local out = ""
+	local isParam = false
+
+	for start, stop in utf8.graphemes(line) do
+		local char = line:sub(start, stop)
+		if char == "{" then
+			isParam = true
+			out = out .. char
+		elseif char == "}" then
+			isParam = false
+			out = out .. char
+		elseif not isParam and charMap[char] then
+			out = out .. charMap[char]
+		else
+			out = out .. char
+		end
+	end
+
+	return out
+end
+
+--- Parses a localization table and adds a pseudo localized locale to the table
+-- @tparam[opt="qlp-pls"] {string} preferredLocaleId Preferred locale to use
+-- @tparam[opt="en-us"] {string} preferredFromLocale Preferred from locale
+function lib.addToLocalizationTable(localizationTable, preferredLocaleId, preferredFromLocale)
+	local localeId = preferredLocaleId or "qlp-pls"
+	local fromLocale = preferredFromLocale or "en-us"
+
+	local entries = localizationTable:GetEntries()
+	for _, entry in pairs(entries) do
+		if not entry.Values[localeId] then
+			local line = entry.Values[fromLocale]
+			if line then
+				entry.Values[localeId] = lib.pseudoLocalize(line)
+			else
+				warn(("[PseudoLocalize.addToLocalizationTable] - No entry in key '%s' for locale '%s'")
+					:format(entry.Key, fromLocale))
+			end
+		end
+	end
+
+	localizationTable:SetEntries(entries)
+end
+
+--- Mapping of English characters to pseudo localized characters
+lib.PseudoCharacterMap = {
+    ["a"] = "á";
+    ["b"] = "β";
+    ["c"] = "ç";
+    ["d"] = "δ";
+    ["e"] = "è";
+    ["f"] = "ƒ";
+    ["g"] = "ϱ";
+    ["h"] = "λ";
+    ["i"] = "ï";
+    ["j"] = "J";
+    ["k"] = "ƙ";
+    ["l"] = "ℓ";
+    ["m"] = "₥";
+    ["n"] = "ñ";
+    ["o"] = "ô";
+    ["p"] = "ƥ";
+    ["q"] = "9";
+    ["r"] = "ř";
+    ["s"] = "ƨ";
+    ["t"] = "ƭ";
+    ["u"] = "ú";
+    ["v"] = "Ʋ";
+    ["w"] = "ω";
+    ["x"] = "ж";
+    ["y"] = "¥";
+    ["z"] = "ƺ";
+    ["A"] = "Â";
+    ["B"] = "ß";
+    ["C"] = "Ç";
+    ["D"] = "Ð";
+    ["E"] = "É";
+    ["F"] = "F";
+    ["G"] = "G";
+    ["H"] = "H";
+    ["I"] = "Ì";
+    ["J"] = "J";
+    ["K"] = "K";
+    ["L"] = "£";
+    ["M"] = "M";
+    ["N"] = "N";
+    ["O"] = "Ó";
+    ["P"] = "Þ";
+    ["Q"] = "Q";
+    ["R"] = "R";
+    ["S"] = "§";
+    ["T"] = "T";
+    ["U"] = "Û";
+    ["V"] = "V";
+    ["W"] = "W";
+    ["X"] = "X";
+    ["Y"] = "Ý";
+    ["Z"] = "Z";
+}
+
+return lib
