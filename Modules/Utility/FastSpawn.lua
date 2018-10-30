3e4d333e5f0eb00626168e0c1de3ef632d09df16
howmanysmall <lhamil29@bruinmail.slcc.edu>
2018-10-30 10:33:27 -0700

    Update FastSpawn.lua

--- Modules/Utility/FastSpawn.lua
+++ Modules/Utility/FastSpawn.lua
@@ -1,16 +1,19 @@
 --- An expensive way to spawn a function. However, unlike spawn(), it executes on the same frame, and
 -- unlike coroutines, does not obscure errors
 -- @module fastSpawn
+local inst = Instance.new
 
 return function(func, ...)
-	assert(type(func) == "function")
-
-	local args = {...}
-	local bindable = Instance.new("BindableEvent")
-	bindable.Event:Connect(function()
-		func(unpack(args))
-	end)
-
+	local bindable = inst("BindableEvent")
+	if ... ~= nil then
+		local args = {...}
+		bindable.Event:Connect(function()
+			func(unpack(args))
+		end)
+	else
+		bindable.Event:Connect(func)
+	end
+	
 	bindable:Fire()
 	bindable:Destroy()
 end
