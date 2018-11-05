d73bc17a813fa6fd076a53e6176e9adf93df46ba
James Onnen <rbxquenty@gmail.com>
2018-11-05 14:42:52 -0600

    Update MouseOverMixin to use button utils

--- Modules/ClassMixin/MouseOverMixin.lua
+++ Modules/ClassMixin/MouseOverMixin.lua
@@ -4,6 +4,7 @@
 local require = require(game:GetService("ReplicatedStorage"):WaitForChild("Nevermore"))
 
 local Maid = require("Maid")
+local ButtonUtils = require("ButtonUtils")
 
 local module = {}
 
@@ -11,19 +12,9 @@ function module:Add(class)
 	assert(class)
 	assert(not class.GetMouseOverColor)
 
-	class.GetMouseOverColor = self.GetMouseOverColor
 	class.GetMouseOverBoolValue = self.GetMouseOverBoolValue
 	class.AddMouseOverEffect = self.AddMouseOverEffect
 	class._getMouseOverTweenProperties = self._getMouseOverTweenProperties
-end
-
----
--- @tparam Color3 originalColor
--- @tparam[opt=1] number factor
-function module:GetMouseOverColor(originalColor, factor)
-	factor = factor or 1
-	local h, s, v = Color3.toHSV(originalColor)
-	return Color3.fromHSV(h, s, v-0.05*factor)
 end
 
 function module:GetMouseOverBoolValue(gui)
@@ -68,24 +59,24 @@ end
 function module:_getMouseOverTweenProperties(gui)
 	if gui:IsA("ImageButton") then
 		return {
-			ImageColor3 = self:GetMouseOverColor(gui.ImageColor3);
-			BackgroundColor3 = self:GetMouseOverColor(gui.BackgroundColor3);
+			ImageColor3 = ButtonUtils.GetMouseOverColor(gui.ImageColor3);
+			BackgroundColor3 = ButtonUtils.GetMouseOverColor(gui.BackgroundColor3);
 		}
 	else
 		return {
-			BackgroundColor3 = self:GetMouseOverColor(gui.BackgroundColor3);
+			BackgroundColor3 = ButtonUtils.GetMouseOverColor(gui.BackgroundColor3);
 		}
 	end
 end
 
 function module:AddMouseOverEffect(gui, tweenProperties)
-	tweenProperties = tweenProperties or self:_getMouseOverTweenProperties(gui)
+	tweenProperties = tweenProperties or ButtonUtils._getMouseOverTweenProperties(gui)
 
 	if gui:IsA("GuiButton") then
 		gui.AutoButtonColor = false
 	end
 
-	local maid, boolValue = self:GetMouseOverBoolValue(gui)
+	local maid, boolValue = ButtonUtils.GetMouseOverBoolValue(gui)
 	local original = {}
 	for property, _ in pairs(tweenProperties) do
 		original[property] = gui[property]
