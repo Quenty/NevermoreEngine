0fec6166c47b4315965d43552675fd6e07c6341e
James Onnen <rbxquenty@gmail.com>
2018-11-17 12:33:53 -0600

    Update WaitForChildPromise to promiseChild

---
+++ Modules/Utility/promiseChild.lua
@@ -0,0 +1,18 @@
+--- Warps the WaitForChild API with a promise
+-- @function promiseChild
+
+local require = require(game:GetService("ReplicatedStorage"):WaitForChild("Nevermore"))
+
+local Promise = require("Promise")
+
+--- Wraps the :WaitForChild API with a promise
+return function(parent, name, timeOut)
+	return Promise.new(function(resolve, reject)
+		local child = parent:WaitForChild(name, timeOut)
+		if child then
+			resolve(child)
+		else
+			reject("Timed out")
+		end
+	end)
+end
