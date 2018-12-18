fd18475aa9f82ccbf3b376cddc36e32384235bfc
James Onnen <rbxquenty@gmail.com>
2018-12-18 00:47:16 -0600

    Add PlayerBinder

---
+++ Modules/Binder/PlayerBinder.lua
@@ -0,0 +1,27 @@
+--- Binds the given class to each player in the game
+-- @classmod PlayerBinder
+
+local require = require(game:GetService("ReplicatedStorage"):WaitForChild("Nevermore"))
+
+local Players = game:GetService("Players")
+
+local Binder = require("Binder")
+
+local PlayerBinder = setmetatable({}, Binder)
+PlayerBinder.ClassName = "PlayerBinder"
+PlayerBinder.__index = PlayerBinder
+
+function PlayerBinder.new(tag, class)
+	local self = setmetatable(Binder.new(tag, class), PlayerBinder)
+
+	self._maid:GiveTask(Players.PlayerAdded:Connect(function(player)
+		self:Bind(player)
+	end))
+	for _, item in pairs(Players:GetPlayers()) do
+		self:Bind(item)
+	end
+
+	return self
+end
+
+return PlayerBinder
