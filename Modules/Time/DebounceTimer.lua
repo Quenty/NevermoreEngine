0e0210e78601ee7d43baadc114391cf937c44cd7
James Onnen <rbxquenty@gmail.com>
2018-12-31 18:31:46 -0600

    Add DebounceTimer

---
+++ Modules/Time/DebounceTimer.lua
@@ -0,0 +1,32 @@
+--- DebounceTimer
+-- @classmod DebounceTimer
+
+local DebounceTimer = {}
+DebounceTimer.ClassName = "DebounceTimer"
+DebounceTimer.__index = DebounceTimer
+
+function DebounceTimer.new(length)
+	local self = setmetatable({}, DebounceTimer)
+
+	self._length = length or error("No length")
+
+	return self
+end
+
+function DebounceTimer:Restart()
+	self._startTime = tick()
+end
+
+function DebounceTimer:IsRunning()
+	return self._startTime ~= nil
+end
+
+function DebounceTimer:IsDone()
+	if not self:IsRunning() then
+		return true
+	end
+
+	return (tick() - self._startTime) <= self._length
+end
+
+return DebounceTimer
