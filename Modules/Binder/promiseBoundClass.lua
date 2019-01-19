bc195935f374f849553c8030584b737cad2eb1ae
James Onnen <rbxquenty@gmail.com>
2019-01-19 13:55:10 -0600

    Add promises for bound classes

---
+++ Modules/Binder/promiseBoundClass.lua
@@ -0,0 +1,28 @@
+---
+-- @function promiseBoundClass
+
+local require = require(game:GetService("ReplicatedStorage"):WaitForChild("Nevermore"))
+
+local Promise = require("Promise")
+local Maid = require("Maid")
+
+return function(binder, inst)
+	local class = binder:Get(inst)
+	if class then
+		return Promise.resolved(class)
+	end
+
+	local maid = Maid.new()
+	local promise = Promise.new()
+	maid:GiveTask(binder:GetClassAddedSignal():Connect(function(classAdded, instance)
+		if instance == inst then
+			promise:Fulfill(classAdded)
+		end
+	end))
+
+	promise:Finally(function()
+		maid:Destroy()
+	end)
+
+	return promise
+end
---
+++ Modules/Binder/promiseBoundLinkedClass.lua
@@ -0,0 +1,38 @@
+---
+-- @function promiseBoundLinkedClass
+
+local require = require(game:GetService("ReplicatedStorage"):WaitForChild("Nevermore"))
+
+local Promise = require("Promise")
+local Maid = require("Maid")
+
+return function(binder, objValue)
+	if objValue.Value then
+		local class = binder:Get(objValue.Value)
+		if class then
+			return Promise.resolved(class)
+		end
+	end
+
+	local maid = Maid.new()
+	local promise = Promise.new()
+
+	maid:GiveTask(objValue.Changed:Connect(function()
+		local class = binder:Get(objValue.Value)
+		if class then
+			promise:Fulfill(class)
+		end
+	end))
+
+	maid:GiveTask(binder:GetClassAddedSignal():Connect(function(class, instance)
+		if instance == objValue.Value then
+			promise:Fulfill(class)
+		end
+	end))
+
+	promise:Finally(function()
+		maid:Destroy()
+	end)
+
+	return promise
+end
