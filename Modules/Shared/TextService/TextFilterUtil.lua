4765aa1b43a8712139c708745d59b90ec604dff9
James Onnen <rbxquenty@gmail.com>
2019-03-04 17:23:49 -0600

    Better util

--- Modules/Shared/TextService/TextFilterUtil.lua
+++ Modules/Shared/TextService/TextFilterUtil.lua
@@ -10,21 +10,31 @@ local Promise = require("Promise")
 
 local TextFilterUtil = {}
 
+function TextFilterUtil.GetNonChatStringForBroadcastAsync(string, userId)
+	local text = nil
+	local ok, err = pcall(function()
+		local result = TextService:FilterStringAsync(string, userId)
+		if not result then
+			error("No TextFilterResult")
+		end
+
+		text = result:GetNonChatStringForBroadcastAsync()
+	end)
+
+	if not ok then
+		return false, err
+	end
+
+	return text
+end
+
 function TextFilterUtil.PromiseNonChatStringForBroadcast(string, userId)
 	assert(type(string) == "string")
 	assert(type(userId) == "number")
 
 	local promise = Promise.new(function(resolve, reject)
-		local text = nil
-		local ok, err = pcall(function()
-			local result = TextService:FilterStringAsync(string, userId)
-			if not result then
-				return reject("No TextFilterResult")
-			end
-
-			text = result:GetNonChatStringForBroadcastAsync()
-		end)
-		if not ok then
+		local text, err = TextFilterUtil.GetNonChatStringForBroadcastAsync(string, userId)
+		if not text then
 			return reject(err or "Pcall failed")
 		end
 		if not text then
