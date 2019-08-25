f5f646cf4534b727e74b257b6504e6bb683b9320
James Onnen <jonnen0@gmail.com>
2019-08-25 13:53:47 -0500

    Prevent weird camera shake from root part being screwy

--- Modules/Client/Ragdoll/RagdollClient.lua
+++ Modules/Client/Ragdoll/RagdollClient.lua
@@ -7,6 +7,7 @@ local Workspace = game:GetService("Workspace")
 local Players = game:GetService("Players")
 
 local RagdollBase = require("RagdollBase")
+local CharacterUtil = require("CharacterUtil")
 
 local RagdollClient = setmetatable({}, RagdollBase)
 RagdollClient.ClassName = "RagdollClient"
@@ -16,6 +17,12 @@ function RagdollClient.new(humanoid)
 	local self = setmetatable(RagdollBase.new(humanoid), RagdollClient)
 
 	self:_setupState()
+
+	local player = CharacterUtil.GetPlayerFromCharacter(self._obj)
+	if player == Players.LocalPlayer then
+		self:_setupCamera()
+	end
+
 	self:StopAnimations()
 
 	return self
@@ -27,6 +34,25 @@ function RagdollClient:_setupState()
 	self._obj:ChangeState(Enum.HumanoidStateType.Physics)
 	self._maid:GiveTask(function()
 		self._obj:ChangeState(Enum.HumanoidStateType.GettingUp)
+	end)
+end
+
+function RagdollClient:_setupCamera()
+	local model = self._obj.Parent
+	if not model then
+		return
+	end
+
+	local torso = model:FindFirstChild("Torso") or model:FindFirstChild("UpperTorso")
+	if not torso then
+		warn("[RagdollClient] - No Torso")
+		return
+	end
+
+	Workspace.CurrentCamera.CameraSubject = torso
+
+	self._maid:GiveTask(function()
+		Workspace.CurrentCamera.CameraSubject = self._obj
 	end)
 end
 
