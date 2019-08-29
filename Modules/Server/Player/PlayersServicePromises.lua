3ff46975e02fd29423ea0d2ae9fe1788eae36cab
James Onnen <jonnen0@gmail.com>
2019-08-28 21:09:24 -0500

    Add new promises for players service

---
+++ Modules/Server/Player/PlayersServicePromises.lua
@@ -0,0 +1,33 @@
+--- Utility methods for async methods in Players service
+-- @module PlayersServicePromises.lua
+
+local require = require(game:GetService("ReplicatedStorage"):WaitForChild("Nevermore"))
+
+local Players = game:GetService("Players")
+
+local Promise = require("Promise")
+
+local PlayersServicePromises = {}
+
+function PlayersServicePromises.promiseUserIdFromName(name)
+	assert(type(name) == "string")
+
+	return Promise.spawn(function(resolve, reject)
+		local userId
+		local ok, err = pcall(function()
+			userId = Players:GetUserIdFromNameAsync(name)
+		end)
+
+		if not ok then
+			return reject(err)
+		end
+
+		if type(userId) ~= "number" then
+			return reject("UserId returned was not a number")
+		end
+
+		resolve(userId)
+	end)
+end
+
+return PlayersServicePromises
