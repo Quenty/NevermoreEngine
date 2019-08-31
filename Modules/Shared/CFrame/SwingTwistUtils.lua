62be4bc87bee4a74facf620e651ad8756c445c6a
James Onnen <jonnen0@gmail.com>
2019-08-31 14:30:46 -0500

    Update swing twist utils

--- Modules/Shared/CFrame/SwingTwistUtils.lua
+++ Modules/Shared/CFrame/SwingTwistUtils.lua
@@ -4,22 +4,28 @@
 
 local SwingTwistUtils = {}
 
-local function getRotationBetween(u, v, axis)
-    local dot, uxv = u:Dot(v), u:Cross(v)
-    if (dot < -0.99999) then return CFrame.fromAxisAngle(axis, math.pi) end
-    return CFrame.new(0, 0, 0, uxv.x, uxv.y, uxv.z, 1 + dot)
+function SwingTwistUtils.swingTwist(cf, direction)
+    local axis, theta = cf:ToAxisAngle()
+    -- convert to quaternion
+    local w, v = math.cos(theta/2),  math.sin(theta/2)*axis
+
+    -- (v . d)*d, plug into CFrame quaternion constructor with w it will solve rest for us
+	local proj = v:Dot(direction)*direction
+    local twist = CFrame.new(0, 0, 0, proj.x, proj.y, proj.z, w)
+
+    -- cf = swing * twist, thus...
+	local swing = cf * twist:Inverse()
+
+	return swing, twist
 end
 
-function SwingTwistUtils.getSwingTwist(cf, direction)
-	local swing = CFrame.new()
-	local rDirection = cf:VectorToWorldSpace(direction)
-    if (rDirection:Dot(direction) > -0.99999) then
-        -- we don't need to provide a backup axis b/c it will nvr be used
-		swing = getRotationBetween(direction, rDirection, nil)
-	end
-	-- cf = swing * twist, thus...
-	local twist = swing:Inverse() * cf
-	return swing, twist
+function SwingTwistUtils.twistAngle(cf, direction)
+    local axis, theta = cf:ToAxisAngle()
+    local w, v = math.cos(theta/2),  math.sin(theta/2)*axis
+	local proj = v:Dot(direction)*direction
+    local twist = CFrame.new(0, 0, 0, proj.x, proj.y, proj.z, w)
+    local _, nTheta = twist:ToAxisAngle()
+    return math.sign(v:Dot(direction))*nTheta
 end
 
 return SwingTwistUtils
