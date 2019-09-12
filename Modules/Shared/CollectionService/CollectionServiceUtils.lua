4e66e4e986f209bfc002d618fe8032cc9257f163
howmanysmall <lhamil29@bruinmail.slcc.edu>
2019-09-12 11:51:43 -0600

    Update CollectionServiceUtils.lua
    Added `findNearestPartWithTag`. Used for searching around a given BasePart to find what part with the given tag is nearest to it.

--- Modules/Shared/CollectionService/CollectionServiceUtils.lua
+++ Modules/Shared/CollectionService/CollectionServiceUtils.lua
@@ -19,5 +19,33 @@ function CollectionServiceUtils.findFirstAncestor(tagName, child)
 	return nil
 end
 
+function CollectionServiceUtils.findNearestPartWithTag(tagName, basePart)
+	assert(type(tagName) == "string")
+	assert(typeof(basePart) == "Instance" and basePart:IsA("BasePart"))
+
+	local taggedInstances = CollectionService:GetTagged(tagName)
+	local length = #taggedInstances
+	if length > 0 then
+		local currentDistance = math.huge
+		local foundPart = nil
+		local basePosition = basePart.Position
+
+		for index = 1, length do
+			local child = taggedInstances[index]
+			if child:IsA("BasePart") then
+				local magnitude = (child.Position - basePosition).Magnitude
+				if magnitude < currentDistance then
+					currentDistance = magnitude
+					foundPart = child
+				end
+			end
+		end
+
+		return foundPart
+	else
+		warn(("Nothing has the tag %s"):format(tagName))
+		return nil
+	end
+end
 
 return CollectionServiceUtils
