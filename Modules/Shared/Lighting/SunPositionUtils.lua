fc2d237402d0d979fbfa7b4f1773ff98a67821cf
James Onnen <jonnen0@gmail.com>
2019-09-15 21:34:38 -0500

    Update sun utils

--- Modules/Shared/Lighting/SunPositionUtils.lua
+++ Modules/Shared/Lighting/SunPositionUtils.lua
@@ -3,15 +3,23 @@
 
 local SunPositionUtils = {}
 
-function SunPositionUtils.getGeographicalLatitude(direction)
+local EARTH_TILT = 23.5
+local NORTH = Vector3.new(0, 0, -1)
+
+function SunPositionUtils.getGeographicalLatitudeFromDirection(direction)
 	local angle = math.atan2(direction.z, math.sqrt(direction.x^2 + direction.y^2))
-	return angle/(math.pi*2)*360+23.5
+	return angle/(math.pi*2)*360+EARTH_TILT
 end
 
-function SunPositionUtils.getClockTime(direction)
-	local clockTime = math.atan2(-direction.y, -direction.x)
+function SunPositionUtils.getClockTimeFromDirection(direction)
+	local altitude = math.atan2(-direction.y, -direction.x)
 
-	return (clockTime/(math.pi*2)*24-6) % 24
+	return (altitude/(math.pi*2)*24-6) % 24
+end
+
+function SunPositionUtils.getDirection(azimuthRad, altitudeRad, north)
+	local cframe = (CFrame.Angles(0, azimuthRad, 0) * CFrame.Angles(altitudeRad, 0, 0))
+	return cframe:vectorToWorldSpace(NORTH)
 end
 
 return SunPositionUtils
