79b8c7b20158dffe1ea1c3a59ff50ef65cc36f19
James Onnen <jonnen0@gmail.com>
2019-09-20 20:54:13 -0500

    Remove force init

--- loader/ReplicatedStorage/Nevermore/ForceInitNevermore.server.lua
+++
@@ -1,6 +0,0 @@
---- This script initializes Nevermore on the server, replicating modules to the client if they exist.
--- You may remove this script if you know you're requiring Nevermore on the server.
--- @script InitNevermore
--- @author Quenty
-
-require(script.Parent)
