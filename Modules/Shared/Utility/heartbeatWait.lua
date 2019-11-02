4979f6f20230f7ef9b5b60bd3612d4526bd61ebd
James Onnen <jonnen0@gmail.com>
2019-11-02 14:06:32 -0500

    Add heartbeatWait() API

---
+++ Modules/Shared/Utility/heartbeatWait.lua
@@ -0,0 +1,18 @@
+--- Heartbeat wait for short waits to avoid wait queue being weird
+-- @module heartbeatWait
+
+local RunService = game:GetService("RunService")
+
+local heartbeat = RunService.Heartbeat
+
+return function(waitTime)
+	assert(type(waitTime) == "number")
+	assert(waitTime > 0)
+
+	local startTime = tick()
+	while (tick() - startTime) >= waitTime do
+		heartbeat:Wait()
+	end
+
+	return tick() - startTime
+end
