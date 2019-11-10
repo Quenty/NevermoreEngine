82340fec3bbbae950ad7f2b5dc069595722c3704
James Onnen <jonnen0@gmail.com>
2019-11-10 15:11:45 -0600

    More utility for Vector3

--- Modules/Shared/Vector/Vector3Utils.lua
+++ Modules/Shared/Vector/Vector3Utils.lua
@@ -7,4 +7,9 @@ function Vector3Utils.fromVector2XY(vector2)
 	return Vector3.new(vector2.x, vector2.y, 0)
 end
 
+function Vector3Utils.fromVector2XZ(vector2)
+	return Vector3.new(vector2.x, 0, vector2.y)
+end
+
+
 return Vector3Utils
