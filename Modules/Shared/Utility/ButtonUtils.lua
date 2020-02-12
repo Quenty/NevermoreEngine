231ff06d90386a790925613b4e0aef98717841c6
James Onnen <jonnen0@gmail.com>
2020-02-12 17:17:24 -0600

    lib -> ButtonUtils

--- Modules/Shared/Utility/ButtonUtils.lua
+++ Modules/Shared/Utility/ButtonUtils.lua
@@ -1,15 +1,15 @@
 --- Provides utility for editing buttons
 -- @module ButtonUtils
 
-local lib = {}
+local ButtonUtils = {}
 
 --- Gets a tinted mouse over color
 -- @tparam Color3 originalColor
 -- @tparam[opt=1] number factor
-function lib.GetMouseOverColor(originalColor, factor)
+function ButtonUtils.GetMouseOverColor(originalColor, factor)
 	factor = factor or 1
 	local h, s, v = Color3.toHSV(originalColor)
 	return Color3.fromHSV(h, s, v-0.05*factor)
 end
 
-return lib
+return ButtonUtils
