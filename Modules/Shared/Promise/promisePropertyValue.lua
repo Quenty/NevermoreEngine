301b7d9a8cc1781fa8e4a56ab076215713cf1d76
James Onnen <jonnen0@gmail.com>
2020-02-28 11:06:34 -0800

    value -> instance for clarity

--- Modules/Shared/Promise/promisePropertyValue.lua
+++ Modules/Shared/Promise/promisePropertyValue.lua
@@ -7,8 +7,11 @@ local Promise = require("Promise")
 
 -- NOTE: To use properly please make sure to reject the promise for proper GC if the object requiring
 -- this value is GCed.
-return function(value, propertyName)
-	local result = value[propertyName]
+return function(instance, propertyName)
+	assert(typeof(instance) == "Instance")
+	assert(type(propertyName) == "string")
+
+	local result = instance[propertyName]
 	if result then
 		return Promise.resolved(result)
 	end
@@ -22,9 +25,9 @@ return function(value, propertyName)
 		end
 	end)
 
-	conn = value:GetPropertyChangedSignal(propertyName):Connect(function()
-		if value[propertyName] then
-			promise:Resolve(value[propertyName])
+	conn = instance:GetPropertyChangedSignal(propertyName):Connect(function()
+		if instance[propertyName] then
+			promise:Resolve(instance[propertyName])
 		end
 	end)
 
