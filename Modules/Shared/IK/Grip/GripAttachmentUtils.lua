5d82f623c969ad09adcbfd081c1c39f74de0137c
James Onnen <jonnen0@gmail.com>
2020-02-28 11:06:26 -0800

    Grip attachments are relative BREAKING CHANGE!

--- Modules/Shared/IK/Grip/GripAttachmentUtils.lua
+++ Modules/Shared/IK/Grip/GripAttachmentUtils.lua
@@ -7,15 +7,15 @@ local GripAttachmentConstants = require("GripAttachmentConstants")
 
 local GripAttachmentUtils = {}
 
-function GripAttachmentUtils.create(binder, part, worldPosition, humanoid)
+function GripAttachmentUtils.create(binder, part, relativePosition, humanoid)
 	assert(binder)
 	assert(typeof(part) == "Instance")
-	assert(typeof(worldPosition) == "Vector3")
+	assert(typeof(relativePosition) == "Vector3")
 	assert(typeof(humanoid) == "Instance")
 
 	local gripAttachment = Instance.new("Attachment")
 	gripAttachment.Name = binder:GetTag()
-	gripAttachment.Position = part.CFrame:pointToObjectSpace(worldPosition)
+	gripAttachment.Position = relativePosition
 
 	local humanoidLink = Instance.new("ObjectValue")
 	humanoidLink.Name = GripAttachmentConstants.HUMANOID_LINK_NAME
