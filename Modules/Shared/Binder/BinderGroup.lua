3e1da02f6ea8b06db71059a326de61d6931e243e
James Onnen <jonnen0@gmail.com>
2020-04-05 18:07:37 -0700

    BinderGroup has cosntructor validation

--- Modules/Shared/Binder/BinderGroup.lua
+++ Modules/Shared/Binder/BinderGroup.lua
@@ -5,10 +5,18 @@ local BinderGroup = {}
 BinderGroup.ClassName = "BinderGroup"
 BinderGroup.__index = BinderGroup
 
-function BinderGroup.new(binders)
+function BinderGroup.new(binders, validateConstructor)
 	local self = setmetatable({}, BinderGroup)
 
 	self._binders = binders or error("No binders")
+
+	-- Assume to be using osyris's typechecking library,
+	-- we have an optional constructor to validate binder classes.
+	if validateConstructor then
+		for _, binder in pairs(self._binders) do
+			assert(validateConstructor(binder:GetConstructor()))
+		end
+	end
 
 	return self
 end
