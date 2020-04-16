25387b37b71b837e306004f15ae5f5df78d9e5b9
James Onnen <jonnen0@gmail.com>
2020-04-15 21:29:54 -0700

    Support module scripts from rojo too

--- Modules/Shared/Localization/JsonToLocalizationTable.lua
+++ Modules/Shared/Localization/JsonToLocalizationTable.lua
@@ -1,4 +1,5 @@
---- Utility to build a localization table from json, intended to be used with rojo
+--- Utility to build a localization table from json, intended to be used with rojo. Can also handle Rojo json
+-- objects turned into tables!
 -- @module JsonToLocalizationTable
 
 local HttpService = game:GetService("HttpService")
@@ -52,6 +53,9 @@ function JsonToLocalizationTable.loadFolder(folder)
 		if item:IsA("StringValue") then
 			local localeId = JsonToLocalizationTable.localeFromName(item.Name)
 			JsonToLocalizationTable.addJsonToTable(localizationTable, localeId, item.Value)
+		elseif item:IsA("ModuleScript") then
+			local localeId = JsonToLocalizationTable.localeFromName(item.Name)
+			recurseAdd(localizationTable, localeId, "", require(item))
 		end
 	end
 	return localizationTable
@@ -62,8 +66,8 @@ end
 -- @param localeId The localeId to use
 -- @param json The json to add with
 function JsonToLocalizationTable.addJsonToTable(localizationTable, localeId, json)
-	local object = HttpService:JSONDecode(json)
-	recurseAdd(localizationTable, localeId, "", object)
+	local decodedTable = HttpService:JSONDecode(json)
+	recurseAdd(localizationTable, localeId, "", decodedTable)
 end
 
 return JsonToLocalizationTable
