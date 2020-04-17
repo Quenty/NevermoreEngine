0b4950220a03e2cc179ff32f26b44f425da7a876
James Onnen <jonnen0@gmail.com>
2020-04-17 14:37:13 -0700

    Update TemplateProvider to not use function, and to ignore folders for better organization (BREAKING)

--- Modules/Shared/Templates/TemplateProvider.lua
+++ Modules/Shared/Templates/TemplateProvider.lua
@@ -1,68 +1,67 @@
 --- Base of a template retrieval system
 -- @classmod TemplateProvider
 
-local require = require(game:GetService("ReplicatedStorage"):WaitForChild("Nevermore"))
-
-local Promise = require("Promise")
-
 local TemplateProvider = {}
 TemplateProvider.ClassName = "TemplateProvider"
 TemplateProvider.__index = TemplateProvider
 
 -- getParentFunc may return a promise too! Executes async.
-function TemplateProvider.new(getParentFunc)
+function TemplateProvider.new(parent)
 	local self = setmetatable({}, TemplateProvider)
 
-	self._getParentFunc = getParentFunc or error("No getParentFunc")
-	self._parentPromise = nil
+	assert(typeof(parent) == "Instance")
+
+	self._parent = parent or error("No parent")
 
 	return self
 end
 
-function TemplateProvider:IsAvailable(templateName)
-	local promise = self:_promiseParent()
-	if not promise:IsFulfilled() then
-		return false
-	end
+function TemplateProvider:Init()
+	self._registry = {}
+	self:_processFolder(self._parent)
+end
 
-	local parent = promise:Wait() -- It's fulfilled so no waiting
-	return parent:FindFirstChild(templateName) ~= nil
+function TemplateProvider:IsAvailable(templateName)
+	return self._registry[templateName] ~= nil
 end
 
 function TemplateProvider:Get(templateName)
 	assert(type(templateName) == "string", "templateName must be a string")
 
-	return self:_getParentYielding():WaitForChild(templateName)
+	return self._registry[templateName]
 end
 
 function TemplateProvider:Clone(templateName)
-	local item = self:Get(templateName):Clone()
+	local template = self._registry[templateName]
+	if not template then
+		error(("[TemplateProvider.Clone] - Cannot provide %q"):format(tostring(templateName)))
+		return nil
+	end
+
+	local newItem = template:Clone()
 	if templateName:sub(-#("Template")) == "Template" then
-		item.Name = templateName:sub(1, -#("Template") - 1)
+		newItem.Name = templateName:sub(1, -#("Template") - 1)
 	end
-	return item
+	return newItem
 end
 
-function TemplateProvider:_promiseParent()
-	if self._parentPromise then
-		return self._parentPromise
-	end
-
-	self._parentPromise = Promise.spawn(function(resolve, reject)
-		local result = self._getParentFunc()
-		if not result then
-			warn("[TemplateProvider] - getParentFunc did not return a value")
-			return reject("getParentFunc did not return a value")
+function TemplateProvider:_processFolder(folder)
+	for _, instance in pairs(folder:GetChildren()) do
+		if instance:IsA("Folder") then
+			self:_processFolder(instance)
+		else
+			self:_addToRegistery(instance)
 		end
-
-		return resolve(result)
-	end)
-
-	return self._parentPromise
+	end
 end
 
-function TemplateProvider:_getParentYielding()
-	return self:_promiseParent():Wait()
+function TemplateProvider:_addToRegistery(instance)
+	if self._registry[instance.Name] then
+		error(("[TemplateProvider._addToRegistery] - Duplicate %q in registery")
+			:format(instance.Name))
+	end
+
+	self._registry[instance.Name] = instance
 end
 
 return TemplateProvider
