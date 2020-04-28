0004bf87d2340a1e907075d216dad177ce1230bd
James Onnen <jonnen0@gmail.com>
2020-04-28 16:35:08 -0700

    Add PlayerGuiUtils

---
+++ Modules/Client/Gui/PlayerGuiUtils.lua
@@ -0,0 +1,18 @@
+---
+-- @module PlayerGuiUtils
+-- @author Quenty
+
+local Players = game:GetService("Players")
+
+local PlayerGuiUtils = {}
+
+function PlayerGuiUtils.getPlayerGui()
+	local localPlayer = Players.LocalPlayer
+	if not localPlayer then
+		error("No localPlayer")
+	end
+
+	return localPlayer:FindFirstChildOfClass("PlayerGui") or error("No PlayerGui")
+end
+
+return PlayerGuiUtils
