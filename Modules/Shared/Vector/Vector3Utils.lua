8a2e91f872bba66ec7ae7d3658e33432f45f134f
James Onnen <jonnen0@gmail.com>
2020-04-28 15:38:24 -0700

    Angle from 2 unit vectors

--- Modules/Shared/Vector/Vector3Utils.lua
+++ Modules/Shared/Vector/Vector3Utils.lua
@@ -11,5 +11,12 @@ function Vector3Utils.fromVector2XZ(vector2)
 	return Vector3.new(vector2.x, 0, vector2.y)
 end
 
+function Vector3Utils.getAngleRad(offset, lookVector)
+	if offset.magnitude == 0 then
+		return nil
+	end
+
+	return math.acos(offset.unit:Dot(lookVector))
+end
 
 return Vector3Utils
