1809c65e733a8dd3753674354bb222901222ba2a
James Onnen <jonnen0@gmail.com>
2020-05-03 19:00:18 -0700

    Don't assume offset unit

--- Modules/Shared/Vector/Vector3Utils.lua
+++ Modules/Shared/Vector/Vector3Utils.lua
@@ -11,12 +11,12 @@ function Vector3Utils.fromVector2XZ(vector2)
 	return Vector3.new(vector2.x, 0, vector2.y)
 end
 
-function Vector3Utils.getAngleRad(offset, lookVector)
-	if offset.magnitude == 0 then
+function Vector3Utils.getAngleRad(offsetUnit, lookVector)
+	if offsetUnit.magnitude == 0 then
 		return nil
 	end
 
-	return math.acos(offset.unit:Dot(lookVector))
+	return math.acos(offsetUnit:Dot(lookVector))
 end
 
 return Vector3Utils
