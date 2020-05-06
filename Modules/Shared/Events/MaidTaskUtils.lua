0835321842a12c78de77b4efececbbdd787d55ae
James Onnen <jonnen0@gmail.com>
2020-05-05 22:32:07 -0700

    Add new maid tasks

--- Modules/Shared/Events/MaidTaskUtils.lua
+++ Modules/Shared/Events/MaidTaskUtils.lua
@@ -4,21 +4,31 @@
 
 local MaidTaskUtils = {}
 
+function MaidTaskUtils.isValidTask(task)
+	return type(task) == "function"
+		or typeof(task) == "RBXScriptConnection"
+		or type(task) == "table" and type(task.Destroy) == "function"
+end
+
+function MaidTaskUtils.doTask(task)
+	if type(task) == "function" then
+		task()
+	elseif typeof(task) == "RBXScriptConnection" then
+		task:Disconnect()
+	elseif type(task) == "table" and type(task.Destroy) == "function" then
+		task:Destroy()
+	else
+		error("Bad task")
+	end
+end
+
 function MaidTaskUtils.delayed(time, task)
 	assert(type(time) == "number")
 	assert(task ~= nil)
 
 	return function()
 		delay(time, function()
-			if type(task) == "function" then
-				task()
-			elseif typeof(task) == "RBXScriptConnection" then
-				task:Disconnect()
-			elseif task.Destroy then
-				task:Destroy()
-			else
-				error("Bad task")
-			end
+			MaidTaskUtils.doTask(task)
 		end)
 	end
 end
