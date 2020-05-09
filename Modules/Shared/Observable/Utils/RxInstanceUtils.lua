49300f3b0c0d0692aabefb2f12e51917e01b3442
James Onnen <jonnen0@gmail.com>
2020-05-08 23:01:50 -0700

    Add more methods

--- Modules/Shared/Observable/Utils/RxInstanceUtils.lua
+++ Modules/Shared/Observable/Utils/RxInstanceUtils.lua
@@ -4,9 +4,11 @@
 
 local require = require(game:GetService("ReplicatedStorage"):WaitForChild("Nevermore"))
 
+local Rx = require("Rx")
 local Observable = require("Observable")
 local Maid = require("Maid")
 local Brio = require("Brio")
+local RxBrioUtils = require("RxBrioUtils")
 
 local RxInstanceUtils = {}
 
@@ -25,6 +27,49 @@ function RxInstanceUtils.observeProperty(instance, property)
 		return maid
 	end)
 end
+
+function RxInstanceUtils.observeValidPropertyBrio(instance, property, predicate)
+	assert(typeof(instance) == "Instance")
+	assert(type(property) == "string")
+	assert(type(predicate) == "function" or predicate == nil)
+
+	return Observable.new(function(fire, fail, complete)
+		local maid = Maid.new()
+
+		local function handlePropertyChanged()
+			maid._property = nil
+
+			local propertyValue = instance[property]
+			if not predicate or predicate(propertyValue) then
+				local brio = Brio.new(instance[property])
+				maid._property = brio
+				fire(brio)
+			end
+		end
+
+		maid:GiveTask(instance:GetPropertyChangedSignal(property):Connect(handlePropertyChanged))
+		handlePropertyChanged()
+
+		return maid
+	end)
+end
+
+function RxInstanceUtils.observeLastNamedChildBrio(parent, className, name)
+	assert(parent)
+	assert(className)
+	assert(name)
+
+	return RxInstanceUtils.observeChildrenBrio(parent, function(child)
+		return child:IsA(className)
+	end):Pipe({
+		Rx.switchMap(RxBrioUtils.mapBrio(function(child)
+			return RxInstanceUtils.observeValidPropertyBrio(child, "Name", function(childName)
+				return childName == name
+			end)
+		end));
+	})
+end
+
 
 -- FIres once, and then completes
 function RxInstanceUtils.observeParentChangeFrom(child, parent)
@@ -52,25 +97,28 @@ function RxInstanceUtils.observeParentChangeFrom(child, parent)
 	end)
 end
 
-function RxInstanceUtils.observeChildrenBrio(parent)
+function RxInstanceUtils.observeChildrenBrio(parent, predicate)
 	assert(typeof(parent) == "Instance")
+	assert(type(predicate) == "function" or predicate == nil)
 
 	return Observable.new(function(fire, fail, complete)
 		local maid = Maid.new()
 
-		maid:GiveTask(parent.ChildAdded:Connect(function(child)
-			local value = Brio.new(child)
-			maid[child] = value
-			fire(value)
-		end))
+		local function handleChild(child)
+			if not predicate or predicate(child) then
+				local value = Brio.new(child)
+				maid[child] = value
+				fire(value)
+			end
+		end
+
+		maid:GiveTask(parent.ChildAdded:Connect(handleChild))
 		maid:GiveTask(parent.ChildRemoved:Connect(function(child)
 			maid[child] = nil
 		end))
 
 		for _, child in pairs(parent:GetChildren()) do
-			local value = Brio.new(child)
-			maid[child] = value
-			fire(value)
+			handleChild(child)
 		end
 
 		return maid
