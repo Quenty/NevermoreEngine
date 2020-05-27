a0ca647946497a1a4568a1ebb65a9183a0f215cb
James Onnen <jonnen0@gmail.com>
2020-05-26 19:56:50 -0700

    Untested number sequence code

---
+++ Modules/Shared/Utility/NumberSequenceUtils.lua
@@ -0,0 +1,49 @@
+---
+-- @module NumberSequenceUtils
+
+local NumberSequenceUtils = {}
+
+local EPSILON = 1e-3
+
+function NumberSequenceUtils.stripe(
+	stripes, backgroundTransparency, strikeTransparency, percentStripeThickness, percentOffset)
+	percentOffset = percentOffset or 0.5
+	percentStripeThickness = math.clamp(percentStripeThickness or 0.5, 0, 1)
+
+	if percentStripeThickness == 0 then
+		return NumberSequence.new(backgroundTransparency)
+	elseif percentStripeThickness == 1 then
+		return NumberSequence.new(strikeTransparency)
+	end
+
+	local waypoints = {}
+
+	local timeWidth = 1/stripes
+	local timeOffset = (percentOffset + timeWidth*(percentStripeThickness - 0.5) % 0.5)*timeWidth
+
+	if percentOffset >= 0.5 then
+		strikeTransparency, backgroundTransparency = backgroundTransparency, strikeTransparency
+	end
+
+	table.insert(waypoints, NumberSequenceKeypoint.new(0, backgroundTransparency))
+
+	for i=0, stripes-1 do
+		local timestampStart = i/stripes + timeOffset
+		local timeStampMiddle = timestampStart + timeWidth*(1 - percentStripeThickness)
+		local timestampEnd = math.min(timestampStart + timeWidth, 1)
+
+		if timeStampMiddle+EPSILON < timestampEnd-EPSILON then
+			table.insert(waypoints, NumberSequenceKeypoint.new(timeStampMiddle, backgroundTransparency))
+			table.insert(waypoints, NumberSequenceKeypoint.new(timeStampMiddle+EPSILON, strikeTransparency))
+
+			table.insert(waypoints, NumberSequenceKeypoint.new(timestampEnd-EPSILON, strikeTransparency))
+			table.insert(waypoints, NumberSequenceKeypoint.new(timestampEnd, backgroundTransparency))
+		else
+			table.insert(waypoints, NumberSequenceKeypoint.new(1, backgroundTransparency))
+		end
+	end
+
+	return NumberSequence.new(waypoints)
+end
+
+return NumberSequenceUtils
