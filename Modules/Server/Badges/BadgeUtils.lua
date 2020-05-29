8bbf604abd1d5604e55b41ffc4cdf4070d51fb6a
James Onnen <jonnen0@gmail.com>
2020-05-28 21:20:32 -0700

    Add badge utils

---
+++ Modules/Server/Badges/BadgeUtils.lua
@@ -0,0 +1,30 @@
+---
+-- @module BadgeUtils
+-- @author Quenty
+
+local BadgeService = game:GetService("BadgeService")
+
+local require = require(game:GetService("ReplicatedStorage"):WaitForChild("Nevermore"))
+
+local Promise = require("Promise")
+
+local BadgeUtils = {}
+
+function BadgeUtils.promiseAwardBadge(player, badgeId)
+	assert(typeof(player) == "Instance" and player:IsA("Player"))
+	assert(type(badgeId) == "number")
+
+	return Promise.spawn(function(resolve, reject)
+		local ok, err = pcall(function()
+			BadgeService:AwardBadge(player.UserId, badgeId)
+		end)
+
+		if not ok then
+			return reject(err)
+		end
+
+		return resolve(true)
+	end)
+end
+
+return BadgeUtils
