5cc4b2051c1de506e697bb2f384b413055ed829c
James Onnen <jonnen0@gmail.com>
2020-06-03 16:34:27 -0700

    Don't use weld constraints (for now)

--- Modules/Shared/Physics/Constraints/WeldConstraintUtils.lua
+++ Modules/Shared/Physics/Constraints/WeldConstraintUtils.lua
@@ -8,13 +8,23 @@ function WeldConstraintUtils.namedBetween(name, part0, part1, parent)
 	assert(typeof(part0) == "Instance")
 	assert(typeof(part1) == "Instance")
 
-	local weldConstraint = Instance.new("WeldConstraint")
-	weldConstraint.Name = name
-	weldConstraint.Part0 = part0
-	weldConstraint.Part1 = part1
-	weldConstraint.Parent = parent
+	-- Roblox weld constraints very buggy!
+	-- https://devforum.roblox.com/t/weld-constraint-behaves-differently-on-server-compared-to-client/445036
+	local weld = Instance.new("Weld")
+	weld.Name = name
+	weld.Part0 = part0
+	weld.Part1 = part1
+	weld.C0 = CFrame.new()
+	weld.C1 = part1.CFrame:ToObjectSpace(part0.CFrame)
+	weld.Parent = parent
 
-	return weldConstraint
+	-- local weldConstraint = Instance.new("WeldConstraint")
+	-- weldConstraint.Name = name
+	-- weldConstraint.Part0 = part0
+	-- weldConstraint.Part1 = part1
+	-- weldConstraint.Parent = parent
+
+	return weld
 end
 
 return WeldConstraintUtils
