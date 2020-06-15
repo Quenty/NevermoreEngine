640525f59ca6b397617c83cafc500325ad91a5cd
James Onnen <jonnen0@gmail.com>
2020-06-15 15:18:02 -0700

    Promise retry setting core

---
+++ Modules/Client/Gui/CoreGuiUtils.lua
@@ -0,0 +1,55 @@
+---
+-- @module CoreGuiUtils
+-- @author Quenty
+
+local require = require(game:GetService("ReplicatedStorage"):WaitForChild("Nevermore"))
+
+local StarterGui = game:GetService("StarterGui")
+
+local Promise = require("Promise")
+
+local CoreGuiUtils = {}
+
+function CoreGuiUtils.promiseRetrySetCore(tries, initialWaitTime, ...)
+	assert(type(tries) == "number")
+
+	local args = {...}
+	local n = select("#", ...)
+
+	return Promise.spawn(function(resolve, reject)
+		local waitTime = initialWaitTime
+
+		local ok, err
+		for _=1, tries do
+			ok, err = CoreGuiUtils.tryToSetCore(unpack(args, 1, n))
+			if ok then
+				return resolve()
+			else
+				wait(waitTime)
+				-- Exponential backoff
+				waitTime = waitTime*2
+			end
+		end
+
+		if not ok then
+			return reject(err)
+		end
+	end)
+end
+
+function CoreGuiUtils.tryToSetCore(...)
+	local args = {...}
+	local n = select("#", ...)
+
+	local ok, err = pcall(function()
+		StarterGui:SetCore(unpack(args, 1, n))
+	end)
+	if not ok then
+		return false, err
+	end
+
+	return true
+end
+
+
+return CoreGuiUtils
