cebe2352bca4a6bc0a051bf9f76af6aa00d739f5
James Onnen <jonnen0@gmail.com>
2020-06-17 16:36:43 -0700

    Make weld constraints function with terrain better

--- Modules/Shared/Physics/Constraints/WeldConstraintUtils.lua
+++ Modules/Shared/Physics/Constraints/WeldConstraintUtils.lua
@@ -8,6 +8,13 @@ function WeldConstraintUtils.namedBetween(name, part0, part1, parent)
 	assert(typeof(part0) == "Instance")
 	assert(typeof(part1) == "Instance")
 
+	if part0:IsA("Terrain") or part1:IsA("Terrain") then
+		-- Terrain likes to remove welds when it deforms for non-touching parts
+		-- Weld constraints do not get affected by this
+		-- https://devforum.roblox.com/t/allow-way-to-prevent-terrain-after-deforming-from-removing-welds/631061
+		return WeldConstraintUtils.namedBetweenForceWeldConstraint(name, part0, part1, parent)
+	end
+
 	-- Roblox weld constraints very buggy!
 	-- https://devforum.roblox.com/t/weld-constraint-behaves-differently-on-server-compared-to-client/445036
 	local weld = Instance.new("Weld")
@@ -18,13 +25,23 @@ function WeldConstraintUtils.namedBetween(name, part0, part1, parent)
 	weld.C1 = part1.CFrame:ToObjectSpace(part0.CFrame)
 	weld.Parent = parent
 
-	-- local weldConstraint = Instance.new("WeldConstraint")
-	-- weldConstraint.Name = name
-	-- weldConstraint.Part0 = part0
-	-- weldConstraint.Part1 = part1
-	-- weldConstraint.Parent = parent
-
 	return weld
 end
+
+function WeldConstraintUtils.namedBetweenForceWeldConstraint(name, part0, part1, parent)
+	assert(typeof(part0) == "Instance")
+	assert(typeof(part1) == "Instance")
+
+	-- Roblox weld constraints very buggy!
+	-- https://devforum.roblox.com/t/weld-constraint-behaves-differently-on-server-compared-to-client/445036
+	local weldConstraint = Instance.new("WeldConstraint")
+	weldConstraint.Name = name
+	weldConstraint.Part0 = part0
+	weldConstraint.Part1 = part1
+	weldConstraint.Parent = parent
+
+	return weldConstraint
+end
+
 
 return WeldConstraintUtils
