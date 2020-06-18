5e5f456975aa3aee9eaa96d418a96e1a4d61ac78
James Onnen <jonnen0@gmail.com>
2020-06-18 14:22:58 -0700

    Improve TextFilterUtils further

--- Modules/Shared/TextService/TextFilterUtils.lua
+++ Modules/Shared/TextService/TextFilterUtils.lua
@@ -9,6 +9,32 @@ local Promise = require("Promise")
 
 local TextFilterUtils = {}
 
+function TextFilterUtils.promiseNonChatStringForBroadcast(str, fromUserId, textContext)
+	assert(type(str) == "string")
+	assert(type(fromUserId) == "number")
+	assert(typeof(textContext) == "EnumItem")
+
+	return TextFilterUtils._promiseTextResult(
+		TextFilterUtils.getNonChatStringForBroadcastAsync,
+		str,
+		fromUserId,
+		textContext)
+end
+
+function TextFilterUtils.promiseNonChatStringForUserAsync(str, fromUserId, toUserId, textContext)
+	assert(type(str) == "string")
+	assert(type(fromUserId) == "number")
+	assert(type(toUserId) == "number")
+	assert(typeof(textContext) == "EnumItem")
+
+	return TextFilterUtils._promiseTextResult(
+		TextFilterUtils.getNonChatStringForUserAsync,
+		str,
+		fromUserId,
+		toUserId,
+		textContext)
+end
+
 function TextFilterUtils.getNonChatStringForBroadcastAsync(str, fromUserId, textContext)
 	assert(type(str) == "string")
 	assert(type(fromUserId) == "number")
@@ -31,18 +57,40 @@ function TextFilterUtils.getNonChatStringForBroadcastAsync(str, fromUserId, text
 	return text
 end
 
-function TextFilterUtils.promiseNonChatStringForBroadcast(str, fromUserId, textContext)
+function TextFilterUtils.getNonChatStringForUserAsync(str, fromUserId, toUserId, textContext)
 	assert(type(str) == "string")
 	assert(type(fromUserId) == "number")
+	assert(type(toUserId) == "number")
 	assert(typeof(textContext) == "EnumItem")
 
+	local text = nil
+	local ok, err = pcall(function()
+		local result = TextService:FilterStringAsync(str, fromUserId, textContext)
+		if not result then
+			error("No TextFilterResult")
+		end
+
+		text = result:GetNonChatStringForUserAsync(toUserId)
+	end)
+
+	if not ok then
+		return false, err
+	end
+
+	return text
+end
+
+function TextFilterUtils._promiseTextResult(getResult, ...)
+	local args = {...}
+
 	local promise = Promise.spawn(function(resolve, reject)
-		local text, err = TextFilterUtils.getNonChatStringForBroadcastAsync(str, fromUserId, textContext)
+		local text, err = getResult(unpack(args))
 		if not text then
 			return reject(err or "Pcall failed")
 		end
-		if not text then
-			return reject("No text")
+
+		if type(text) ~= "string" then
+			return reject("Bad text result")
 		end
 
 		resolve(text)
