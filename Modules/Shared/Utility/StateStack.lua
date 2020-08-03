32b58ee2577599c6b0864550a4f7db4d015860b0
James Onnen <jonnen0@gmail.com>
2020-08-03 13:46:50 -0700

    Add StateStack

---
+++ Modules/Shared/Utility/StateStack.lua
@@ -0,0 +1,56 @@
+---
+-- @classmod StateStack
+-- @author Quenty
+
+local require = require(game:GetService("ReplicatedStorage"):WaitForChild("Nevermore"))
+
+local BaseObject = require("BaseObject")
+
+local StateStack = setmetatable({}, BaseObject)
+StateStack.ClassName = "StateStack"
+StateStack.__index = StateStack
+
+function StateStack.new()
+	local self = setmetatable(BaseObject.new(), StateStack)
+
+	self._state = Instance.new("BoolValue")
+	self._state.Value = false
+	self._maid:GiveTask(self._state)
+
+	self._stateStack = {}
+
+	self.Changed = self._state.Changed
+
+	return self
+end
+
+function StateStack:GetState()
+	return self._state.Value
+end
+
+function StateStack:PushState()
+	local data = {}
+	table.insert(self._stateStack, data)
+
+	self:_updateState()
+
+	return function()
+		self:_popState(data)
+	end
+end
+
+function StateStack:_popState(data)
+	local index = table.find(self._stateStack, data)
+	if index then
+		table.remove(self._stateStack, index)
+		self:_updateState()
+	else
+		warn("[StateStack] - Failed to find index")
+	end
+end
+
+function StateStack:_updateState()
+	self._state.Value = next(self._stateStack) ~= nil
+end
+
+return StateStack
