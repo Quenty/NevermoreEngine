e8e28ac872b72e7b5cb8255dff44672ecd18778e
James Onnen <jonnen0@gmail.com>
2020-12-06 16:43:34 -0800

    Add optional maxWidth

--- Modules/Client/Gui/TextServiceUtils.lua
+++ Modules/Client/Gui/TextServiceUtils.lua
@@ -6,11 +6,14 @@ local TextService = game:GetService("TextService")
 
 local TextServiceUtils = {}
 
-function TextServiceUtils.getSizeForLabel(textLabel, text)
+function TextServiceUtils.getSizeForLabel(textLabel, text, maxWidth)
 	assert(typeof(textLabel) == "Instance")
 	assert(type(text) == "string")
 
-	return TextService:GetTextSize(text, textLabel.TextSize, textLabel.Font, Vector2.new(1e6, 1e6))
+	maxWidth = maxWidth or 1e6
+	assert(maxWidth > 0)
+
+	return TextService:GetTextSize(text, textLabel.TextSize, textLabel.Font, Vector2.new(maxWidth, 1e6))
 end
 
 return TextServiceUtils
