ab3d7018e1b58f6642a03e4e676a3d5dadae5868
James Onnen <jonnen0@gmail.com>
2020-12-29 22:07:07 -0800

    Allow raycaster to be injected

--- Modules/Shared/Physics/Explosions/GetPercentExposedUtils.lua
+++ Modules/Shared/Physics/Explosions/GetPercentExposedUtils.lua
@@ -14,16 +14,24 @@ GetPercentExposed.RAY_COUNT = 314
 -- @tparam Vector3 point point to search
 -- @tparam number Radius
 -- @return A table mapping parts to to percent exposure
-function GetPercentExposed.search(point, radius)
+function GetPercentExposed.search(point, radius, raycaster)
 	local hits = {}
 	local totalHits = 0
 
 	for _=1, GetPercentExposed.RAY_COUNT do
 		local ray = Ray.new(point, RandomVector3Utils.getRandomUnitVector() * radius)
-		local part = Workspace:FindPartOnRay(ray, nil, true) -- Ignore water
-		if part then
-			totalHits = totalHits + 1
-			hits[part] = (hits[part] or 0) + 1
+		if raycaster then
+			local hitData = raycaster:FindPartOnRay(ray)
+			if hitData then
+				totalHits = totalHits + 1
+				hits[hitData.Part] = (hits[hitData.Part] or 0) + 1
+			end
+		else
+			local part = Workspace:FindPartOnRay(ray, nil, true) -- Ignore water
+			if part then
+				totalHits = totalHits + 1
+				hits[part] = (hits[part] or 0) + 1
+			end
 		end
 	end
 
