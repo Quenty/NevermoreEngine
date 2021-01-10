ab35cb91a250427881abe4ce8c15db36a2bb7e5d
James Onnen <jonnen0@gmail.com>
2021-01-10 14:23:58 -0800

    Add remote function utils

---
+++ Modules/Shared/Network/RemoteFunctionUtils.lua
@@ -0,0 +1,34 @@
+---
+-- @module RemoteFunctionUtils
+-- @author Quenty
+
+local require = require(game:GetService("ReplicatedStorage"):WaitForChild("Nevermore"))
+
+local Promise = require("Promise")
+
+local RemoteFunctionUtils = {}
+
+function RemoteFunctionUtils.promiseInvokeServer(remoteFunction, ...)
+	assert(typeof(remoteFunction) == "Instance" and remoteFunction:IsA("RemoteFunction"))
+
+	local args = table.pack(...)
+
+	return Promise.spawn(function(resolve, reject)
+		local results
+		local ok, err = pcall(function()
+			results = table.pack(remoteFunction:InvokeServer(table.unpack(args, 1, args.n)))
+		end)
+
+		if not ok then
+			return reject(err or "Failed to invoke server")
+		end
+
+		if not results then
+			return reject("Failed to get results somehow")
+		end
+
+		return resolve(table.pack(results, 1, results.n))
+	end)
+end
+
+return RemoteFunctionUtils
