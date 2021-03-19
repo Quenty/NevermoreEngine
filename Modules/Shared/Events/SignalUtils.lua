eb27615c3751d429ca6b7e4efe21bfedbf40a301
InvisibleWater <InvisibleWater2345@gmail.com>
2021-03-19 00:01:19 -0400

    Update SignalUtils.lua

--- Modules/Shared/Events/SignalUtils.lua
+++ Modules/Shared/Events/SignalUtils.lua
@@ -1,10 +1,12 @@
 --- Utilities involving signals
 -- @module SignalUtils
+local Nevermore = require(game.ReplicatedStorage:WaitForChild("Nevermore"))
+local Signal = Nevermore("Signal")
 
 local SignalUtils = {}
 
 function SignalUtils.onNext(event, _function)
-	assert(typeof(event) == "RBXScriptSignal")
+	assert(typeof(event) == "RBXScriptSignal" or (type(event) == "table" and getmetatable(event) == Signal))
 	assert(type(_function) == "function")
 
 	local conn
