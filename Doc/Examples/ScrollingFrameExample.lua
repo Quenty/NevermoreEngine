576d1b304bdae3fc02465e47a5ff522701013332
James Onnen <jonnen0@gmail.com>
2021-07-24 15:09:56 -0700

    Remove old example code

--- Doc/Examples/ScrollingFrameExample.lua
+++
@@ -1,7 +0,0 @@
-local require = require(game:GetService("ReplicatedStorage"):WaitForChild("Nevermore"))
-
-local ScrollingFrame = require("ScrollingFrame")
-local Scrollbar = require("Scrollbar")
-
-local scrollingFrame = ScrollingFrame.new(script.Parent)
-scrollingFrame:AddScrollbar(Scrollbar.fromContainer(script.Parent.ScrollbarContainer))
--- Doc/readme.md
+++
@@ -1,7 +0,0 @@
-Nevermore uses [LDoc](https://github.com/stevedonovan/LDoc) to generate docs. Run the following to build documentation (After installing LDoc).
-
-```
-ldoc .
-```
-
-See [here](http://quenty.github.io/api/) for a compiled version of the API.
