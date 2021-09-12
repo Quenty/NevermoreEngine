669fe8e9c023f982bb3584a7aa28f776e1a94a5e
James Onnen <jonnen0@gmail.com>
2021-09-12 14:06:32 -0700

    chore: Add cooldown package

---
+++ src/cooldown/src/Shared/CooldownConstants.lua
@@ -0,0 +1,13 @@
+---
+-- @module CooldownConstants
+-- @author Quenty
+
+local require = require(script.Parent.loader).load(script)
+
+local Table = require("Table")
+
+return Table.readonly({
+	COOLDOWN_NAME = "Cooldown";
+	COOLDOWN_TIME_NAME = "CooldownTime";
+	COOLDOWN_START_TIME_NAME = "CooldownStartTime";
+})
---
+++ src/cooldown/src/Shared/CooldownUtils.lua
@@ -0,0 +1,39 @@
+--- Helper methods for cooldown
+-- @module CooldownUtils
+-- @author Quenty
+
+local require = require(script.Parent.loader).load(script)
+
+local CooldownConstants = require("CooldownConstants")
+
+local CooldownUtils = {}
+
+function CooldownUtils.findCooldown(cooldownBinder, parent)
+	assert(cooldownBinder, "Bad cooldownBinder")
+	assert(typeof(parent) == "Instance", "Bad parent")
+
+	local cooldownObj = parent:FindFirstChild(CooldownConstants.COOLDOWN_NAME)
+	if not cooldownObj then
+		return nil
+	end
+
+	return cooldownBinder:Get(cooldownObj)
+end
+
+function CooldownUtils.create(cooldownBinder, parent, length)
+	assert(cooldownBinder, "Bad cooldownBinder")
+	assert(typeof(parent) == "Instance", "Bad parent")
+	assert(type(length) == "number", "Bad length")
+	assert(length > 0, "Bad length")
+
+	local cooldown = Instance.new("NumberValue")
+	cooldown.Value = length
+	cooldown.Name = CooldownConstants.COOLDOWN_NAME
+	cooldown.Parent = parent
+
+	cooldownBinder:Bind(cooldown)
+
+	return cooldown
+end
+
+return CooldownUtils
