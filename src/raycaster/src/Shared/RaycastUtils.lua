f486bfd08165b162590f3abe49a124ce20ba41af
James Onnen <jonnen0@gmail.com>
2021-10-02 14:09:41 -0700

    feat: Add RaycastUtils

---
+++ src/raycaster/src/Shared/RaycastUtils.lua
@@ -0,0 +1,57 @@
+--- General utility for raycasting
+-- @module RaycastUtils
+-- @author Quenty, AxisAngle
+
+local RaycastUtils = {}
+
+--works for non-convex
+--assumes non-selfintersecting
+--what is the first exit along this line segment?
+function RaycastUtils.raycastSingleExit(origin, direction, part)
+	local params = RaycastParams.new()
+	params.FilterType = Enum.RaycastFilterType.Whitelist
+	params.FilterDescendantsInstances = {part}
+
+	local resultFinal = workspace:Raycast(origin + direction, -direction, params)
+
+	return resultFinal
+end
+
+function RaycastUtils.ignoreCanCollideFalse(part)
+	return not part.CanCollide
+end
+
+function RaycastUtils.raycast(
+	origin, direction,
+	ignoreListWorkingEnvironment,
+	ignoreFunc, keepIgnoreListChanges,
+	ignoreWater
+)
+	local resultFinal
+	local initialIgnoreListLength = #ignoreListWorkingEnvironment
+
+	local params = RaycastParams.new()
+	params.FilterDescendantsInstances = ignoreListWorkingEnvironment
+	params.IgnoreWater = ignoreWater and true or false
+
+	while true do
+		local result = workspace:Raycast(origin, direction, params)
+		if ignoreFunc and result and ignoreFunc(result.Instance) then
+			table.insert(ignoreListWorkingEnvironment, result.Instance)
+			params.FilterDescendantsInstances = ignoreListWorkingEnvironment
+		else
+			resultFinal = result
+			break
+		end
+	end
+
+	if not keepIgnoreListChanges then
+		for i = #ignoreListWorkingEnvironment, initialIgnoreListLength + 1, -1 do
+			ignoreListWorkingEnvironment[i] = nil
+		end
+	end
+
+	return resultFinal
+end
+
+return RaycastUtils
