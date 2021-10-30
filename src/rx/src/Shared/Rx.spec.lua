e58a34425f2056bfbd3a523a6f84f992470baa13
James Onnen <jonnen0@gmail.com>
2021-10-30 12:35:42 -0700

    test: Add Rx.spec.lua tests

---
+++ src/rx/src/Shared/Rx.spec.lua
@@ -0,0 +1,49 @@
+---
+-- @module Rx.spec.lua
+
+local require = require(game:GetService("ServerScriptService"):FindFirstChild("LoaderUtils", true).Parent).load(script)
+
+local Rx = require("Rx")
+
+return function()
+	describe("Rx.combineLatest({})", function()
+		local observe = Rx.combineLatest({})
+		local externalResult
+
+		it("should execute immediately", function()
+			observe:Subscribe(function(result)
+				externalResult = result
+			end)
+
+			expect(externalResult).to.be.a("table")
+		end)
+	end)
+
+	describe("Rx.combineLatest({ value = 5 })", function()
+		local observe = Rx.combineLatest({ value = 5 })
+		local externalResult
+
+		it("should execute immediately", function()
+			observe:Subscribe(function(result)
+				externalResult = result
+			end)
+
+			expect(externalResult).to.be.a("table")
+			expect(externalResult.value).to.equal(5)
+		end)
+	end)
+
+	describe("Rx.combineLatest({ value = Rx.of(5) })", function()
+		local observe = Rx.combineLatest({ value = Rx.of(5) })
+		local externalResult
+
+		it("should execute immediately", function()
+			observe:Subscribe(function(result)
+				externalResult = result
+			end)
+
+			expect(externalResult).to.be.a("table")
+			expect(externalResult.value).to.equal(5)
+		end)
+	end)
+end
