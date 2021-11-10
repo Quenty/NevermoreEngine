e7d99157a8d2de701cbe5261fd27b8eb2a30971f
James Onnen <jonnen0@gmail.com>
2021-11-09 16:31:04 -0800

    fix: Prevent double service initialization from occuring when retrieving service that has not yet been added to service bag

--- src/servicebag/src/Shared/ServiceBag.lua
+++ src/servicebag/src/Shared/ServiceBag.lua
@@ -18,7 +18,8 @@ function ServiceBag.new(parentProvider)
 	self._services = {}
 	self._parentProvider = parentProvider
 
-	self._serviceTypesToInitialize = {}
+	self._serviceTypesToInitializeSet = {}
+	self._initializedServiceTypeSet = {}
 	self._initializing = false
 
 	self._serviceTypesToStart = {}
@@ -41,39 +42,41 @@ function ServiceBag:GetService(serviceType)
 
 	local service = self._services[serviceType]
 	if service then
-		-- Ensure initialized if we're during init phase
-		if self._serviceTypesToInitialize and self._initializing then
-			local index = table.find(self._serviceTypesToInitialize, service)
-			if index then
-				local foundServiceType = assert(table.remove(self._serviceTypesToInitialize, index), "No service removed")
-				assert(foundServiceType == service, "foundServiceType ~= service")
-				self:_initService(foundServiceType)
-			end
+		self:_ensureInitialization(serviceType)
+		return self._services[serviceType]
+	else
+		if self._parentProvider then
+			return self._parentProvider:GetService(serviceType)
 		end
 
+		-- Try to add the service if we're still initializing services
+		self:_addServiceType(serviceType)
+		self:_ensureInitialization(serviceType)
 		return self._services[serviceType]
 	end
+end
 
-	if self._parentProvider then
-		return self._parentProvider:GetService(serviceType)
+function ServiceBag:HasService(serviceType)
+	if self._services[serviceType] then
+		return true
+	else
+		return false
 	end
-
-	-- Try to add the service if we're still initializing services
-	self:_addServiceType(serviceType)
-	return self._services[serviceType]
 end
 
 function ServiceBag:Init()
 	assert(not self._initializing, "Already initializing")
-	assert(self._serviceTypesToInitialize, "Already initialized")
+	assert(self._serviceTypesToInitializeSet, "Already initialized")
 	self._initializing = true
 
-	while next(self._serviceTypesToInitialize) do
-		local serviceType = table.remove(self._serviceTypesToInitialize)
-		self:_initService(serviceType)
+	while next(self._serviceTypesToInitializeSet) do
+		local serviceType = next(self._serviceTypesToInitializeSet)
+		self._serviceTypesToInitializeSet[serviceType] = nil
+
+		self:_ensureInitialization(serviceType)
 	end
 
-	self._serviceTypesToInitialize = nil
+	self._serviceTypesToInitializeSet = nil
 	self._initializing = false
 end
 
@@ -92,9 +95,26 @@ function ServiceBag:Start()
 	self._serviceTypesToStart = nil
 end
 
+function ServiceBag:CreateScope()
+	local provider = ServiceBag.new(self)
+
+	self:_addServiceType(provider)
+
+	-- Remove from parent provider
+	self._maid[provider] = provider._destroying:Connect(function()
+		self._maid[provider] = nil
+		self._services[provider] = nil
+	end)
+
+	return provider
+end
+
 --- Adds a service to this provider only
 function ServiceBag:_addServiceType(serviceType)
-	assert(self._serviceTypesToInitialize, "Already finished initializing, cannot add more services")
+	if not self._serviceTypesToInitializeSet then
+		error(("Already finished initializing, cannot add %q"):format(tostring(serviceType)))
+		return
+	end
 
 	-- Already added
 	if self._services[serviceType] then
@@ -105,11 +125,22 @@ function ServiceBag:_addServiceType(serviceType)
 	local service = setmetatable({}, { __index = serviceType })
 	self._services[serviceType] = service
 
+	self:_ensureInitialization(serviceType)
+end
+
+function ServiceBag:_ensureInitialization(serviceType)
+	if self._initializedServiceTypeSet[serviceType] then
+		return
+	end
+
 	if self._initializing then
-		-- let's initialize immediately
+		self._serviceTypesToInitializeSet[serviceType] = nil
+		self._initializedServiceTypeSet[serviceType] = true
 		self:_initService(serviceType)
+	elseif self._serviceTypesToInitializeSet then
+		self._serviceTypesToInitializeSet[serviceType] = true
 	else
-		table.insert(self._serviceTypesToInitialize, serviceType)
+		error("[ServiceBag._ensureInitialization] - Cannot initialize past initializing phase ")
 	end
 end
 
@@ -123,19 +154,6 @@ function ServiceBag:_initService(serviceType)
 	table.insert(self._serviceTypesToStart, serviceType)
 end
 
-function ServiceBag:CreateScope()
-	local provider = ServiceBag.new(self)
-
-	self._services[provider] = provider
-
-	-- Remove from parent provider
-	self._maid[provider] = provider._destroying:Connect(function()
-		self._maid[provider] = nil
-		self._services[provider] = nil
-	end)
-
-	return provider
-end
 
 function ServiceBag:Destroy()
 	local super = getmetatable(ServiceBag)
