d19f1b2d461b8c09533569ae2636170ede7b1405
Boegie19 <Bramboegheim@gmail.com>
2021-11-23 12:45:40 +0100

    Added asserts and waitTimeBetweenRetys to retry

--- src/promise/src/Shared/PromiseUtils.lua
+++ src/promise/src/Shared/PromiseUtils.lua
@@ -115,12 +115,17 @@ function PromiseUtils.timeout(timeoutTime, fromPromise)
 
 end
 
-function PromiseUtils.retry(callback, times, ...)
+function PromiseUtils.retry(callback, times,waitTimeBetweenRetys, ...)
+	assert(type(callback) == "function", "Bad callback")
+	assert(type(times) == "number", "Bad times")
+	assert(type(waitTimeBetweenRetys) == "number", "Bad waitTime")
+	
 	local args, length = {...}, select("#", ...)
 
 	return callback(...):Catch(function(...)
 		if times > 0 then
-			return PromiseUtils.retry(callback, times - 1, unpack(args, 1, length)) 
+			task.wait(waitTimeBetweenRetys)
+			return PromiseUtils.retry(callback, times - 1,waitTimeBetweenRetys, unpack(args, 1, length)) 
 		else
 			return Promise.rejected(...)
 		end
