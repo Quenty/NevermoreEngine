4f8bd7303b6845c9ecdbe93001b6c2e0505626fc
James Onnen <jonnen0@gmail.com>
2021-12-04 13:12:31 -0800

    fix: Do not search for the animation provider in replicated storage

--- src/animationprovider/src/Shared/AnimationProvider.lua
+++ src/animationprovider/src/Shared/AnimationProvider.lua
@@ -5,15 +5,6 @@
 
 local require = require(script.Parent.loader).load(script)
 
-local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local TaggedTemplateProvider = require("TaggedTemplateProvider")
 
-local provider = TaggedTemplateProvider.new("AnimationContainer")
-
--- This is a fallback animation setup
-local animations = ReplicatedStorage:FindFirstChild("Animations")
-if animations then
-	provider:AddContainer(animations)
-end
-
-return provider
+return TaggedTemplateProvider.new("AnimationContainer")
