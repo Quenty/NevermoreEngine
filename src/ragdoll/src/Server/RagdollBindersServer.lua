77005a7ae2893305c3a29b9df509a6a6bf8559d2
James Onnen <jonnen0@gmail.com>
2021-12-13 17:14:03 -0800

    feat: Ragdoll package automatically binds ragdollable and ragdoll humanoid on death

--- src/ragdoll/src/Server/RagdollBindersServer.lua
+++ src/ragdoll/src/Server/RagdollBindersServer.lua
@@ -6,12 +6,13 @@ local require = require(script.Parent.loader).load(script)
 
 local BinderProvider = require("BinderProvider")
 local Binder = require("Binder")
+local PlayerHumanoidBinder = require("PlayerHumanoidBinder")
 
 return BinderProvider.new(function(self, serviceBag)
 	self:Add(Binder.new("Ragdoll", require("Ragdoll"), serviceBag))
-	self:Add(Binder.new("Ragdollable", require("Ragdollable"), serviceBag))
+	self:Add(PlayerHumanoidBinder.new("Ragdollable", require("Ragdollable"), serviceBag))
 
-	self:Add(Binder.new("RagdollHumanoidOnDeath", require("RagdollHumanoidOnDeath"), serviceBag))
+	self:Add(PlayerHumanoidBinder.new("RagdollHumanoidOnDeath", require("RagdollHumanoidOnDeath"), serviceBag))
 	self:Add(Binder.new("RagdollHumanoidOnFall", require("RagdollHumanoidOnFall"), serviceBag))
 	self:Add(Binder.new("UnragdollAutomatically", require("UnragdollAutomatically"), serviceBag))
 end)
