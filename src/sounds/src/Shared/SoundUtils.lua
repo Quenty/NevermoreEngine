71b5c5b007fc4229b9c10adcbb20ad0c87227550
James Onnen <jonnen0@gmail.com>
2021-12-13 17:22:00 -0800

    feat: Add SoundUtils.playFromId(id)

--- src/sounds/src/Shared/SoundUtils.lua
+++ src/sounds/src/Shared/SoundUtils.lua
@@ -15,11 +15,38 @@ function SoundUtils.playTemplate(templates, templateName)
 
 	SoundService:PlayLocalSound(sound)
 
-	delay(sound.TimeLength + 0.05, function()
+	task.delay(sound.TimeLength + 0.05, function()
 		sound:Destroy()
 	end)
 
 	return sound
+end
+
+function SoundUtils.playFromId(id)
+	local soundId = SoundUtils.toRbxAssetId(id)
+	assert(type(soundId) == "string", "Bad id")
+
+	local sound = Instance.new("Sound")
+	sound.Name = ("Sound_%s"):format(soundId)
+	sound.SoundId = soundId
+	sound.Volume = 0.25
+	sound.Archivable = false
+
+	SoundService:PlayLocalSound(sound)
+
+	task.delay(sound.TimeLength + 0.05, function()
+		sound:Destroy()
+	end)
+
+	return sound
+end
+
+function SoundUtils.toRbxAssetId(id)
+	if type(id) == "number" then
+		return ("rbxassetid://%d"):format(id)
+	else
+		return id
+	end
 end
 
 function SoundUtils.playTemplateInParent(templates, templateName, parent)
@@ -29,7 +56,7 @@ function SoundUtils.playTemplateInParent(templates, templateName, parent)
 
 	sound:Play()
 
-	delay(sound.TimeLength + 0.05, function()
+	task.delay(sound.TimeLength + 0.05, function()
 		sound:Destroy()
 	end)
 
