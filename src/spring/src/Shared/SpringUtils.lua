a7706970e4de8969bdd0f5a8b54e1803c89f9e87
James Onnen <jonnen0@gmail.com>
2021-12-16 17:50:00 -0800

    feat: Allow transformation of Color3 into a linear value for interpolation

--- src/spring/src/Shared/SpringUtils.lua
+++ src/spring/src/Shared/SpringUtils.lua
@@ -4,6 +4,9 @@
 
 local EPSILON = 1e-6
 
+local require = require(script.Parent.loader).load(script)
+local LinearValue = require("LinearValue")
+
 local SpringUtils = {}
 
 function SpringUtils.animating(spring, epsilon)
@@ -16,7 +19,7 @@ function SpringUtils.animating(spring, epsilon)
 	if type(target) == "number" then
 		animating = math.abs(spring.Position - spring.Target) > epsilon
 			or math.abs(spring.Velocity) > epsilon
-	elseif typeof(target) == "Vector3" then
+	elseif typeof(target) == "Vector3" or LinearValue.isLinear(target) then
 		animating = (spring.Position - spring.Target).magnitude > epsilon
 			or spring.Velocity.magnitude > epsilon
 	else
@@ -38,6 +41,22 @@ function SpringUtils.getVelocityAdjustment(velocity, dampen, speed)
 	assert(speed, "Bad speed")
 
 	return velocity*(2*dampen/speed)
+end
+
+function SpringUtils.toLinearIfNeeded(value)
+	if typeof(value) == "Color3" then
+		return LinearValue.new(Color3.new, {value.r, value.g, value.b})
+	else
+		return value
+	end
+end
+
+function SpringUtils.fromLinearIfNeeded(value)
+	if LinearValue.isLinear(value) then
+		return value:ToBaseValue()
+	else
+		return value
+	end
 end
 
 return SpringUtils
