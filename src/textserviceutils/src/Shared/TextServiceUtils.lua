804a2d783a21aaf7ac7f583f7d1c5c9f99bf70bf
James Onnen <jonnen0@gmail.com>
2021-12-21 21:25:30 -0800

    feat: Add observeSizeForLabelProps

--- src/textserviceutils/src/Shared/TextServiceUtils.lua
+++ src/textserviceutils/src/Shared/TextServiceUtils.lua
@@ -4,6 +4,10 @@
 
 local TextService = game:GetService("TextService")
 
+local require = require(script.Parent.loader).load(script)
+
+local Blend = require("Blend")
+
 local TextServiceUtils = {}
 
 function TextServiceUtils.getSizeForLabel(textLabel, text, maxWidth)
@@ -14,6 +18,17 @@ function TextServiceUtils.getSizeForLabel(textLabel, text, maxWidth)
 	assert(maxWidth > 0, "Bad maxWidth")
 
 	return TextService:GetTextSize(text, textLabel.TextSize, textLabel.Font, Vector2.new(maxWidth, 1e6))
+end
+
+function TextServiceUtils.observeSizeForLabelProps(props)
+	assert(props.Text, "Bad props.Text")
+	assert(props.TextSize, "Bad props.TextSize")
+	assert(props.Font, "Bad props.Font")
+
+	return Blend.Computed(props.Text, props.TextSize, props.Font, props.MaxSize or Vector2.new(1e6, 1e6),
+		function(text, textSize, font, maxSize)
+			return TextService:GetTextSize(text, textSize, font, maxSize)
+		end)
 end
 
 return TextServiceUtils
