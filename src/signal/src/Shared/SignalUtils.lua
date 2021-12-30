4af6d2a9642c8b916e3b200da0f1f6553537f15a
James Onnen <jonnen0@gmail.com>
2021-12-30 00:40:35 -0800

    docs: Add documentation on SignalUtils

--- src/signal/src/Shared/SignalUtils.lua
+++ src/signal/src/Shared/SignalUtils.lua
@@ -5,6 +5,12 @@
 
 local SignalUtils = {}
 
+--[=[
+	Executes on the next event connection.
+	@param event RBXScriptSignal
+	@param _function function
+	@return RBXScriptConnection
+]=]
 function SignalUtils.onNext(event, _function)
 	assert(typeof(event) == "RBXScriptSignal", "Bad event")
 	assert(type(_function) == "function", "Bad _function")
