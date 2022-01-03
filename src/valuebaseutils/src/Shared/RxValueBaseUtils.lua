776b13ec207b98ca0b4e59aafaf09e2c4c2e2f6c
James Onnen <jonnen0@gmail.com>
2022-01-02 17:11:36 -0800

    feat: Deprecate RxValueBaseUtils.observe and add RxValueBaseUtils.observeBrio

--- src/valuebaseutils/src/Shared/RxValueBaseUtils.lua
+++ src/valuebaseutils/src/Shared/RxValueBaseUtils.lua
@@ -9,8 +9,21 @@ local RxBrioUtils = require("RxBrioUtils")
 
 local RxValueBaseUtils = {}
 
+--[=[
+	:::warning
+	This caches the last value seen, and may memory leak.
+	:::
+
+	@param parent Instance
+	@param className string
+	@param name string
+	@return Observable<any>
+	:::
+]=]
 -- TODO: Handle default value/nothing there, instead of memory leaking!
 function RxValueBaseUtils.observe(parent, className, name)
+	warn("[RxValueBaseUtils.observe] - Deprecated since 4.0.0. Use RxValueBaseUtils.observeBrio")
+
 	return RxInstanceUtils.observeLastNamedChildBrio(parent, className, name)
 		:Pipe({
 			RxBrioUtils.switchMap(function(valueObject)
@@ -19,6 +32,29 @@ function RxValueBaseUtils.observe(parent, className, name)
 		})
 end
 
+--[=[
+	Observes a value base underneath a parent (last named child).
+
+	@param parent Instance
+	@param className string
+	@param name string
+	@return Observable<Brio<any>>
+]=]
+function RxValueBaseUtils.observeBrio(parent, className, name)
+	return RxInstanceUtils.observeLastNamedChildBrio(parent, className, name)
+		:Pipe({
+			RxBrioUtils.switchMapBrio(function(valueObject)
+				return RxValueBaseUtils.observeValue(valueObject)
+			end)
+		})
+end
+
+
+--[=[
+	Observables a given value object's value
+	@param valueObject Instance
+	@return Observable<T>
+]=]
 function RxValueBaseUtils.observeValue(valueObject)
 	return RxInstanceUtils.observeProperty(valueObject, "Value")
 end
