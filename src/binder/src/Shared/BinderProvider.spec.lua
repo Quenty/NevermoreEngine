146501e39873547403d502089f12f0de4122b409
James Onnen <jonnen0@gmail.com>
2022-01-07 11:27:58 -0800

    fix: Binders initialize properly and provider better warnings

---
+++ src/binder/src/Shared/BinderProvider.spec.lua
@@ -0,0 +1,41 @@
+--[[
+	@class BinderProvider.spec.lua
+]]
+
+local require = require(game:GetService("ServerScriptService"):FindFirstChild("LoaderUtils", true).Parent).load(script)
+
+local BinderProvider = require("BinderProvider")
+local Binder = require("Binder")
+
+return function()
+	describe("BinderProvider.new()", function()
+		local provider
+		local initialized = false
+
+		it("should execute immediately", function()
+			provider = BinderProvider.new(function(self, arg)
+				initialized = true
+				assert(arg == 12345, "Bad arg")
+
+				self:Add(Binder.new("Test", function()
+					return { Destroy = function() end; }
+				end))
+			end)
+
+			expect(provider).to.be.a("table")
+		end)
+
+		it("should initialize", function()
+			expect(initialized).to.equal(false)
+			provider:Init(12345)
+			expect(initialized).to.equal(true)
+		end)
+
+		it("should contain the binder", function()
+			expect(provider.Test).to.be.a("table")
+		end)
+
+		provider:Destroy()
+	end)
+
+end
