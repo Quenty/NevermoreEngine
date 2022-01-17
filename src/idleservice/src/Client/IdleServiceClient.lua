12a258e5546397244c9f2eb788d40f03007a2658
James Onnen <jonnen0@gmail.com>
2022-01-17 14:57:37 -0800

    feat!: Rename IdleService to IdleServiceClient

--- src/idleservice/src/Client/IdleServiceClient.lua
+++ src/idleservice/src/Client/IdleServiceClient.lua
@@ -1,6 +1,6 @@
 --[=[
 	Helps track whether or not a player is idle and if so, then can show UI or other cute things
-	@class IdleService
+	@class IdleServiceClient
 ]=]
 
 local require = require(script.Parent.loader).load(script)
@@ -12,19 +12,24 @@ local HumanoidTrackerService = require("HumanoidTrackerService")
 local Maid = require("Maid")
 local RagdollBindersClient = require("RagdollBindersClient")
 local StateStack = require("StateStack")
+local RxValueBaseUtils = require("RxValueBaseUtils")
 
-local IdleService = {}
+local IdleServiceClient = {}
 
 local STANDING_TIME_REQUIRED = 0.5
 
-function IdleService:Init(serviceBag)
+function IdleServiceClient:Init(serviceBag)
 	assert(not self._maid, "Already initialized")
 
 	self._maid = Maid.new()
-	self._humanoidTracker = serviceBag:GetService(HumanoidTrackerService):GetHumanoidTracker()
-	self._ragdollBindersClient = serviceBag:GetService(RagdollBindersClient)
+	self._serviceBag = assert(serviceBag, "No serviceBag")
 
-	self._disabledStack = StateStack.new()
+	-- External
+	self._serviceBag:GetService(require("RagdollServiceClient"))
+	self._humanoidTracker = self._serviceBag:GetService(HumanoidTrackerService):GetHumanoidTracker()
+	self._ragdollBindersClient = self._serviceBag:GetService(RagdollBindersClient)
+
+	self._disabledStack = StateStack.new(false)
 	self._maid:GiveTask(self._disabledStack)
 
 	self._enabled = Instance.new("BoolValue")
@@ -38,7 +43,9 @@ function IdleService:Init(serviceBag)
 	self._humanoidIdle = Instance.new("BoolValue")
 	self._humanoidIdle.Value = false
 	self._maid:GiveTask(self._humanoidIdle)
+end
 
+function IdleServiceClient:Start()
 	self._maid:GiveTask(self._humanoidIdle.Changed:Connect(function()
 		self:_updateShowIdleUI()
 	end))
@@ -59,39 +66,43 @@ function IdleService:Init(serviceBag)
 	self:_updateShowIdleUI()
 end
 
-function IdleService:IsHumanoidIdle()
+function IdleServiceClient:IsHumanoidIdle()
 	return self._humanoidIdle.Value
 end
 
-function IdleService:DoShowIdleUI()
+function IdleServiceClient:DoShowIdleUI()
 	return self._showIdleUI.Value
 end
 
-function IdleService:GetShowIdleUIBoolValue()
+function IdleServiceClient:ObserveShowIdleUI()
+	return RxValueBaseUtils.observeValue(self._showIdleUI)
+end
+
+function IdleServiceClient:GetShowIdleUIBoolValue()
 	assert(self._showIdleUI, "Not initialized")
 
 	return self._showIdleUI
 end
 
-function IdleService:PushDisable()
+function IdleServiceClient:PushDisable()
 	if not RunService:IsRunning() then
 		return function() end
 	end
 
 	assert(self._disabledStack, "Not initialized")
-	return self._disabledStack:PushState()
+	return self._disabledStack:PushState(true)
 end
 
-function IdleService:_setEnabled(enabled)
+function IdleServiceClient:_setEnabled(enabled)
 	assert(type(enabled) == "boolean", "Bad enabled")
 	self._enabled.Value = enabled
 end
 
-function IdleService:_updateShowIdleUI()
+function IdleServiceClient:_updateShowIdleUI()
 	self._showIdleUI.Value = self._humanoidIdle.Value and self._enabled.Value and not VRService.VREnabled
 end
 
-function IdleService:_handleAliveHumanoidChanged()
+function IdleServiceClient:_handleAliveHumanoidChanged()
 	local humanoid = self._humanoidTracker.AliveHumanoid.Value
 	if not humanoid then
 		self._maid._humanoidMaid = nil
@@ -135,4 +146,4 @@ function IdleService:_handleAliveHumanoidChanged()
 	self._maid._humanoidMaid = maid
 end
 
-return IdleService
+return IdleServiceClient
