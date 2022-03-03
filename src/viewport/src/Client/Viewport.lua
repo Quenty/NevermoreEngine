2634a43f399514b9dd2b8f5411d36481a8d94813
James Onnen <jonnen0@gmail.com>
2022-03-03 11:38:29 -0800

    refactor: Move Viewport to Shared area

--- src/viewport/src/Client/Viewport.lua
+++ src/viewport/src/Client/Viewport.lua
@@ -26,8 +26,11 @@ Viewport.__index = Viewport
 function Viewport.new()
 	local self = setmetatable(BasicPane.new(), Viewport)
 
-	self._current = ValueObject.new()
+	self._current = ValueObject.new(nil)
 	self._maid:GiveTask(self._current)
+
+	self._transparency = ValueObject.new(0)
+	self._maid:GiveTask(self._transparency)
 
 	self._absoluteSize = ValueObject.new(Vector2.new())
 	self._maid:GiveTask(self._absoluteSize)
@@ -52,13 +55,29 @@ function Viewport.blend(props)
 		local maid = Maid.new()
 
 		local viewport = Viewport.new()
-		if props.FieldOfView then
-			viewport:SetFieldOfView(props.FieldOfView)
+
+		local function bindObservable(propName, callback)
+			if props[propName] then
+				local observe = Blend.toPropertyObservable(props[propName])
+				if observe then
+					maid:GiveTask(observe:Subscribe(function(value)
+						callback(value)
+					end))
+				else
+					callback(props[propName])
+				end
+			end
 		end
 
-		if props.Instance then
-			viewport:SetInstance(props.Instance)
-		end
+		bindObservable("FieldOfView", function(value)
+			viewport:SetFieldOfView(value)
+		end)
+		bindObservable("Instance", function(value)
+			viewport:SetInstance(value)
+		end)
+		bindObservable("Transparency", function(value)
+			viewport:SetTransparency(value)
+		end)
 
 		maid:GiveTask(viewport:Render(props):Subscribe(function(result)
 			sub:Fire(result)
@@ -68,6 +87,12 @@ function Viewport.blend(props)
 	end)
 end
 
+function Viewport:SetTransparency(transparency)
+	assert(type(transparency) == "number", "Bad transparency")
+
+	self._transparency.Value = transparency
+end
+
 function Viewport:SetFieldOfView(fieldOfView)
 	assert(type(fieldOfView) == "number", "Bad fieldOfView")
 
@@ -75,7 +100,7 @@ function Viewport:SetFieldOfView(fieldOfView)
 end
 
 function Viewport:SetInstance(instance)
-	assert(typeof(instance) == "Instance", "Bad instance")
+	assert(typeof(instance) == "Instance" or instance == nil, "Bad instance")
 
 	self._current.Value = instance
 end
@@ -109,9 +134,35 @@ function Viewport:Render(props)
 		CurrentCamera = currentCamera;
 		LightColor = props.LightColor or Color3.new(1, 1, 1);
 		Ambient = props.Ambient or Color3.new(1, 1, 1);
+		ImageTransparency = self._transparency;
 		[Blend.OnChange "AbsoluteSize"] = self._absoluteSize;
 		[Blend.Attached(function(viewport)
 			return ViewportControls.new(viewport, self)
+		end)] = true;
+		[Blend.Attached(function(viewport)
+			-- custom parenting scheme to ensure we don't call destroy on children
+			local maid = Maid.new()
+
+			local function update()
+				local value = self._current.Value
+				if value then
+					value.Parent = viewport
+				end
+			end
+
+			maid:GiveTask(self._current.Changed:Connect(update))
+			update()
+
+			maid:GiveTask(function()
+				local value = self._current.Value
+
+				-- Ensure we don't call :Destroy() on our preview instance.
+				if value then
+					value.Parent = nil
+				end
+			end)
+
+			return maid
 		end)] = true;
 		[Blend.Children] = {
 			self._current;
@@ -132,7 +183,6 @@ function Viewport:Render(props)
 						if not bbCFrame then
 							return CFrame.new()
 						end
-
 
 						local fit = CameraUtils.fitBoundingBoxToCamera(bbSize, fov, aspectRatio)
 						return CFrame.new(bbCFrame.p) * CFrame.Angles(0, rotationYaw, 0) * CFrame.Angles(rotationPitch, 0, 0) * CFrame.new(0, 0, fit)
