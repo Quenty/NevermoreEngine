5931f101809a19a0352eb124919a08fe8609dba1
James Onnen <jonnen0@gmail.com>
2022-05-21 14:50:57 -0700

    feat: Add RxR15Utils.observeRigMotorBrio() and RxR15Utils.observeCharacterPart()

--- src/r15utils/src/Shared/RxR15Utils.lua
+++ src/r15utils/src/Shared/RxR15Utils.lua
@@ -1,4 +1,5 @@
 --[=[
+	Utility methods to query components of an R15 character.
 	@class RxR15Utils
 ]=]
 
@@ -9,17 +10,57 @@ local RxBrioUtils = require("RxBrioUtils")
 
 local RxR15Utils = {}
 
+--[=[
+	Observes a rig attachment as a brio
+	@param character Model
+	@param partName string
+	@Param attachmentName string
+	@return Observable<Brio<Attachment>>
+]=]
 function RxR15Utils.observeRigAttachmentBrio(character, partName, attachmentName)
 	assert(typeof(character) == "Instance", "Bad character")
 	assert(type(partName) == "string", "Bad partName")
 	assert(type(attachmentName) == "string", "Bad attachmentName")
 
-	return RxInstanceUtils.observeLastNamedChildBrio(character, "BasePart", partName)
+	return RxR15Utils.observeCharacterPart(character, partName)
 		:Pipe({
 			RxBrioUtils.switchMapBrio(function(part)
 				return RxInstanceUtils.observeLastNamedChildBrio(part, "Attachment", attachmentName)
 			end);
 		})
+end
+
+--[=[
+	Observes a rig motor as a brio
+	@param character Model
+	@param partName string
+	@Param motorName string
+	@return Observable<Brio<Motor6D>>
+]=]
+function RxR15Utils.observeRigMotorBrio(character, partName, motorName)
+	assert(typeof(character) == "Instance", "Bad character")
+	assert(type(partName) == "string", "Bad partName")
+	assert(type(motorName) == "string", "Bad motorName")
+
+	return RxInstanceUtils.observeLastNamedChildBrio(character, "BasePart", partName)
+		:Pipe({
+			RxBrioUtils.switchMapBrio(function(part)
+				return RxInstanceUtils.observeLastNamedChildBrio(part, "Motor6D", motorName)
+			end);
+		})
+end
+
+--[=[
+	Observes a rig motor as a brio
+	@param character Model
+	@param partName string
+	@return Observable<Brio<BasePart>>
+]=]
+function RxR15Utils.observeCharacterPart(character, partName)
+	assert(typeof(character) == "Instance", "Bad character")
+	assert(type(partName) == "string", "Bad partName")
+
+	return RxInstanceUtils.observeLastNamedChildBrio(character, "BasePart", partName)
 end
 
 return RxR15Utils
