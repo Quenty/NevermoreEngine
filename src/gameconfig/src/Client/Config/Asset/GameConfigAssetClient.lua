23c78abbb5f1dcf2de8c8c855dfe20a8a58b4e53
James Onnen <jonnen0@gmail.com>
2022-08-14 12:57:26 -0700

    fix: Fix localization failure occuring on GameConfigAssets where source would be the same across the same asset so the cloud table wouldn't publish

--- src/gameconfig/src/Client/Config/Asset/GameConfigAssetClient.lua
+++ src/gameconfig/src/Client/Config/Asset/GameConfigAssetClient.lua
@@ -7,6 +7,7 @@ local require = require(script.Parent.loader).load(script)
 local GameConfigAssetBase = require("GameConfigAssetBase")
 local GameConfigTranslator = require("GameConfigTranslator")
 local Rx = require("Rx")
+local PseudoLocalize = require("PseudoLocalize")
 
 local GameConfigAssetClient = setmetatable({}, GameConfigAssetBase)
 GameConfigAssetClient.ClassName = "GameConfigAssetClient"
@@ -38,21 +39,26 @@ function GameConfigAssetClient:ObserveTranslatedName()
 	-- TODO: Multicast
 
 	return Rx.combineLatest({
+		assetKey = self:ObserveAssetKey();
 		translationKey = self:ObserveNameTranslationKey();
 		text = self:ObserveCloudName();
 	}):Pipe({
+		Rx.throttleDefer();
 		Rx.switchMap(function(state)
-			if type(state.translationKey) == "string" and state.text then
+			if type(state.translationKey) == "string" and state.text and state.assetKey then
 				-- Immediately write if necessary
 
 				local localizationTable = self._configTranslator:GetLocalizationTable()
 				local key = state.translationKey
-				local source = ""
-				local context = ""
+				local source = state.text
+				local context = ("GameConfigAsset.%s"):format(state.assetKey)
 				local localeId = "en"
 				local value = state.text
 
 				localizationTable:SetEntryValue(key, source, context, localeId, value)
+				localizationTable:SetEntryValue(key, source, context,
+					PseudoLocalize.getDefaultPseudoLocaleId(),
+					PseudoLocalize.pseudoLocalize(value))
 
 				return self._configTranslator:ObserveFormatByKey(state.translationKey)
 			else
@@ -70,21 +76,26 @@ function GameConfigAssetClient:ObserveTranslatedDescription()
 	-- TODO: Multicast
 
 	return Rx.combineLatest({
+		assetKey = self:ObserveAssetKey();
 		translationKey = self:ObserveDescriptionTranslationKey();
 		text = self:ObserveCloudDescription();
 	}):Pipe({
+		Rx.throttleDefer();
 		Rx.switchMap(function(state)
-			if type(state.translationKey) == "string" and state.text then
+			if type(state.translationKey) == "string" and state.text and state.assetKey then
 				-- Immediately write if necessary
 
 				local localizationTable = self._configTranslator:GetLocalizationTable()
 				local key = state.translationKey
-				local source = ""
-				local context = ""
+				local source = state.text
+				local context = ("GameConfigAsset.%s"):format(state.assetKey)
 				local localeId = "en"
 				local value = state.text
 
 				localizationTable:SetEntryValue(key, source, context, localeId, value)
+				localizationTable:SetEntryValue(key, source, context,
+					PseudoLocalize.getDefaultPseudoLocaleId(),
+					PseudoLocalize.pseudoLocalize(value))
 
 				return self._configTranslator:ObserveFormatByKey(state.translationKey)
 			else
