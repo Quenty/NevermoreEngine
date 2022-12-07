e0e8e44a21692d4c383274985d01a965dcfe389c
James Onnen <jonnen0@gmail.com>
2022-12-07 12:26:10 -0800

    feat: Add ability to generate new Nevermore library packages

---
+++ tools/nevermore-cli/templates/nevermore-library-package-template/src/Shared/{{packageNameProper}}Utils.lua
@@ -0,0 +1,9 @@
+--[=[
+	@class {{packageNameProper}}Utils
+]=]
+
+local require = require(script.Parent.loader).load(script)
+
+local {{packageNameProper}}Utils = {}
+
+return {{packageNameProper}}Utils
---
+++ tools/nevermore-template-helpers/README.md
@@ -0,0 +1,3 @@
+## Nevermore Template Helpers
+
+This library helps generate or create templates for packages or other scenarios.
