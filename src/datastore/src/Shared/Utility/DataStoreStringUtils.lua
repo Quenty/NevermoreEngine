d3b245a8fbd469ec51fb973a72cefc868e94c99a
James Onnen <jonnen0@gmail.com>
2023-01-15 18:32:33 -0800

    feat: Add DataStoreStringUtils.isValidUTF8(str)

---
+++ src/datastore/src/Shared/Utility/DataStoreStringUtils.lua
@@ -0,0 +1,30 @@
+--[=[
+	Utility methods to validate strings which can be used to attack datastores in a string saving attack otherwise.
+
+	@class DataStoreStringUtils
+]=]
+
+local require = require(script.Parent.loader).load(script)
+
+local DataStoreStringUtils = {}
+
+--[=[
+	Checks to see if a string can be stored in the datastore
+
+	@param str string
+	@return boolean
+]=]
+function DataStoreStringUtils.isValidUTF8(str)
+	if type(str) ~= "string" then
+		return false, "Not a string"
+	end
+
+	-- https://gist.github.com/TheGreatSageEqualToHeaven/e0e1dc2698307c93f6013b9825705899?permalink_comment_id=4334757#gistcomment-4334757
+	if utf8.len(str) == nil then
+		return false, "Invalid string"
+	end
+
+	return true
+end
+
+return DataStoreStringUtils
