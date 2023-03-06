054ff850302982ad52e6a781c40e9635ec95b3ce
James Onnen <jonnen0@gmail.com>
2023-03-05 20:33:14 -0800

    feat: Add flipbook package (which replaces AnimatedSpritesheet and AnimatedSpritesheetPlayer)

---
+++ src/flipbook/src/Client/Flipbook.lua
@@ -0,0 +1,160 @@
+--[=[
+	A spritesheet that is animated. See [FlipbookPlayer] for playback.
+	@class Flipbook
+]=]
+
+local require = require(script.Parent.loader).load(script)
+
+local Sprite = require("Sprite")
+
+--[=[
+	@interface FlipbookData
+	.imageId string
+	.frameCount number
+	.rows number
+	.columns number
+	.imageRectSize Vector2
+	.frameRate number
+	@within Flipbook
+]=]
+
+local Flipbook = {}
+Flipbook.ClassName = "Flipbook"
+Flipbook.__index = Flipbook
+
+--[=[
+	Constructs a new Flipbook
+	@param data FlipbookData
+	@return Flipbook
+]=]
+function Flipbook.new(data)
+	local self = setmetatable({}, Flipbook)
+
+	self._frameSprites = {}
+	self._frameRate = 60
+
+	if data.frameCount then
+		self:SetFrameCount(data.frameCount)
+	else
+		assert(data.columns, "Bad columns")
+		assert(data.columns, "Bad rows")
+
+		self:SetFrameCount(data.rows*data.columns)
+	end
+
+	if data.frameRate then
+		self:SetFrameRate(data.frameRate)
+	end
+
+	do
+		local image = assert(data.image, "Bad image")
+		local rows = assert(data.rows, "Bad rows")
+		local imageRectSize = assert(data.imageRectSize, "Bad imageRectSize")
+
+		self:_loadFrames(image, rows, imageRectSize)
+	end
+
+	return self
+end
+
+function Flipbook.isFlipbook(value)
+	return type(value) == "table" and getmetatable(value) == Flipbook
+end
+
+function Flipbook:_loadFrames(image, rows, imageRectSize)
+	assert(type(rows) == "number", "Bad rows")
+	assert(typeof(imageRectSize) == "Vector2", "Bad imageRectSize")
+
+	self:SetImageRectSize(imageRectSize)
+
+	for i=0, self._frameCount do
+		local x = i % rows
+		local y = math.floor(i / rows)
+		local position = imageRectSize*Vector2.new(x, y)
+
+		local index = i + 1
+		local name = string.format("Frame_%d", index)
+
+		self:SetSpriteAtIndex(index, Sprite.new({
+			Texture = image;
+			Size = imageRectSize;
+			Position = position;
+			Name = name;
+		}))
+	end
+end
+
+function Flipbook:SetSpriteAtIndex(index, sprite)
+	assert(type(index) == "number", "Bad index")
+	assert(type(sprite) == "table", "Bad sprite")
+
+	self._frameSprites[index] = sprite
+end
+
+function Flipbook:SetImageRectSize(imageRectSize)
+	assert(typeof(imageRectSize) == "Vector2", "No imageRectSize")
+
+	self._imageRectSize = imageRectSize
+end
+
+function Flipbook:SetFrameCount(frameCount)
+	assert(type(frameCount) == "number", "Bad frameCount")
+
+	self._frameCount = frameCount
+end
+
+function Flipbook:SetFrameRate(frameRate)
+	assert(type(frameRate) == "number", "Bad frameRate")
+
+	self._frameRate = frameRate
+end
+
+function Flipbook:GetPreloadAssetId()
+	return { self._imageId }
+end
+
+function Flipbook:GetSprite(index)
+	assert(type(index) == "number", "Bad index")
+
+	return self._frameSprites[index]
+end
+
+function Flipbook:HasSprite(index)
+	assert(type(index) == "number", "Bad index")
+
+	return self._frameSprites[index] ~= nil
+end
+
+--[=[
+	Gets the sprite size
+	@return Vector2
+]=]
+function Flipbook:GetImageRectSize()
+	return self._imageRectSize
+end
+
+--[=[
+	Gets the frames per a second
+	@return number
+]=]
+function Flipbook:GetFrameRate()
+	return self._frameRate
+end
+
+--[=[
+	Gets the play time for the animated sheet
+	@return number
+]=]
+function Flipbook:GetPlayTime()
+	return self._frameCount/self._frameRate
+end
+
+--[=[
+	Retrieves the frames for the sprite sheet.
+	@return frames
+]=]
+function Flipbook:GetFrameCount()
+	return self._frameCount
+end
+
+return Flipbook
---
+++ src/flipbook/src/Client/Player/FlipbookPlayer.story.lua
@@ -0,0 +1,140 @@
+--[[
+	@class Flipbook.story
+]]
+
+local require = require(game:GetService("ServerScriptService"):FindFirstChild("LoaderUtils", true).Parent).load(script)
+
+local Flipbook = require("Flipbook")
+local FlipbookPlayer = require("FlipbookPlayer")
+local Maid = require("Maid")
+local UICornerUtils = require("UICornerUtils")
+
+local function makeLabel(maid, flipbook, target, isBoomarang)
+	local size = flipbook:GetImageRectSize()
+
+	local imageLabel = Instance.new("ImageLabel")
+	imageLabel.BackgroundTransparency = 0
+	imageLabel.BackgroundColor3 = Color3.new(0.5, 0.5, 0.5)
+	imageLabel.Size = UDim2.fromOffset(size.x, size.y)
+	imageLabel.BorderSizePixel = 0
+	maid:GiveTask(imageLabel)
+
+	UICornerUtils.fromOffset(8, imageLabel)
+
+	local flipbookPlayer = FlipbookPlayer.new(imageLabel)
+	flipbookPlayer:SetFlipbook(flipbook)
+	maid:GiveTask(flipbookPlayer)
+
+	if isBoomarang then
+		flipbookPlayer:SetIsBoomarang(true)
+	end
+
+	flipbookPlayer:Play()
+
+	imageLabel.Parent = target
+
+	return imageLabel
+end
+
+local function makeButton(maid, flipbook, target, isBoomarang)
+	local size = flipbook:GetImageRectSize()
+
+	local imageButton = Instance.new("ImageButton")
+	imageButton.BackgroundTransparency = 0
+	imageButton.BackgroundColor3 = Color3.new(0.5, 0.5, 0.5)
+	imageButton.Size = UDim2.fromOffset(size.x, size.y)
+	imageButton.BorderSizePixel = 0
+	maid:GiveTask(imageButton)
+
+	UICornerUtils.fromOffset(8, imageButton)
+
+	local flipbookPlayer = FlipbookPlayer.new(imageButton)
+	flipbookPlayer:SetFlipbook(flipbook)
+	maid:GiveTask(flipbookPlayer)
+
+	if isBoomarang then
+		flipbookPlayer:SetIsBoomarang(true)
+	end
+
+	maid:GiveTask(imageButton.Activated:Connect(function()
+		flipbookPlayer:PromisePlayOnce()
+	end))
+
+	imageButton.Parent = target
+
+	return imageButton
+end
+
+return function(target)
+	local maid = Maid.new()
+
+	local container = Instance.new("Frame")
+	container.Size = UDim2.new(1, 0, 1, 0)
+	container.BackgroundTransparency = 0
+	container.BackgroundColor3 = Color3.new(0, 0, 0)
+	container.Parent = target
+	maid:GiveTask(container)
+
+	local uiListLayout = Instance.new("UIListLayout")
+	uiListLayout.FillDirection = Enum.FillDirection.Horizontal
+	uiListLayout.VerticalAlignment = Enum.VerticalAlignment.Center
+	uiListLayout.HorizontalAlignment = Enum.HorizontalAlignment.Center
+	uiListLayout.Padding = UDim.new(0, 10)
+	uiListLayout.SortOrder = Enum.SortOrder.LayoutOrder
+	uiListLayout.Parent = container
+
+	makeLabel(maid, Flipbook.new({
+		image = "rbxassetid://5085366281";
+		rows = 6;
+		columns = 6;
+		frameCount = 34;
+		imageRectSize = Vector2.new(85.33333, 85.33333);
+		frameRate = 20;
+	}), container, false)
+
+	makeLabel(maid, Flipbook.new({
+		image = "rbxassetid://5085366281";
+		rows = 6;
+		columns = 6;
+		frameCount = 34;
+		imageRectSize = Vector2.new(85.33333, 85.33333);
+		frameRate = 20;
+	}), container, true)
+
+	makeLabel(maid, Flipbook.new({
+		image = "rbxassetid://8966463176";
+		rows = 6;
+		columns = 6;
+		imageRectSize = Vector2.new(170, 170);
+		frameRate = 50;
+	}), container, false)
+
+	makeLabel(maid, Flipbook.new({
+		image = "rbxassetid://9234616869";
+		rows = 6;
+		columns = 6;
+		imageRectSize = Vector2.new(102, 102);
+		frameRate = 50;
+	}), container, true)
+
+	makeButton(maid, Flipbook.new({
+		image = "rbxassetid://9234616869";
+		rows = 6;
+		columns = 6;
+		imageRectSize = Vector2.new(102, 102);
+		frameRate = 60;
+	}), container, true)
+
+	makeButton(maid, Flipbook.new({
+		image = "rbxassetid://5085366281";
+		rows = 6;
+		columns = 6;
+		frameCount = 34;
+		imageRectSize = Vector2.new(85.33333, 85.33333);
+		frameRate = 20;
+	}), container, true)
+
+	return function()
+		maid:DoCleaning()
+	end
+end
