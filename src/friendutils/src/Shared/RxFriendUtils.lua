2d90850b5926fc953fd4875b8c6d243946291bf7
James Onnen <jonnen0@gmail.com>
2023-03-31 17:14:01 -0700

    feat: RxFriendUtils is available on server

--- src/friendutils/src/Shared/RxFriendUtils.lua
+++ src/friendutils/src/Shared/RxFriendUtils.lua
@@ -20,9 +20,14 @@ local RxFriendUtils = {}
 	The lifetimes exist for the whole duration another player is a friend and in your server.
 	This means if a player is unfriended + friended multiple times per session, they will have emitted multiple friend lifetimes.
 
+	@param player Player | nil
 	@return Observable<Brio<Player>>
 ]=]
-function RxFriendUtils.observeFriendsInServerAsBrios()
+function RxFriendUtils.observeFriendsInServerAsBrios(player)
+	player = player or Players.LocalPlayer
+
+	assert(typeof(player) == "Instance", "Bad player")
+
 	-- Note that 'PlayerFriendedEvent' and 'PlayerUnfriendedEvent' are currently unreliable.
 	-- See: https://devforum.roblox.com/t/getcores-playerfriendedevent-and-playerunfriendedevent-bindableevents-firing-at-inappropriate-times/570403/4
 	-- Due to players initially starting with an 'unknown' friend value, they fire for all players in the game at launch, and on the first time another player that joins the server.
@@ -49,43 +54,49 @@ function RxFriendUtils.observeFriendsInServerAsBrios()
 			end
 		end
 
-		-- Handle players leaving / joining.
-		maid:GiveTask(Players.PlayerRemoving:Connect(function(player: Player)
-			maid[player] = nil
-		end))
-		local function handleNewPlayer(otherPlayer: Player)
-			if otherPlayer ~= Players.LocalPlayer then
-				-- You can't be friends with yourself!
-				-- Pcall as :IsFriendsWith() could throw.
-				local success, isFriendsWith = pcall(function()
-					return Players.LocalPlayer:IsFriendsWith(otherPlayer.UserId)
+		local function handleNewPlayerAsync(otherPlayer: Player)
+			if otherPlayer ~= player then
+				local ok, isFriendsWith = pcall(function()
+					return player:IsFriendsWith(otherPlayer.UserId)
 				end)
-				if not success then
-					warn(("[RxFriendUtils] Couldn't get friendship status with %q!"):format(otherPlayer.Name))
+				if not ok then
+					warn(string.format("[RxFriendUtils.observeFriendsInServerAsBrios] Couldn't get friendship status with %q!",
+						otherPlayer.Name))
+
+					-- If the call failed, then 'isFriendsWith' will be nil.
+					-- We'll assume that this player isn't a friend on failure.
+					handleFriendState(otherPlayer, false)
+					return
 				end
-				-- If the call failed, then 'isFriendsWith' will be nil.
-				-- We'll assume that this player isn't a friend on failure.
+
 				handleFriendState(otherPlayer, isFriendsWith)
 			end
 		end
-		maid:GiveTask(Players.PlayerAdded:Connect(handleNewPlayer))
+
+		-- Handle players leaving / joining.
+		maid:GiveTask(Players.PlayerRemoving:Connect(function(otherPlayer: Player)
+			maid[otherPlayer] = nil
+		end))
+		maid:GiveTask(Players.PlayerAdded:Connect(handleNewPlayerAsync))
+
 		for _, otherPlayer in Players:GetPlayers() do
-			-- :IsFriendsWith() yields, so we call it in a new thread.
-			task.defer(handleNewPlayer, otherPlayer)
+			task.spawn(handleNewPlayerAsync, otherPlayer)
 		end
 
 		-- Handle changes for players already in this server.
 		-- There's a non-zero chance these get removed someday... :(
 		-- https://devforum.roblox.com/t/playerfriendedevent-was-deleted-from-corescripts/696683
 		-- So just incase these connections throw, use a new thread so we don't error out the whole observable.
-		task.spawn(function()
-			maid:GiveTask(StarterGui:GetCore("PlayerFriendedEvent").Event:Connect(function(player: Player)
-				handleFriendState(player, true)
-			end))
-			maid:GiveTask(StarterGui:GetCore("PlayerUnfriendedEvent").Event:Connect(function(player: Player)
-				handleFriendState(player, false)
-			end))
-		end)
+		if player == Players.LocalPlayer then
+			task.spawn(function()
+				maid:GiveTask(StarterGui:GetCore("PlayerFriendedEvent").Event:Connect(function(otherPlayer: Player)
+					handleFriendState(otherPlayer, true)
+				end))
+				maid:GiveTask(StarterGui:GetCore("PlayerUnfriendedEvent").Event:Connect(function(otherPlayer: Player)
+					handleFriendState(otherPlayer, false)
+				end))
+			end)
+		end
 
 		return maid
 	end)
