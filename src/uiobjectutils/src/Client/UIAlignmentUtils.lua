fb98622dad4ca05bf28ecf90b55521c6d48f9b38
James Onnen <jonnen0@gmail.com>
2023-05-11 19:54:02 -0700

    feat: Add UIAlignmentUtils.verticalAlignmentToBias(verticalAlignment) and UIAlignmentUtils.horizontalAlignmentToBias(horizontalAlignment)

--- src/uiobjectutils/src/Client/UIAlignmentUtils.lua
+++ src/uiobjectutils/src/Client/UIAlignmentUtils.lua
@@ -1,39 +1,88 @@
 --[=[
+	Utility methods for working with horizontal and vertical alignment
 	@class UIAlignmentUtils
 ]=]
 
 local UIAlignmentUtils = {}
 
+--[=[
+	Converts alignment to a number from 0 to 1
+
+	@param horizontalAlignment HorizontalAlignment
+	@return number
+]=]
 function UIAlignmentUtils.horizontalAlignmentToNumber(horizontalAlignment)
 	assert(horizontalAlignment, "Bad horizontalAlignment")
 
-	local x
 	if horizontalAlignment == Enum.HorizontalAlignment.Left then
-		x = 0
+		return 0
 	elseif horizontalAlignment == Enum.HorizontalAlignment.Center then
-		x = 0.5
+		return 0.5
 	elseif horizontalAlignment == Enum.HorizontalAlignment.Right then
-		x = 1
+		return 1
 	else
 		error(("[UIAlignmentUtils] - Bad horizontalAlignment %q"):format(tostring(horizontalAlignment)))
 	end
-
-	return x
 end
 
+--[=[
+	Converts alignment to a number from -1 to 1
+
+	@param horizontalAlignment HorizontalAlignment
+	@return number
+]=]
+function UIAlignmentUtils.horizontalAlignmentToBias(horizontalAlignment)
+	assert(horizontalAlignment, "Bad horizontalAlignment")
+
+	if horizontalAlignment == Enum.HorizontalAlignment.Left then
+		return -1
+	elseif horizontalAlignment == Enum.HorizontalAlignment.Center then
+		return 0
+	elseif horizontalAlignment == Enum.HorizontalAlignment.Right then
+		return 1
+	else
+		error(("[UIAlignmentUtils] - Bad horizontalAlignment %q"):format(tostring(horizontalAlignment)))
+	end
+end
+
+--[=[
+	Converts alignment to a number from 0 to 1
+
+	@param verticalAlignment VerticalAlignment
+	@return number
+]=]
 function UIAlignmentUtils.verticalAlignmentToNumber(verticalAlignment)
 	assert(verticalAlignment, "Bad verticalAlignment")
-	local y
+
 	if verticalAlignment == Enum.VerticalAlignment.Top then
-		y = 0
+		return 0
 	elseif verticalAlignment == Enum.VerticalAlignment.Center then
-		y = 0.5
+		return 0.5
 	elseif verticalAlignment == Enum.VerticalAlignment.Bottom then
-		y = 1
+		return 1
 	else
 		error(("[UIAlignmentUtils] - Bad verticalAlignment %q"):format(tostring(verticalAlignment)))
 	end
-	return y
+end
+
+--[=[
+	Converts alignment to a number from -1 to 1
+
+	@param verticalAlignment VerticalAlignment
+	@return number
+]=]
+function UIAlignmentUtils.verticalAlignmentToBias(verticalAlignment)
+	assert(verticalAlignment, "Bad verticalAlignment")
+
+	if verticalAlignment == Enum.VerticalAlignment.Top then
+		return -1
+	elseif verticalAlignment == Enum.VerticalAlignment.Center then
+		return 0
+	elseif verticalAlignment == Enum.VerticalAlignment.Bottom then
+		return 1
+	else
+		error(("[UIAlignmentUtils] - Bad verticalAlignment %q"):format(tostring(verticalAlignment)))
+	end
 end
 
 return UIAlignmentUtils
