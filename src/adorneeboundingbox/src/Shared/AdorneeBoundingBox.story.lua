6e1f8f72394cc788d558b56720c04f1ec155f3c9
James Onnen <jonnen0@gmail.com>
2023-06-23 18:23:55 -0700

    feat: Add AdorneeBoundingBox code

---
+++ src/adorneeboundingbox/src/Shared/AdorneeBoundingBox.story.lua
@@ -0,0 +1,46 @@
+--[[
+	@class AdorneeBoundingBox.story
+]]
+
+local require = require(game:GetService("ServerScriptService"):FindFirstChild("LoaderUtils", true).Parent).load(script)
+
+local Maid = require("Maid")
+local AdorneeBoundingBox = require("AdorneeBoundingBox")
+local RxSelectionUtils = require("RxSelectionUtils")
+local Rx = require("Rx")
+local Draw = require("Draw")
+
+return function(_target)
+	local topMaid = Maid.new()
+
+	topMaid:GiveTask(RxSelectionUtils.observeSelectionItemsBrio():Subscribe(function(brio)
+		if brio:IsDead() then
+			return
+		end
+
+		local maid = brio:ToMaid()
+		local part = brio:GetValue()
+
+		local adorneeBoundingBox = AdorneeBoundingBox.new(part)
+		maid:GiveTask(adorneeBoundingBox)
+
+		maid:GiveTask(Rx.combineLatest({
+			cframe = adorneeBoundingBox:ObserveCFrame();
+			size = adorneeBoundingBox:ObserveSize();
+		}):Subscribe(function(state)
+			if state.cframe and state.size then
+				if state.size == Vector3.zero then
+					maid._current = Draw.cframe(state.cframe)
+				else
+					maid._current = Draw.box(state.cframe, state.size)
+				end
+			else
+				maid._current = nil
+			end
+		end))
+	end))
+
+	return function()
+		topMaid:DoCleaning()
+	end
+end
