5230266032a171167b27f06798908e10c731c718
James Onnen <jonnen0@gmail.com>
2023-07-10 13:48:15 -0700

    feat: Add RbxAsset package

---
+++ src/rbxasset/src/Shared/RbxAssetUtils.lua
@@ -0,0 +1,26 @@
+--[=[
+	@class RbxAssetUtils
+]=]
+
+local require = require(script.Parent.loader).load(script)
+
+local RbxAssetUtils = {}
+
+--[=[
+	Converts a string or number to a string for playback.
+	@param id string? | number
+	@return string?
+]=]
+function RbxAssetUtils.toRbxAssetId(id: string? | number): string
+	if type(id) == "number" then
+		return ("rbxassetid://%d"):format(id)
+	else
+		return id
+	end
+end
+
+function RbxAssetUtils.isConvertableToRbxAsset(id: any): boolean
+	return type(id) == "string" or type(id) == "number"
+end
+
+return RbxAssetUtils
