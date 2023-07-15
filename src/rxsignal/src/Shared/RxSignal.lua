9c932e4483f240243dfcd8d0174850540ad96a2b
James Onnen <jonnen0@gmail.com>
2023-07-14 22:33:36 -0700

    feat: Add RxSignal package

---
+++ src/rxsignal/src/Shared/RxSignal.lua
@@ -0,0 +1,41 @@
+--[=[
+	@class RxSignal
+]=]
+
+local require = require(script.Parent.loader).load(script)
+
+local Rx = require("Rx")
+
+local RxSignal = {}
+RxSignal.ClassName = "RxSignal"
+RxSignal.__index = RxSignal
+
+--[=[
+	Converts an observable to the Signal interface
+
+	@param observable Observable<T>
+	@return RxSignal<T>
+]=]
+function RxSignal.new(observable)
+	assert(observable, "No observable")
+
+	local self = setmetatable({}, RxSignal)
+
+	self._observable = observable:Pipe({
+		Rx.onlyAfterDefer();
+	})
+
+	return self
+end
+
+function RxSignal:Connect(callback)
+	return self._observable:Subscribe(callback)
+end
+
+function RxSignal:Once(callback)
+	return self._observable:Pipe({
+		Rx.take(1);
+	}):Subscribe(callback)
+end
+
+return RxSignal
