713e5e434391e6ba53e568010c9c3c96885331e9
James Onnen <jonnen0@gmail.com>
2023-12-22 14:15:45 -0800

    docs: Update docs spelling mistake

--- src/chatproviderservice/src/Server/ChatProviderService.lua
+++ src/chatproviderservice/src/Server/ChatProviderService.lua
@@ -41,7 +41,7 @@ function ChatProviderService:Init(serviceBag)
 	self._serviceBag:GetService(require("ChatTag"))
 	self._hasChatTagsBinder = self._serviceBag:GetService(require("HasChatTags"))
 
-	-- note: normally we don't expose default APi surfaces like this with defaults, however, because this only affects developers and this
+	-- note: normally we don't expose default API surfaces like this with defaults, however, because this only affects developers and this
 	-- tends to significantly improve feedback we're leaving this default configuration in place.
 	self:SetDeveloperTag(ChatTagDataUtils.createChatTagData({
 		TagText = "(dev)";
