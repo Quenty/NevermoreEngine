49463828ae0a67c07baebf69bb33bf81a3b3e48a
James Onnen <jonnen0@gmail.com>
2024-04-23 10:38:23 -0700

    refactor: Update datastore management maid call

--- src/datastore/src/Server/PlayerDataStoreService.lua
+++ src/datastore/src/Server/PlayerDataStoreService.lua
@@ -113,19 +113,18 @@ function PlayerDataStoreService:PromiseManager()
 			return DataStorePromises.promiseDataStore(self._dataStoreName, self._dataStoreScope)
 		end)
 		:Then(function(dataStore)
-			local manager = PlayerDataStoreManager.new(
+			local manager = self._maid:Add(PlayerDataStoreManager.new(
 				dataStore,
 				function(player)
 					return tostring(player.UserId)
 				end,
-				true)
+				true))
 
 			-- A lot safer if we're hot reloading or need to monitor bind to close calls
 			self._maid:GiveTask(self._bindToCloseService:RegisterPromiseOnCloseCallback(function()
 				return manager:PromiseAllSaves()
 			end))
 
-			self._maid:GiveTask(manager)
 			return manager
 		end)
 
