87b7538e05f93cefdf495579616c8882860af1a1
Matthew Miller <InvisibleWater2345@gmail.com>
2024-08-13 15:56:33 -0400

    Overhaul of tag retrieval for Blend.Tags

--- src/blend/src/Shared/Blend/Blend.lua
+++ src/blend/src/Shared/Blend/Blend.lua
@@ -641,7 +641,7 @@ end
 function Blend.Tags(parent, value)
 	assert(typeof(parent) == "Instance", "Bad parent")
 
-	local observe = Blend._observeTags(value, parent)
+	local observe = Blend._observeTags(value)
 
 	if observe then
 		return observe:Pipe({
@@ -663,13 +663,9 @@ function Blend._observeTags(tags)
 	if type(tags) == "string" then
 		return Rx.of(tags)
 	elseif type(tags) == "table" then
-		if Observable.isObservable(tags) then
-			return tags
-		else
-			error("Bad tags")
-		end
+		return Blend.toPropertyObservable(tags) or Rx.of(unpack(tags))
 	else
-		error("Bad tags")
+		return nil
 	end
 end
 
