6b7f1bf9905a1cfa5864dcd5de4b811ad0b6f0a5
James Onnen <jonnen0@gmail.com>
2024-09-24 18:41:05 -0700

    feat: Add TrajectoryDrawUtils

---
+++ src/trajectory/src/Shared/TrajectoryDrawUtils.lua
@@ -0,0 +1,33 @@
+--[=[
+	@class TrajectoryDrawUtils
+]=]
+
+local require = require(script.Parent.loader).load(script)
+
+local MinEntranceVelocityUtils = require("MinEntranceVelocityUtils")
+local Draw = require("Draw")
+
+local ORIGIN_COLOR = Color3.new(0, 1, 0)
+local FINISH_COLOR = Color3.new(1, 0, 0)
+
+local TrajectoryDrawUtils = {}
+
+function TrajectoryDrawUtils.draw(velocity, origin, target, accel)
+	Draw.point(origin, ORIGIN_COLOR)
+	Draw.point(target, FINISH_COLOR)
+
+	local entranceTime = MinEntranceVelocityUtils.computeEntranceTime(velocity, origin, target, accel)
+
+	for t=0, entranceTime, 0.1 do
+		local t0 = math.clamp(t, 0, entranceTime)
+		local t1 = math.clamp(t + 0.1, 0, entranceTime)
+
+		local p0 = origin + velocity*t0 + 0.5*accel*t0*t0
+		local p1 = origin + velocity*t1 + 0.5*accel*t1*t1
+
+		local percent = t/entranceTime
+		Draw.line(p0, p1, ORIGIN_COLOR:Lerp(FINISH_COLOR, percent))
+	end
+end
+
+return TrajectoryDrawUtils
