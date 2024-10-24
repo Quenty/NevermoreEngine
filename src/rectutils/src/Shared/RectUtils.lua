7a99464a0a5982f5a921aa17311f49e2eba896f3
James Onnen <jonnen0@gmail.com>
2024-10-24 15:51:57 -0700

    feat: Add RectUtils package

---
+++ src/rectutils/src/Shared/RectUtils.lua
@@ -0,0 +1,26 @@
+--[=[
+	Utility method to work with the Roblox Rect data object.
+
+	@class RectUtils
+]=]
+
+local require = require(script.Parent.loader).load(script)
+
+local RectUtils = {}
+
+--[=[
+	Returns true if the position is contained within the rect
+
+	@param position Vector2
+	@return boolean
+]=]
+function RectUtils.contains(rect: Rect, position: Vector2)
+	local relativePosition = position - rect.Min
+
+	return relativePosition.X >= 0
+		and relativePosition.Y >= 0
+		and relativePosition.X <= rect.Width
+		and relativePosition.Y <= rect.Height
+end
+
+return RectUtils
