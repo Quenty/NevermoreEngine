da7ebe96293020467156a8aba9a71961e1f3e6a6
unrooot <ty@n4w.to>
2025-03-31 19:47:43 -0700

    fix: Remove unused files from test template

--- tests/test-place-template/src/modules/Shared/UnitTestTranslator.lua
+++
@@ -1,9 +0,0 @@
---[[
-	@class UnitTestTranslator
-]]
-
-local require = require(script.Parent.loader).load(script)
-
-return require("JSONTranslator").new("UnitTestTranslator", "en", {
-	gameName = "UnitTest";
-})
