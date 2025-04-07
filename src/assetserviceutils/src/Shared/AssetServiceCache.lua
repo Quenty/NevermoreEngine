11ae6894c9c40c596e521dc1d2a71977af63752f
James Onnen <jonnen@roblox.com>
2025-04-07 13:01:28 -0700

    docs: Update docs for AssetServiceCache

--- src/assetserviceutils/src/Shared/AssetServiceCache.lua
+++ src/assetserviceutils/src/Shared/AssetServiceCache.lua
@@ -1,4 +1,7 @@
+--!strict
 --[=[
+	AssetServiceCache is a cache for the AssetServiceUtils functions.
+
 	@class AssetServiceCache
 ]=]
 
@@ -7,10 +10,15 @@ local require = require(script.Parent.loader).load(script)
 local MemorizeUtils = require("MemorizeUtils")
 local AssetServiceUtils = require("AssetServiceUtils")
 local _ServiceBag = require("ServiceBag")
+local _Promise = require("Promise")
 
 local AssetServiceCache = {}
 AssetServiceCache.ServiceName = "AssetServiceCache"
 
+--[=[
+	Initializes the AssetServiceCache.
+	@param serviceBag ServiceBag
+]=]
 function AssetServiceCache:Init(serviceBag: _ServiceBag.ServiceBag)
 	assert(not self._serviceBag, "Already initialized")
 	self._serviceBag = assert(serviceBag, "No serviceBag")
@@ -18,7 +26,12 @@ function AssetServiceCache:Init(serviceBag: _ServiceBag.ServiceBag)
 	self:_ensureInit()
 end
 
-function AssetServiceCache:PromiseBundleDetails(bundleId: number)
+--[=[
+	Returns a promise that resolves to the bundle details for the given bundleId.
+	@param bundleId number
+	@return Promise<BundleDetails>
+]=]
+function AssetServiceCache:PromiseBundleDetails(bundleId: number): _Promise.Promise<AssetServiceUtils.BundleDetails>
 	assert(type(bundleId) == "number", "Bad bundleId")
 
 	self:_ensureInit()
