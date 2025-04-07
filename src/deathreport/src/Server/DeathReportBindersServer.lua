2095d5bf4ae5883930858d8b5f6f19ebc28350b9
James Onnen <jonnen0@gmail.com>
2025-04-07 15:39:47 -0700

    refactor: Binder changes

--- src/deathreport/src/Server/DeathReportBindersServer.lua
+++ src/deathreport/src/Server/DeathReportBindersServer.lua
@@ -6,12 +6,8 @@ local require = require(script.Parent.loader).load(script)
 
 local Binder = require("Binder")
 local BinderProvider = require("BinderProvider")
-local PlayerHumanoidBinder = require("PlayerHumanoidBinder")
 
 return BinderProvider.new(script.Name, function(self, serviceBag)
-	-- Tracking
-	self:Add(PlayerHumanoidBinder.new("DeathTrackedHumanoid", require("DeathTrackedHumanoid"), serviceBag))
-
 	-- Stats
 	self:Add(Binder.new("TeamKillTracker", require("TeamKillTracker"), serviceBag))
 	self:Add(Binder.new("PlayerKillTracker", require("PlayerKillTracker"), serviceBag))
