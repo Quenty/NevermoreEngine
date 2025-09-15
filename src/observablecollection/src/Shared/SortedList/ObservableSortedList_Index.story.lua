21fbb0d422ee0339366710ca682c8d4425368d52
James Onnen <jonnen0@gmail.com>
2025-09-14 17:55:12 -0700

    fix: A few more cleanups (#585)
    * fix: Settings cleanup properly
    
    * refactor: Add typing
    
    * fix: Add notes about sorted list
    
    * style: Fix styling

---
+++ src/observablecollection/src/Shared/SortedList/ObservableSortedList_Index.story.lua
@@ -0,0 +1,30 @@
+--[[
+	@class ObservableSortedList.story
+]]
+
+local require =
+	require(game:GetService("ServerScriptService"):FindFirstChild("LoaderUtils", true).Parent).bootstrapStory(script)
+
+local Maid = require("Maid")
+local ObservableSortedList = require("ObservableSortedList")
+
+return function(_target)
+	local maid = Maid.new()
+
+	local observableSortedList = maid:Add(ObservableSortedList.new())
+
+	maid:GiveTask(observableSortedList:ObserveAtIndex(-1):Subscribe(function(entry)
+		print("stack entry", entry)
+	end))
+
+	observableSortedList:Add("A", 0)
+	local removeB = observableSortedList:Add("B", 1)
+
+	task.defer(function()
+		removeB()
+	end)
+
+	return function()
+		maid:DoCleaning()
+	end
+end
