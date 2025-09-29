0f057550765d2db4cbf6967224d9db05818989e6
Daimywil <daimywillemsen@hotmail.nl>
2025-09-29 17:02:49 +0200

    fixed baseobject typings

--- src/baseobject/src/Shared/BaseObject.d.ts
+++ src/baseobject/src/Shared/BaseObject.d.ts
@@ -2,7 +2,7 @@ import { Maid } from '@quenty/maid';
 
 export class BaseObject<T extends Instance | undefined = undefined> {
   public static readonly ClassName: 'BaseObject';
-  protected _instance: T;
+  protected _obj: T;
   protected _maid: Maid;
   constructor(instance?: T);
   public Destroy(): void;
