841d7dd6e8d2da6e3fd85e12d0619dc15e0bd794
Daimywil <daimywillemsen@hotmail.nl>
2025-10-01 19:40:08 +0200

    improved rxbrioutils

--- src/brio/src/Shared/RxBrioUtils.d.ts
+++ src/brio/src/Shared/RxBrioUtils.d.ts
@@ -99,6 +99,12 @@ export namespace RxBrioUtils {
     source: Observable<Brio<T>>
   ) => Observable<Brio<T>>;
   function switchToBrio<T>(
+    predicate?: (value: T) => value is NonNullable<T>
+  ): (source: Observable<T | Brio<T>>) => Observable<Brio<NonNullable<T>>>;
+  function switchToBrio<T>(
+    predicate?: (value: T) => value is Exclude<T, NonNullable<T>>
+  ): (source: Observable<T | Brio<T>>) => Observable<Brio<Exclude<T, NonNullable<T>>>>;
+  function switchToBrio<T>(
     predicate?: (value: T) => boolean
   ): (source: Observable<T | Brio<T>>) => Observable<Brio<T>>;
 }
