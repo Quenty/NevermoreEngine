23580a979936eb13dd850b097b7c050e54373d37
James Onnen <jonnen0@gmail.com>
2025-10-01 11:20:34 -0700

    feat: Add RxHumanoidUtils (#593)

---
+++ src/humanoidutils/src/Shared/RxHumanoidUtils.lua
@@ -0,0 +1,64 @@
+--[=[
+	@class RxHumanoidUtils
+]=]
+
+local require = require(script.Parent.loader).load(script)
+
+local Maid = require("Maid")
+local Observable = require("Observable")
+
+local RxHumanoidUtils = {}
+
+--[=[
+	Observes the running speed of a humanoid
+
+	:::tip
+	When using :MoveTo this is the only way to know if the humanoid is walking at any speed
+	:::
+
+	@return Observable<number>
+]=]
+function RxHumanoidUtils.observeRunningSpeed(humanoid: Humanoid): Observable.Observable<number>
+	assert(typeof(humanoid) == "Instance" and humanoid:IsA("Humanoid"), "No humanoid")
+
+	return Observable.new(function(sub)
+		local maid = Maid.new()
+
+		local lastRunningSpeed = nil
+
+		local function emitRunningSpeed(speed: number)
+			if lastRunningSpeed ~= speed then
+				lastRunningSpeed = speed
+				sub:Fire(speed)
+			end
+		end
+
+		maid:GiveTask(humanoid.Running:Connect(function(speed)
+			emitRunningSpeed(speed)
+		end))
+
+		maid:GiveTask(humanoid.Swimming:Connect(function()
+			emitRunningSpeed(0)
+		end))
+
+		maid:GiveTask(humanoid.Jumping:Connect(function()
+			emitRunningSpeed(0)
+		end))
+
+		maid:GiveTask(humanoid.FreeFalling:Connect(function()
+			emitRunningSpeed(0)
+		end))
+
+		maid:GiveTask(humanoid.Seated:Connect(function()
+			emitRunningSpeed(0)
+		end))
+
+		if not lastRunningSpeed then
+			emitRunningSpeed(0)
+		end
+
+		return maid
+	end)
+end
+
+return RxHumanoidUtils
