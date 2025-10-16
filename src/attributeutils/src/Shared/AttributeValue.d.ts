f90ab3be82bac4b77a2373ee18a61807d6e57276
Daimywil <daimywillemsen@hotmail.nl>
2025-10-16 11:18:15 +0200

    improved attributevalue typings

--- src/attributeutils/src/Shared/AttributeValue.d.ts
+++ src/attributeutils/src/Shared/AttributeValue.d.ts
@@ -1,11 +1,17 @@
-import { Brio } from '../../../brio';
-import { Observable } from '../../../rx';
+import { Brio } from '@quenty/brio';
+import { Observable } from '@quenty/rx';
 import { Signal } from '@quenty/signal';
 
 interface AttributeValue<T> {
   Value: T;
   Changed: Signal;
   Observe(): Observable<T>;
+  ObserveBrio(
+    condition: (value: T) => value is NonNullable<T>
+  ): Observable<Brio<NonNullable<T>>>;
+  ObserveBrio(
+    condition: (value: T) => value is Exclude<T, NonNullable<T>>
+  ): Observable<Brio<Exclude<T, NonNullable<T>>>>;
   ObserveBrio(condition?: (value: T) => boolean): Observable<Brio<T>>;
   Destroy(): void;
 }
