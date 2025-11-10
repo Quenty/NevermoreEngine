572d20bb0c6f8fa626836e4ce57f580ee4adf229
Daimywil <daimywillemsen@hotmail.nl>
2025-11-10 16:32:18 +0100

    improved brioutils

--- src/brio/src/Shared/BrioUtils.d.ts
+++ src/brio/src/Shared/BrioUtils.d.ts
@@ -1,6 +1,18 @@
 import { Brio } from './Brio';
 
-type ToTuple<T> = T extends unknown[] ? T : [T];
+type ToTuple<T> = T extends [unknown, ...unknown[]] ? T : [T];
+
+type BrioInnerOrTuple<B> = B extends Brio<infer R>
+  ? R extends unknown[]
+    ? R
+    : [R]
+  : never;
+
+type FlattenTuples<T extends unknown[]> = T extends [infer Head, ...infer Tail]
+  ? Head extends unknown[]
+    ? [...Head, ...FlattenTuples<Tail>]
+    : [Head, ...FlattenTuples<Tail>]
+  : [];
 
 export namespace BrioUtils {
   function clone<T>(brio: Brio<T>): Brio<T>;
@@ -11,12 +23,22 @@ export namespace BrioUtils {
   ): Brio<Map<K, T>>;
   function first<T, U>(brios: Array<Brio<T>>, ...values: ToTuple<U>): Brio<U>;
   function withOtherValues<T, U>(brio: Brio<T>, ...values: ToTuple<U>): Brio<U>;
-  function extend<T>(brio: Brio<unknown[]>, ...values: ToTuple<T>): Brio<T>;
+  function extend<T, U extends Brio<unknown>[]>(
+    brio: Brio<T>,
+    ...values: U
+  ): Brio<
+    LuaTuple<
+      [
+        ...ToTuple<T>,
+        ...FlattenTuples<{ [K in keyof U]: BrioInnerOrTuple<U[K]> }>
+      ]
+    >
+  >;
   function prepend<T>(brio: Brio<unknown[]>, ...values: ToTuple<T>): Brio<T>;
   function merge<T, U>(
     brio: Brio<T>,
     otherBrio: Brio<U>
-  ): Brio<[...ToTuple<T>, ...ToTuple<U>]>;
+  ): Brio<LuaTuple<[...ToTuple<T>, ...ToTuple<U>]>>;
 }
 
 export {};
