5c25a4ce76a35164de35cb3a1738e5fa5032336d
James Onnen <jonnen0@gmail.com>
2026-01-17 20:09:06 -0800

    feat: Add DynamicHide to the Hide package to allow for dynamically hiding content

--- src/hide/src/Server/Hide.lua
+++ src/hide/src/Server/Hide.lua
@@ -1,4 +1,5 @@
 --!strict
+local HideUtils = require(script.Parent.Parent.Shared.HideUtils)
 --[=[
 	Primarily used for authoring, this hides the tagged instance from render. Great for
 	making bounding boxes in studio that are then hidden upon runtime.
@@ -25,28 +26,27 @@ Hide.__index = Hide
 function Hide.new(adornee: Instance)
 	local self = setmetatable({}, Hide)
 
-	self._obj = assert(adornee, "No adornee")
+	self._instance = assert(adornee, "No adornee")
 
-	if self._obj:IsA("BasePart") then
-		self:_setupPart(self._obj)
-	elseif self._obj:IsA("Model") then
-		for _, item in self._obj:GetChildren() do
-			if item:IsA("BasePart") then
-				self:_setupPart(item)
-			end
-		end
-	elseif self._obj:IsA("GuiObject") then
-		self._obj.BackgroundTransparency = 1
-	else
-		error("[Hide] - Bad object type for hide")
+	if not self._instance:HasTag("DynamicHide") then
+		self:_hideInstance(self._instance)
 	end
 
 	return self
 end
 
-function Hide:_setupPart(part: BasePart)
-	part.Locked = true
-	part.Transparency = 1
+function Hide:_hideInstance(instance: Instance)
+	if instance:IsA("BasePart") then
+		instance.Locked = true
+		instance.Transparency = 1
+	elseif instance:IsA("Model") or instance:IsA("Folder") then
+		-- Be limited in what we recurse down
+		for _, child in self._instance:GetChildren() do
+			self:_hideInstance(child)
+		end
+	elseif HideUtils.hasTransparency(instance) then
+		(instance :: any).Transparency = 1
+	end
 end
 
 --[=[
--- src/hide/src/Shared/HideUtils.lua
+++ src/hide/src/Shared/HideUtils.lua
@@ -19,4 +19,31 @@ function HideUtils.isHidden(inst: Instance): boolean
 	return CollectionService:HasTag(inst, "Hide")
 end
 
+function HideUtils.hasLocalTransparencyModifier(instance: Instance): boolean
+	return instance:IsA("BasePart")
+		or instance:IsA("Beam")
+		or instance:IsA("Decal")
+		or instance:IsA("Explosion")
+		or instance:IsA("Fire")
+		or instance:IsA("ParticleEmitter")
+		or instance:IsA("Smoke")
+		or instance:IsA("Sparkles")
+		or instance:IsA("Trail")
+end
+
+function HideUtils.hasTransparency(instance: Instance): boolean
+	return instance:IsA("BasePart")
+		or instance:IsA("Beam")
+		or instance:IsA("CanvasGroup")
+		or instance:IsA("Decal")
+		or instance:IsA("GuiBase3d")
+		or instance:IsA("GuiObject")
+		or instance:IsA("ParticleEmitter")
+		or instance:IsA("Path2D")
+		or instance:IsA("Terrain")
+		or instance:IsA("Trail")
+		or instance:IsA("UIGradient")
+		or instance:IsA("UIStroke")
+end
+
 return HideUtils
