ff9cedb0b01d6332a82fe8d1b9945db2685fbe6d
James Onnen <jonnen0@gmail.com>
2026-04-08 11:54:18 -0700

    refactor: service-package-template has unit tests by default

--- tools/nevermore-cli/templates/nevermore-service-package-template/test/scripts/Server/ServerMain.server.lua
+++ tools/nevermore-cli/templates/nevermore-service-package-template/test/scripts/Server/ServerMain.server.lua
@@ -6,6 +6,11 @@ local ServerScriptService = game:GetService("ServerScriptService")
 local loader = ServerScriptService:FindFirstChild("LoaderUtils", true).Parent
 local require = require(loader).bootstrapGame(ServerScriptService.{{packageName}})
 
+local NevermoreTestRunnerUtils = require("NevermoreTestRunnerUtils")
+if NevermoreTestRunnerUtils.runTestsIfNeededAsync(ServerScriptService.{{packageName}}) then
+	return
+end
+
 local serviceBag = require("ServiceBag").new()
 serviceBag:GetService(require("{{packageNameProper}}Service"))
 serviceBag:Init()
