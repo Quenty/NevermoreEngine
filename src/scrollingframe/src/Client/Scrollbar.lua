bd5908afefafaaa181cb1d72a85176d1838adcfa
James Onnen <jonnen0@gmail.com>
2026-06-26 03:53:05 +0000

    feat(scrollingframe): convert package to --!strict

--- src/scrollingframe/src/Client/Scrollbar.lua
+++ src/scrollingframe/src/Client/Scrollbar.lua
@@ -1,4 +1,4 @@
---!nonstrict
+--!strict
 --[=[
 	@class Scrollbar
 ]=]
@@ -7,15 +7,37 @@ local require = require(script.Parent.loader).load(script)
 
 local Maid = require("Maid")
 local SCROLL_TYPE = require("SCROLL_TYPE")
+local ScrollModel = require("ScrollModel")
 local Signal = require("Signal")
 local Table = require("Table")
+
+type ScrollType = typeof(SCROLL_TYPE.Vertical)
+
+-- ScrollingFrame is still nonstrict and exports no type; type the surface we use.
+type ScrollingFrameLike = {
+	GetModel: (self: ScrollingFrameLike) -> ScrollModel.ScrollModel,
+	StartScrollbarScrolling: (self: ScrollingFrameLike, scrollbarContainer: Instance, inputBeganObject: InputObject) -> Maid.Maid,
+}
 
 local Scrollbar = {}
 Scrollbar.ClassName = "Scrollbar"
 Scrollbar.__index = Scrollbar
 
-function Scrollbar.new(gui, scrollType)
-	local self = setmetatable({}, Scrollbar)
+export type Scrollbar = typeof(setmetatable(
+	{} :: {
+		Gui: GuiObject,
+		DraggingBegan: Signal.Signal<()>,
+		_maid: Maid.Maid,
+		_container: Instance,
+		_scrollType: ScrollType,
+		_scrollingFrame: ScrollingFrameLike,
+		_model: ScrollModel.ScrollModel,
+	},
+	{} :: typeof({ __index = Scrollbar })
+))
+
+function Scrollbar.new(gui: GuiObject, scrollType: ScrollType?): Scrollbar
+	local self: Scrollbar = setmetatable({} :: any, Scrollbar)
 
 	self.Gui = gui or error("No gui")
 	self.DraggingBegan = Signal.new()
@@ -27,7 +49,7 @@ function Scrollbar.new(gui, scrollType)
 	return self
 end
 
-function Scrollbar.fromContainer(container, scrollType)
+function Scrollbar.fromContainer(container: GuiObject, scrollType: ScrollType?): Scrollbar
 	local gui = Instance.new("ImageButton")
 	gui.Size = UDim2.fromScale(1, 1)
 	gui.Name = "ScrollBar"
@@ -42,12 +64,12 @@ function Scrollbar.fromContainer(container, scrollType)
 	return Scrollbar.new(gui, scrollType)
 end
 
-function Scrollbar:SetScrollType(scrollType)
+function Scrollbar.SetScrollType(self: Scrollbar, scrollType: ScrollType): ()
 	assert(Table.contains(SCROLL_TYPE, scrollType))
 	self._scrollType = scrollType
 end
 
-function Scrollbar:SetScrollingFrame(scrollingFrame)
+function Scrollbar.SetScrollingFrame(self: Scrollbar, scrollingFrame: ScrollingFrameLike): ()
 	self._scrollingFrame = scrollingFrame or error("No scrollingFrame")
 	self._model = self._scrollingFrame:GetModel()
 
@@ -60,7 +82,7 @@ function Scrollbar:SetScrollingFrame(scrollingFrame)
 	self:UpdateRender()
 end
 
-function Scrollbar:UpdateRender()
+function Scrollbar.UpdateRender(self: Scrollbar): ()
 	if self._model.TotalContentLength > self._model.ViewSize then
 		local percentSize = self._model.RenderedContentScrollPercentSize
 		local pos = (1 - percentSize) * self._model.RenderedContentScrollPercent
@@ -81,10 +103,10 @@ function Scrollbar:UpdateRender()
 	end
 end
 
-function Scrollbar:Destroy()
-	self._maid:DoCleaning()
-	self._maid = nil
-	setmetatable(self, nil)
+function Scrollbar.Destroy(self: Scrollbar): ()
+	self._maid:DoCleaning();
+	(self :: any)._maid = nil
+	setmetatable(self :: any, nil)
 end
 
 return Scrollbar
