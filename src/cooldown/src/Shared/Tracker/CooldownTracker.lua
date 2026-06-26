f5ea8a95563c51e8ed49d8c6817562a989bcfe66
James Onnen <jonnen0@gmail.com>
2026-06-26 03:53:03 +0000

    feat(cooldown): convert package to --!strict

--- src/cooldown/src/Shared/Tracker/CooldownTracker.lua
+++ src/cooldown/src/Shared/Tracker/CooldownTracker.lua
@@ -1,4 +1,4 @@
---!nonstrict
+--!strict
 --[=[
 	Tracks current cooldown on an object
 	@class CooldownTracker
@@ -7,6 +7,7 @@
 local require = require(script.Parent.loader).load(script)
 
 local BaseObject = require("BaseObject")
+local Binder = require("Binder")
 local CooldownShared = require("CooldownShared")
 local CooldownTrackerModel = require("CooldownTrackerModel")
 local Maid = require("Maid")
@@ -18,10 +19,20 @@ local CooldownTracker = setmetatable({}, BaseObject)
 CooldownTracker.ClassName = "CooldownTracker"
 CooldownTracker.__index = CooldownTracker
 
-function CooldownTracker.new(serviceBag: ServiceBag.ServiceBag, parent)
+export type CooldownTracker = typeof(setmetatable(
+	{} :: {
+		_serviceBag: ServiceBag.ServiceBag,
+		_cooldownBinder: Binder.Binder<CooldownShared.CooldownShared>,
+		CurrentCooldown: ValueObject.ValueObject<CooldownShared.CooldownShared?>,
+		_cooldownTrackModel: CooldownTrackerModel.CooldownTrackerModel,
+	},
+	{} :: typeof({ __index = CooldownTracker })
+)) & BaseObject.BaseObject
+
+function CooldownTracker.new(serviceBag: ServiceBag.ServiceBag, parent: Instance): CooldownTracker
 	assert(typeof(parent) == "Instance", "Bad parent")
 
-	local self = setmetatable(BaseObject.new(parent), CooldownTracker)
+	local self: CooldownTracker = setmetatable(BaseObject.new(parent) :: any, CooldownTracker)
 
 	self._serviceBag = assert(serviceBag, "No serviceBag")
 	self._cooldownBinder = self._serviceBag:GetService(CooldownShared)
@@ -35,7 +46,7 @@ function CooldownTracker.new(serviceBag: ServiceBag.ServiceBag, parent)
 
 	-- Handle not running
 	self._maid:GiveTask(
-		RxBinderUtils.observeBoundChildClassBrio(self._cooldownBinder, self._obj):Subscribe(function(brio)
+		RxBinderUtils.observeBoundChildClassBrio(self._cooldownBinder, self._obj :: Instance):Subscribe(function(brio)
 			if brio:IsDead() then
 				return
 			end
@@ -64,15 +75,19 @@ function CooldownTracker.new(serviceBag: ServiceBag.ServiceBag, parent)
 	return self
 end
 
-function CooldownTracker:GetCooldownTrackerModel()
+function CooldownTracker.GetCooldownTrackerModel(self: CooldownTracker): CooldownTrackerModel.CooldownTrackerModel
 	return self._cooldownTrackModel
 end
 
-function CooldownTracker:IsCoolingDown(): boolean
+function CooldownTracker.IsCoolingDown(self: CooldownTracker): boolean
 	return self._cooldownTrackModel:IsCoolingDown()
 end
 
-function CooldownTracker:_handleNewCooldown(new, _old)
+function CooldownTracker._handleNewCooldown(
+	self: CooldownTracker,
+	new: CooldownShared.CooldownShared?,
+	_old: CooldownShared.CooldownShared?
+): ()
 	local maid = Maid.new()
 
 	if new then
