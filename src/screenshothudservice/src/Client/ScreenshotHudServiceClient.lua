02f67bfe7dbd55fc5c6a4982180f523a1a683807
James Onnen <jonnen0@gmail.com>
2026-07-03 05:51:55 +0000

    feat(screenshothudservice): convert package to --!strict

--- src/screenshothudservice/src/Client/ScreenshotHudServiceClient.lua
+++ src/screenshothudservice/src/Client/ScreenshotHudServiceClient.lua
@@ -1,4 +1,4 @@
---!nonstrict
+--!strict
 --[=[
 	@class ScreenshotHudServiceClient
 ]=]
@@ -7,27 +7,39 @@ local require = require(script.Parent.loader).load(script)
 
 local GuiService = game:GetService("GuiService")
 
+local Brio = require("Brio")
 local Maid = require("Maid")
+local Observable = require("Observable")
 local Rx = require("Rx")
 local RxBrioUtils = require("RxBrioUtils")
 local RxInstanceUtils = require("RxInstanceUtils")
+local ScreenshotHudModel = require("ScreenshotHudModel")
 local ServiceBag = require("ServiceBag")
 local StateStack = require("StateStack")
 
 local ScreenshotHudServiceClient = {}
 ScreenshotHudServiceClient.ServiceName = "ScreenshotHudServiceClient"
 
-function ScreenshotHudServiceClient:Init(serviceBag: ServiceBag.ServiceBag)
-	assert(not self._serviceBag, "Already initialized")
+export type ScreenshotHudServiceClient = typeof(setmetatable(
+	{} :: {
+		_serviceBag: ServiceBag.ServiceBag,
+		_maid: Maid.Maid,
+		_screenshotHudState: StateStack.StateStack<ScreenshotHudModel.ScreenshotHudModel?>,
+	},
+	{} :: typeof({ __index = ScreenshotHudServiceClient })
+))
+
+function ScreenshotHudServiceClient.Init(self: ScreenshotHudServiceClient, serviceBag: ServiceBag.ServiceBag): ()
+	assert(not (self :: any)._serviceBag, "Already initialized")
 	self._serviceBag = assert(serviceBag, "No serviceBag")
 	self._maid = Maid.new()
 
-	self._screenshotHudState = self._maid:Add(StateStack.new(nil))
+	self._screenshotHudState = self._maid:Add(StateStack.new(nil :: ScreenshotHudModel.ScreenshotHudModel?))
 
-	self._maid:GiveTask(RxBrioUtils.flatCombineLatest({
+	self._maid:GiveTask((RxBrioUtils.flatCombineLatest({
 		model = self._screenshotHudState:Observe(),
 		screenshotHUD = self:_observeScreenshotHudBrio(),
-	}):Subscribe(function(state)
+	}) :: any):Subscribe(function(state: any)
 		self._maid._current = nil
 		local maid = Maid.new()
 		if state.model and state.screenshotHUD then
@@ -46,7 +58,10 @@ end
 	@param screenshotHudModel ScreenshotHudModel
 	@return function -- cleanup code
 ]=]
-function ScreenshotHudServiceClient:PushModel(screenshotHudModel)
+function ScreenshotHudServiceClient.PushModel(
+	self: ScreenshotHudServiceClient,
+	screenshotHudModel: ScreenshotHudModel.ScreenshotHudModel
+): () -> ()
 	local maid = Maid.new()
 
 	maid:GiveTask(self._screenshotHudState:PushState(screenshotHudModel))
@@ -56,11 +71,16 @@ function ScreenshotHudServiceClient:PushModel(screenshotHudModel)
 	end
 end
 
-function ScreenshotHudServiceClient:_bindModelToHUD(maid: Maid.Maid, model, screenshotHUD)
-	maid:GiveTask(Rx.combineLatest({
+function ScreenshotHudServiceClient._bindModelToHUD(
+	self: ScreenshotHudServiceClient,
+	maid: Maid.Maid,
+	model: ScreenshotHudModel.ScreenshotHudModel,
+	screenshotHUD: any
+): ()
+	maid:GiveTask((Rx.combineLatest({
 		visible = model:ObserveCloseButtonVisible(),
 		position = model:ObserveCloseButtonPosition(),
-	}):Subscribe(function(state)
+	}) :: any):Subscribe(function(state: any)
 		if state.visible then
 			screenshotHUD.CloseButtonPosition = state.position
 		else
@@ -70,10 +90,10 @@ function ScreenshotHudServiceClient:_bindModelToHUD(maid: Maid.Maid, model, scre
 	end))
 
 	-- I'm not sure why you would do this, but it's here
-	maid:GiveTask(Rx.combineLatest({
+	maid:GiveTask((Rx.combineLatest({
 		visible = model:ObserveCameraButtonVisible(),
 		position = model:ObserveCameraButtonPosition(),
-	}):Subscribe(function(state)
+	}) :: any):Subscribe(function(state: any)
 		if state.visible then
 			screenshotHUD.CameraButtonPosition = state.position
 		else
@@ -132,11 +152,13 @@ function ScreenshotHudServiceClient:_bindModelToHUD(maid: Maid.Maid, model, scre
 	end)
 end
 
-function ScreenshotHudServiceClient:_observeScreenshotHudBrio()
-	return RxInstanceUtils.observeLastNamedChildBrio(GuiService, "ScreenshotHud", "ScreenshotHud")
+function ScreenshotHudServiceClient._observeScreenshotHudBrio(
+	self: ScreenshotHudServiceClient
+): Observable.Observable<Brio.Brio<Instance>>
+	return RxInstanceUtils.observeLastNamedChildBrio(GuiService, "ScreenshotHud", "ScreenshotHud") :: any
 end
 
-function ScreenshotHudServiceClient:Destroy()
+function ScreenshotHudServiceClient.Destroy(self: ScreenshotHudServiceClient): ()
 	self._maid:DoCleaning()
 end
 
