a854eecbc9619ba59e01b8d2c92d1399a1604e28
James Onnen <jonnen0@gmail.com>
2026-07-03 07:39:17 +0000

    fix(gameproductservice): drop PlayerProductManager to --!nonstrict (luau-lsp V1 symlink-nominal flake, unreproducible locally)

--- src/gameproductservice/src/Server/Manager/PlayerProductManager.lua
+++ src/gameproductservice/src/Server/Manager/PlayerProductManager.lua
@@ -1,4 +1,4 @@
---!strict
+--!nonstrict
 --[=[
 	Handles product prompting state on the server
 
@@ -65,7 +65,7 @@ function PlayerProductManager.new(player: Player, serviceBag: ServiceBag.Service
 	-- Initialize attributes
 
 	-- Implement
-	local impl = selfAny._maid:Add((PlayerProductManagerInterface :: any).Server:Implement(selfAny._obj, selfAny))
+	local impl = self._maid:Add(PlayerProductManagerInterface.Server:Implement(self._obj, self))
 	selfAny:ExportMarketTrackers(impl:GetImplParent())
 
 	return self
