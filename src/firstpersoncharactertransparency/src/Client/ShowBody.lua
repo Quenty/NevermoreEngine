9c18471c74105c75985b6845a42029d49e28bbd1
James Onnen <jonnen0@gmail.com>
2026-07-03 05:51:54 +0000

    feat(firstpersoncharactertransparency): convert package to --!strict

--- src/firstpersoncharactertransparency/src/Client/ShowBody.lua
+++ src/firstpersoncharactertransparency/src/Client/ShowBody.lua
@@ -1,4 +1,4 @@
---!nonstrict
+--!strict
 --[=[
 	@class ShowBody
 ]=]
@@ -6,6 +6,8 @@
 local require = require(script.Parent.loader).load(script)
 
 local BaseObject = require("BaseObject")
+local Brio = require("Brio")
+local Observable = require("Observable")
 local RxBrioUtils = require("RxBrioUtils")
 local RxInstanceUtils = require("RxInstanceUtils")
 local ServiceBag = require("ServiceBag")
@@ -15,11 +17,21 @@ local ShowBody = setmetatable({}, BaseObject)
 ShowBody.ClassName = "ShowBody"
 ShowBody.__index = ShowBody
 
-function ShowBody.new(character, serviceBag: ServiceBag.ServiceBag)
-	local self = setmetatable(BaseObject.new(character), ShowBody)
+export type ShowBody =
+	typeof(setmetatable(
+		{} :: {
+			_serviceBag: ServiceBag.ServiceBag,
+			_transparencyService: TransparencyService.TransparencyService,
+		},
+		{} :: typeof({ __index = ShowBody })
+	))
+	& BaseObject.BaseObject
+
+function ShowBody.new(character: Model, serviceBag: ServiceBag.ServiceBag): ShowBody
+	local self: ShowBody = setmetatable(BaseObject.new(character) :: any, ShowBody)
 
 	self._serviceBag = assert(serviceBag, "No serviceBag")
-	self._transparencyService = self._serviceBag:GetService(TransparencyService)
+	self._transparencyService = self._serviceBag:GetService(TransparencyService) :: any
 
 	self._maid:GiveTask(self:_observeNonHeadAndFaceAccessoryPartsBrio():Subscribe(function(brio)
 		if brio:IsDead() then
@@ -54,33 +66,33 @@ function ShowBody.new(character, serviceBag: ServiceBag.ServiceBag)
 	return self
 end
 
-function ShowBody:_observeNonHeadParts()
-	return RxInstanceUtils.observeChildrenBrio(self._obj, function(child)
+function ShowBody._observeNonHeadParts(self: ShowBody): Observable.Observable<Brio.Brio<BasePart>>
+	return RxInstanceUtils.observeChildrenBrio(self._obj :: Instance, function(child)
 		return child:IsA("BasePart") and child.Name ~= "Head" -- make some assumptions
-	end)
+	end) :: any
 end
 
-function ShowBody:_observeNonHeadAndFaceAccessoryPartsBrio()
-	return self:_observeAccessoriesBrio():Pipe({
-		RxBrioUtils.flatMapBrio(function(accessory)
-			return RxInstanceUtils.observePropertyBrio(accessory, "AccessoryType", function(accessoryType)
+function ShowBody._observeNonHeadAndFaceAccessoryPartsBrio(self: ShowBody): Observable.Observable<Brio.Brio<BasePart>>
+	return (self:_observeAccessoriesBrio() :: any):Pipe({
+		RxBrioUtils.flatMapBrio(function(accessory: any): any
+			return (RxInstanceUtils.observePropertyBrio(accessory, "AccessoryType", function(accessoryType)
 				return accessoryType ~= Enum.AccessoryType.Hat and accessoryType ~= Enum.AccessoryType.Face
-			end):Pipe({
-				RxBrioUtils.onlyLastBrioSurvives(),
-				RxBrioUtils.switchMapBrio(function()
+			end) :: any):Pipe({
+				RxBrioUtils.onlyLastBrioSurvives() :: any,
+				RxBrioUtils.switchMapBrio(function(): any
 					return RxInstanceUtils.observeDescendantsBrio(accessory, function(child)
 						return child:IsA("BasePart")
 					end)
-				end),
+				end) :: any,
 			})
 		end),
 	})
 end
 
-function ShowBody:_observeAccessoriesBrio()
-	return RxInstanceUtils.observeDescendantsBrio(self._obj, function(inst)
+function ShowBody._observeAccessoriesBrio(self: ShowBody): Observable.Observable<Brio.Brio<Accessory>>
+	return RxInstanceUtils.observeDescendantsBrio(self._obj :: Instance, function(inst)
 		return inst:IsA("Accessory")
-	end)
+	end) :: any
 end
 
 return ShowBody
