bdaf1b45f24882af05f40a941f48eb5b125eac84
James Onnen <jonnen0@gmail.com>
2026-07-03 06:59:54 +0000

    fix(settings): cast cmdr handle to dodge flaky symlink-nominal analyze error

--- src/settings/src/Client/SettingsServiceClient.lua
+++ src/settings/src/Client/SettingsServiceClient.lua
@@ -165,9 +165,9 @@ function SettingsServiceClient.PromisePlayerSettings(
 end
 
 function SettingsServiceClient._setupCmdr(self: SettingsServiceClient)
-	local cmdrServiceClient = self._serviceBag:GetService(require("CmdrServiceClient")) :: any
+	local cmdrServiceClient = self._serviceBag:GetService(require("CmdrServiceClient"))
 
-	self._maid:GivePromise(cmdrServiceClient:PromiseCmdr()):Then(function(cmdr)
+	self._maid:GivePromise((cmdrServiceClient :: any):PromiseCmdr()):Then(function(cmdr)
 		SettingsCmdrUtils.registerSettingDefinition(cmdr, self._serviceBag)
 	end)
 end
